7e373a4bbaf5a32a8cc0004830caef9f5eb9fd74
Makoto Morita <makmorit@outlook.jp>
2017-11-01 14:26:53 +0900

    キーハンドル生成関連の処理を共通化
    Register、Authenticationの双方で、キーハンドル格納バッファが共通利用できるよう修正。

--- nRF5_SDK_v13.0.0/examples/diverta/ble_u2f_lib/ble_u2f_crypto_ecb.c
+++ nRF5_SDK_v13.0.0/examples/diverta/ble_u2f_lib/ble_u2f_crypto_ecb.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "ble_u2f.h"
 #include "ble_u2f_flash.h"
+#include "ble_u2f_crypto_ecb.h"
 #include "ble_u2f_util.h"
 #include "fds.h"
 
@@ -30,6 +31,11 @@ static uint32_t            m_random_vector[8];
 // AES CFBモード
 #define AES_CFB_MODE_ENCRYPTION 0
 #define AES_CFB_MODE_DECRYPTION 1
+
+// キーハンドル生成・格納用領域
+// Register, Authenticateで共通使用
+uint8_t keyhandle_base_buffer[64];
+uint8_t keyhandle_buffer[64];
 
 
 static bool write_random_vector(uint32_t *p_fds_record_buffer)
@@ -204,5 +210,36 @@ void ble_u2f_crypto_ecb_decrypt(uint8_t *packet, uint32_t packet_length, uint8_t
 {
     process_aes_cfb_crypto(AES_CFB_MODE_DECRYPTION, packet, packet_length, out_packet);
 }
+
+
+void ble_u2f_crypto_ecb_generate_keyhandle(uint8_t *p_appid_hash, nrf_value_length_t *private_key)
+{
+    // Register/Authenticateリクエストから取得した
+    // appIdHash、秘密鍵(リトルエンディアン)を指定の領域に格納
+    memset(keyhandle_base_buffer, 0, sizeof(keyhandle_base_buffer));
+    memcpy(keyhandle_base_buffer, p_appid_hash, U2F_APPID_SIZE);
+    memcpy(keyhandle_base_buffer + U2F_APPID_SIZE, private_key->p_value, private_key->length);
+
+    // AES ECB暗号対象のバイト配列＆長さを指定し、
+    // Cipher Feedback Modeによる暗号化を実行
+    memset(keyhandle_buffer, 0, sizeof(keyhandle_buffer));
+    uint16_t data_length = 64;
+    ble_u2f_crypto_ecb_encrypt(keyhandle_base_buffer, data_length, keyhandle_buffer);
+}
+
+
+void ble_u2f_crypto_ecb_restore_keyhandle_base(nrf_value_length_t *keyhandle)
+{
+    // Authenticateリクエストから取得した
+    // キーハンドルを指定の領域に格納
+    memset(keyhandle_buffer, 0, sizeof(keyhandle_buffer));
+    memcpy(keyhandle_buffer, keyhandle->p_value, keyhandle->length);
+
+    // Cipher Feedback Modeにより暗号化された
+    // バイト配列を、同じ手法により復号化
+    memset(keyhandle_base_buffer, 0, sizeof(keyhandle_base_buffer));
+    uint16_t data_length = 64;
+    ble_u2f_crypto_ecb_decrypt(keyhandle_buffer, data_length, keyhandle_base_buffer);
+ }
 
 #endif // NRF_MODULE_ENABLED(BLE_U2F)
