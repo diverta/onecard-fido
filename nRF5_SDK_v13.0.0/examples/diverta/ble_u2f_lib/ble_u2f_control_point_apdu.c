568cd33e9fb5e71809a954f7083dc9b4c93b342d
Makoto Morita <makmorit@outlook.jp>
2017-11-09 12:36:35 +0900

    Leバイトが非分割送信時に取得されない不具合を解消
    ・受信データ長がLcを超えた場合のチェックを、データ格納処理の後ろに移動
    ・先頭パケット受信処理内にあった不要なチェックロジックを削除
    ・デバッグログの不要な文字列を削除
    ・変数名が一部、実態に即していない命名なので修正

--- nRF5_SDK_v13.0.0/examples/diverta/ble_u2f_lib/ble_u2f_control_point_apdu.c
+++ nRF5_SDK_v13.0.0/examples/diverta/ble_u2f_lib/ble_u2f_control_point_apdu.c
@@ -34,12 +34,12 @@ static uint16_t get_apdu_lc_value(U2F_APDU_T *p_apdu, uint8_t *control_point_buf
             } else {
                 p_apdu->Le = length;
             }
-            NRF_LOG_DEBUG("INIT frame: Extended Length Encoding: Lc(%d) Le(%d) \r\n", p_apdu->Lc, p_apdu->Le);
+            NRF_LOG_DEBUG("Lc(%d bytes) Le(%d bytes) in Extended Length Encoding\r\n", p_apdu->Lc, p_apdu->Le);
         } else {
             // 先頭パケットからはLcの値だけしか取得できない
             // Leの値は最終パケットから取得する
-            NRF_LOG_DEBUG("INIT frame: Extended Length Encoding: Lc(%d) \r\n", p_apdu->Lc);
             p_apdu->Lc = length;
+            NRF_LOG_DEBUG("Lc(%d bytes) in Extended Length Encoding\r\n", p_apdu->Lc);
         }
 
     } else {
@@ -60,12 +60,12 @@ static uint16_t get_apdu_lc_value(U2F_APDU_T *p_apdu, uint8_t *control_point_buf
             } else {
                 p_apdu->Le = length;
             }
-            NRF_LOG_DEBUG("INIT frame: Short Encoding: Lc(%d) Le(%d) \r\n", p_apdu->Lc, p_apdu->Le);
+            NRF_LOG_DEBUG("Lc(%d bytes) Le(%d bytes) in Short Encoding\r\n", p_apdu->Lc, p_apdu->Le);
         } else {
             // 先頭パケットからはLcの値だけしか取得できない
             // Leの値は最終パケットから取得する
             p_apdu->Lc = length;
-            NRF_LOG_DEBUG("INIT frame: Short Encoding: Lc(%d) \r\n", p_apdu->Lc);
+            NRF_LOG_DEBUG("Lc(%d bytes) in Short Encoding\r\n", p_apdu->Lc);
         }
     }
 
@@ -82,7 +82,7 @@ uint8_t ble_u2f_control_point_apdu_header(U2F_APDU_T *p_apdu, uint8_t *control_p
     p_apdu->P1  = control_point_buffer[offset + 2];
     p_apdu->P2  = control_point_buffer[offset + 3];
 
-    NRF_LOG_DEBUG("INIT frame: CLA(0x%02x) INS(0x%02x) P1(0x%02x) P2(0x%02x) \r\n", 
+    NRF_LOG_DEBUG("CLA(0x%02x) INS(0x%02x) P1(0x%02x) P2(0x%02x) \r\n", 
         p_apdu->CLA, p_apdu->INS, p_apdu->P1, p_apdu->P2);
 
     // APDUヘッダーだけの場合はここで終了
@@ -136,79 +136,86 @@ bool ble_u2f_control_point_apdu_allocate(ble_u2f_context_t *p_u2f_context, U2F_A
     return true;
 }
 
-
-void ble_u2f_control_point_apdu_from_leading(U2F_APDU_T *p_apdu, uint8_t *control_point_buffer, uint16_t control_point_buffer_length, uint8_t offset)
-{
-    // Control Pointに格納されている
-    // 受信データの先頭アドレスとデータ長を取得
-    uint8_t *data         = control_point_buffer + offset;
-    int      data_length  = control_point_buffer_length - offset;
-
-    // データを格納し、格納データのバイト数を保持
-    memcpy(p_apdu->data, data, data_length);
-    p_apdu->data_length = data_length;
-
-    if (p_apdu->data_length < p_apdu->Lc) {
-        NRF_LOG_DEBUG("INIT frame: received data (%d of %d) \r\n", p_apdu->data_length, p_apdu->Lc);
-    } else {
-        NRF_LOG_DEBUG("INIT frame: received data (%dbytes) \r\n", p_apdu->data_length);
-    }
-}
-
-
-static uint16_t get_apdu_le_value(U2F_APDU_T *p_apdu, uint8_t *control_point_buffer, uint16_t control_point_buffer_length)
+static uint16_t get_apdu_le_value(U2F_APDU_T *p_apdu, uint8_t *received_data, uint16_t received_data_length)
 {
     // Leのバイト数を求める
-    uint16_t data_length = control_point_buffer_length - 1;
-    uint16_t le_length = (p_apdu->data_length + data_length) - p_apdu->Lc;
+    uint16_t le_length = (p_apdu->data_length + received_data_length) - p_apdu->Lc;
 
     if (le_length == 2) {
         // Leバイトが2バイトの場合
         // Extended Length Encoding と扱い、データの長さを
         // control_point_bufferの末尾２バイトから取得
         p_apdu->Le = (uint32_t)(
-            (control_point_buffer[control_point_buffer_length-2] << 8 & 0xFF00) +
-             control_point_buffer[control_point_buffer_length-1]);
+            (received_data[received_data_length-2] << 8 & 0xFF00) +
+             received_data[received_data_length-1]);
         if (p_apdu->Le == 0) {
             p_apdu->Le = 65536;
         }
-        NRF_LOG_DEBUG("CONT frame: Extended Length Encoding: Le(%d) \r\n", p_apdu->Le);
+        NRF_LOG_DEBUG("Le(%d bytes) in Extended Length Encoding \r\n", p_apdu->Le);
 
     } else if (le_length == 1) {
         // Leバイトが1バイトの場合
         // Short Encoding と扱い、データの長さを
         // control_point_bufferの最終バイトから取得
-        p_apdu->Le = (uint32_t)control_point_buffer[control_point_buffer_length-1];
+        p_apdu->Le = (uint32_t)received_data[received_data_length-1];
         if (p_apdu->Le == 0) {
             p_apdu->Le = 256;
         }
-        NRF_LOG_DEBUG("CONT frame: Short Encoding: Le(%d) \r\n", p_apdu->Le);
+        NRF_LOG_DEBUG("Le(%d bytes) in Short Encoding \r\n", p_apdu->Le);
 
     } else {
-        NRF_LOG_DEBUG("CONT frame: Unknown Encoding: Le(%d bytes) \r\n", le_length);
+        // エンコーディングルールに反している場合はエラーとして長さ0を戻す
+        NRF_LOG_DEBUG("Le(%d bytes) in Unknown Encoding \r\n", le_length);
+        le_length = 0;
     }
     
     return le_length;
 }
 
+void ble_u2f_control_point_apdu_from_leading(U2F_APDU_T *p_apdu, uint8_t *control_point_buffer, uint16_t control_point_buffer_length, uint8_t offset)
+{
+    // Control Pointに格納されている
+    // 受信データの先頭アドレスとデータ長を取得
+    uint8_t *received_data        = control_point_buffer + offset;
+    int      received_data_length = control_point_buffer_length - offset;
+
+    if (received_data_length > p_apdu->Lc) {
+        // データの先頭パケットだが、データ長をオーバーしている場合、
+        // オーバーした部分はLeバイトとして扱い、
+        // Leバイトを除いた部分を、データ部として扱う
+        uint16_t le_length = get_apdu_le_value(p_apdu, received_data, received_data_length);
+        received_data_length = received_data_length - le_length;
+    }
+
+    // データを格納し、格納データのバイト数を保持
+    memcpy(p_apdu->data, received_data, received_data_length);
+    p_apdu->data_length = received_data_length;
+
+    if (p_apdu->data_length < p_apdu->Lc) {
+        NRF_LOG_DEBUG("INIT frame: received data (%d of %d) \r\n", p_apdu->data_length, p_apdu->Lc);
+    } else {
+        NRF_LOG_DEBUG("INIT frame: received data (%d bytes) \r\n", p_apdu->data_length);
+    }
+}
+
 void ble_u2f_control_point_apdu_from_following(BLE_HEADER_T *p_ble_header, U2F_APDU_T *p_apdu, uint8_t *control_point_buffer, uint16_t control_point_buffer_length)
 {
     // 受信データの先頭アドレスとデータ長を取得
-    uint8_t *data = control_point_buffer + 1;
-    uint16_t data_length = control_point_buffer_length - 1;
+    uint8_t *received_data        = control_point_buffer + 1;
+    uint16_t received_data_length = control_point_buffer_length - 1;
 
-    if (p_apdu->data_length + data_length > p_apdu->Lc) {
+    if (p_apdu->data_length + received_data_length > p_apdu->Lc) {
         // データの最終パケットだが、データ長をオーバーしている場合、
         // オーバーした部分はLeバイトとして扱い、
         // Leバイトを除いた部分を、データ部として扱う
-        uint16_t le_length = get_apdu_le_value(p_apdu, control_point_buffer, control_point_buffer_length);
-        data_length = data_length - le_length;
+        uint16_t le_length = get_apdu_le_value(p_apdu, received_data, received_data_length);
+        received_data_length = received_data_length - le_length;
     }
 
     // コピー済みのデータの直後に取得したデータを連結
-    memcpy(p_apdu->data + p_apdu->data_length, data, data_length);
-    p_apdu->data_length += data_length;
-    NRF_LOG_DEBUG("CONT frame: received data (%dbytes) \r\n", p_apdu->data_length);
+    memcpy(p_apdu->data + p_apdu->data_length, received_data, received_data_length);
+    p_apdu->data_length += received_data_length;
+    NRF_LOG_DEBUG("CONT frame: received data (%d bytes) \r\n", p_apdu->data_length);
 }
 
 
