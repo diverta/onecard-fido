436eb54264ed397f10f8557835cf53595f81e5f3
makmorit <makmorit@outlook.jp>
2018-02-07 13:06:43 +0900

    画面アプリ初版 ・フォーム・アプリケーションの実装はC# ・ボタンクリックで、コンソールアプリ版（U2FMaintenanceTool.exe）が起動・実行されるところまで確認済み ・ファイル選択ボタン、テキストエリア部分の処理は未実装

---
+++ U2FMaintenanceTool/WindowsExe/U2FMaintenanceToolGUI/U2FMaintenanceToolGUI/MainForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace U2FMaintenanceToolGUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textPath1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textPath2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonPath1 = new System.Windows.Forms.Button();
+            this.buttonPath2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Location = new System.Drawing.Point(462, 12);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(110, 25);
+            this.buttonQuit.TabIndex = 0;
+            this.buttonQuit.Text = "終了";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ペアリング情報消去";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 25);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "鍵・証明書消去／AES暗号生成";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textPath1
+            // 
+            this.textPath1.Location = new System.Drawing.Point(121, 77);
+            this.textPath1.Name = "textPath1";
+            this.textPath1.Size = new System.Drawing.Size(395, 19);
+            this.textPath1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "鍵ファイルのパス";
+            // 
+            // textPath2
+            // 
+            this.textPath2.Location = new System.Drawing.Point(121, 108);
+            this.textPath2.Name = "textPath2";
+            this.textPath2.Size = new System.Drawing.Size(395, 19);
+            this.textPath2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "証明書ファイルのパス";
+            // 
+            // buttonPath1
+            // 
+            this.buttonPath1.Location = new System.Drawing.Point(522, 74);
+            this.buttonPath1.Name = "buttonPath1";
+            this.buttonPath1.Size = new System.Drawing.Size(50, 25);
+            this.buttonPath1.TabIndex = 7;
+            this.buttonPath1.Text = "参照";
+            this.buttonPath1.UseVisualStyleBackColor = true;
+            this.buttonPath1.Click += new System.EventHandler(this.buttonPath1_Click);
+            // 
+            // buttonPath2
+            // 
+            this.buttonPath2.Location = new System.Drawing.Point(522, 105);
+            this.buttonPath2.Name = "buttonPath2";
+            this.buttonPath2.Size = new System.Drawing.Size(50, 25);
+            this.buttonPath2.TabIndex = 8;
+            this.buttonPath2.Text = "参照";
+            this.buttonPath2.UseVisualStyleBackColor = true;
+            this.buttonPath2.Click += new System.EventHandler(this.buttonPath2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 25);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "鍵・証明書ファイルのインストール";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 164);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(200, 25);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "ヘルスチェック実行";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(462, 164);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(110, 25);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "Chrome設定";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 195);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(560, 164);
+            this.textBox1.TabIndex = 12;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 371);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.buttonPath2);
+            this.Controls.Add(this.buttonPath1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textPath2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textPath1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonQuit);
+            this.Name = "MainForm";
+            this.Text = "U2F Maintenance Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonQuit;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textPath1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textPath2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonPath1;
+        private System.Windows.Forms.Button buttonPath2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ U2FMaintenanceTool/WindowsExe/U2FMaintenanceToolGUI/U2FMaintenanceToolGUI/MainForm.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace U2FMaintenanceToolGUI
+{
+    public partial class MainForm : Form
+    {
+        private AppMain app;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            app = new AppMain();
+        }
+
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+            // このアプリケーションを終了する
+            app.doExit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // ペアリング情報削除
+            enableButtons(false);
+            bool ret = app.doEraseBond();
+            popupResultMessage("ペアリング情報削除処理", ret);
+            enableButtons(true);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 鍵・証明書削除
+            enableButtons(false);
+            bool ret = app.doEraseSkeyCert();
+            popupResultMessage("鍵・証明書削除処理", ret);
+            enableButtons(true);            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // 鍵・証明書インストール
+            enableButtons(false);
+            bool ret = app.doInstallSkeyCert(textPath1.Text, textPath2.Text);
+            popupResultMessage("鍵・証明書インストール", ret);
+            enableButtons(true);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // ヘルスチェック実行
+            enableButtons(false);
+            bool ret = app.doHealthCheck();
+            popupResultMessage("ヘルスチェック", ret);
+            enableButtons(true);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // プロンプトで表示されるメッセージ
+            string message = string.Format("{0}\n\n{1}\n{2}",
+                "ChromeでBLE U2Fトークンが使用できるよう設定します。",
+                "ChromeでBLE U2Fトークンを使用時、U2FMaintenanceTool.exeが、Chromeのサブプロセスとしてバックグラウンド起動されます。",
+                "設定を実行しますか？");
+
+            // Chrome Native Messaging有効化設定
+            // プロンプトを表示し、Yesの場合だけ処理を行う
+            enableButtons(false);
+            if (displayPromptPopup(message)) {
+                bool ret = app.doSetupChromeNativeMessaging();
+                popupResultMessage("Chrome Native Messaging有効化設定", ret);
+            }
+            enableButtons(true);
+        }
+
+        private void buttonPath1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPath2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void enableButtons(bool enabled)
+        {
+            button1.Enabled = enabled;
+            button2.Enabled = enabled;
+            button3.Enabled = enabled;
+            button4.Enabled = enabled;
+            button5.Enabled = enabled;
+            buttonPath1.Enabled = enabled;
+            buttonPath2.Enabled = enabled;
+            textPath1.Enabled = enabled;
+            textPath2.Enabled = enabled;
+            buttonQuit.Enabled = enabled;
+        }
+
+        private void popupResultMessage(string message, bool success)
+        {
+            string formatted = string.Format("{0}が{1}しました。",
+                message, success ? "成功" : "失敗");
+            MessageBox.Show(formatted, AppMain.U2FMaintenanceToolTitle);
+        }
+
+        private bool displayPromptPopup(string message)
+        {
+            DialogResult dialogResult = MessageBox.Show(
+                message, AppMain.U2FMaintenanceToolTitle, 
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            // Yesがクリックされた場合 true を戻す
+            return (dialogResult == DialogResult.Yes);
+        }
+    }
+}
