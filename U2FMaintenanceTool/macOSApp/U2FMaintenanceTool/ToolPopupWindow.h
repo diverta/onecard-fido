71d1f62283f29d3005dc1038d3701fc308e18c38
Makoto Morita <makmorit@outlook.jp>
2018-02-26 15:37:17 +0900

    ポップアップ表示用クラスを新設

---
+++ U2FMaintenanceTool/macOSApp/U2FMaintenanceTool/ToolPopupWindow.h
@@ -0,0 +1,19 @@
+//
+//  ToolPopupWindow.h
+//  U2FMaintenanceTool
+//
+//  Created by Makoto Morita on 2018/02/26.
+//
+#ifndef ToolPopupWindow_h
+#define ToolPopupWindow_h
+
+@interface ToolPopupWindow : NSObject
+
+    + (void)critical:     (NSString *)message informativeText:(NSString *)subMessage;
+    + (void)warning:      (NSString *)message informativeText:(NSString *)subMessage;
+    + (void)informational:(NSString *)message informativeText:(NSString *)subMessage;
+    + (bool)promptYesNo:  (NSString *)message informativeText:(NSString *)subMessage;
+
+@end
+
+#endif /* ToolPopupWindow_h */
