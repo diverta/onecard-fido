b11aaef8e4005f3a0f5bbdb1923f9d31996fbbee
makmorit <makmorit@outlook.jp>
2018-04-18 12:15:42 +0900

    [Issue #43] U2F管理ツールのバージョンが確認できるようにするための対応 ・「U2F管理ツールについて」画面サイズを拡張できないように修正 ・プログラム配布用媒体を修正

--- U2FMaintenanceTool/WindowsExe/U2FMaintenanceToolGUI/U2FMaintenanceToolGUI/AboutForm.Designer.cs
+++ U2FMaintenanceTool/WindowsExe/U2FMaintenanceToolGUI/U2FMaintenanceToolGUI/AboutForm.Designer.cs
@@ -78,6 +78,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutForm";
