8ae6b0e69f867e4900180119b4fb9a5c09c28826
Makoto Morita <makmorit@outlook.jp>
2018-06-25 13:57:09 +0900

    mbedサンプルアプリを修正
    ・U2F Versionコマンドを追加実装
    （One Cardと連携は不要のため、mbedアプリ内でレスポンスしています）
    ・業務固有部分のロジックをU2FHID.cppに切り出し
    ・チャネルIDをuint32_tからuint8_tに変換して扱うよう修正
    ・チャネルからのリクエストデータを分割受信できるよう修正
    （64バイト以上のリクエストデータが送信されてくるU2F Registerに対応）
    ・以上の修正で、U2F Registerコマンドが、U2Fクライアントからmbedアプリに送信されるところまで確認できています。

---
+++ Research/USBHID/NucleoF411RE_usbmouse/U2FHID.cpp
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "U2FHID.h"
+
+//
+// U2Fリクエスト／レスポンスデータ格納領域
+// （コマンド共通）
+//
+uint8_t u2f_request_buffer[128];
+uint8_t u2f_response_buffer[128];
+size_t  u2f_response_length;
+
+//
+// コマンド別のレスポンスデータ編集領域
+//
+typedef struct u2f_hid_init_response
+{
+    uint8_t nonce[8];
+    uint8_t cid[4];
+    uint8_t version_id;
+    uint8_t version_major;
+    uint8_t version_minor;
+    uint8_t version_build;
+    uint8_t cflags;
+} U2F_HID_INIT_RES;
+
+typedef struct u2f_version_response
+{
+    uint8_t  version[6];
+    uint8_t  status_word[2];
+} U2F_VERSION_RES;
+
+U2F_HID_INIT_RES  init_res;
+U2F_VERSION_RES   version_res;
+
+
+uint32_t get_CID(uint8_t *cid)
+{
+    uint32_t _CID;
+
+    _CID  = (cid[0] << 24) & 0xff000000;
+    _CID |= (cid[1] << 16) & 0x00ff0000;
+    _CID |= (cid[2] <<  8) & 0x0000ff00;
+    _CID |= (cid[3] <<  0) & 0x000000ff;
+    
+    return _CID;
+}
+
+void set_CID(uint8_t *cid, uint32_t _CID)
+{
+    cid[0] = (_CID >> 24) & 0x000000ff;
+    cid[1] = (_CID >> 16) & 0x000000ff;
+    cid[2] = (_CID >>  8) & 0x000000ff;
+    cid[3] = (_CID >>  0) & 0x000000ff;
+}
+
+
+void generate_hid_init_response(void)
+{
+    // レスポンスデータを編集 (17 bytes)
+    memcpy(init_res.nonce, u2f_request_buffer, 8);
+    set_CID(init_res.cid, 0x00003301);
+    init_res.version_id    = 2;
+    init_res.version_major = 1;
+    init_res.version_minor = 1;
+    init_res.version_build = 0;
+    init_res.cflags        = 1;
+    
+    // レスポンスを格納
+    u2f_response_length = sizeof(U2F_HID_INIT_RES);
+    memcpy(u2f_response_buffer, &init_res, u2f_response_length);
+}
+
+void generate_u2f_version_response(void)
+{
+    // レスポンスデータを編集 (8 bytes)
+    strcpy((char *)version_res.version, "U2F_V2");
+    uint16_t status_word = U2F_SW_NO_ERROR;
+    version_res.status_word[0] = (status_word >> 8) & 0x00ff;
+    version_res.status_word[1] = status_word & 0x00ff;
+    
+    // レスポンスを格納
+    u2f_response_length = sizeof(U2F_VERSION_RES);
+    memcpy(u2f_response_buffer, &version_res, u2f_response_length);
+}
--- Research/USBHID/NucleoF411RE_usbmouse/main.cpp
+++ Research/USBHID/NucleoF411RE_usbmouse/main.cpp
@@ -2,11 +2,7 @@
 
 #include "mbed.h"
 #include "USBU2FAuthenticator.h"
-
-#define HID_PACKET_SIZE 64
-
-HID_REPORT send_report;
-HID_REPORT recv_report;
+#include "U2FHID.h"
 
 //
 // PC-->mbed: 64バイト
@@ -17,127 +13,160 @@ HID_REPORT recv_report;
 USBU2FAuthenticator u2fAuthenticator(64, 32);
 
 //
-// 後日、U2F HID関連のファイルに切り出し予定
+// HID送信／受信パケット格納領域
 //
-#ifndef U2F_HID_H_
-#define U2F_HID_H_
+HID_REPORT send_report;
+HID_REPORT recv_report;
 
-#include <stdint.h>
-
-#define TYPE_MASK           0x80    // Frame type mask
-#define TYPE_INIT           0x80    // Initial frame identifier
-#define TYPE_CONT           0x00    // Continuation frame identifier
-
-#define U2FHID_PING         (TYPE_INIT | 0x01)  // Echo data through local processor only
-#define U2FHID_MSG          (TYPE_INIT | 0x03)  // Send U2F message frame
-#define U2FHID_LOCK         (TYPE_INIT | 0x04)  // Send lock channel command
-#define U2FHID_INIT         (TYPE_INIT | 0x06)  // Channel initialization
-#define U2FHID_WINK         (TYPE_INIT | 0x08)  // Send device identification wink
-#define U2FHID_ERROR        (TYPE_INIT | 0x3f)  // Error response
-
-#define U2FHID_BROADCAST    0xffffffff
-
-#define U2FHID_INIT_PAYLOAD_SIZE  (HID_PACKET_SIZE-7)
-#define U2FHID_CONT_PAYLOAD_SIZE  (HID_PACKET_SIZE-5)
-#define U2FHID_MAX_PAYLOAD_SIZE   (7609)
-
-#define U2FHID_IS_INIT(cmd) ((cmd) & 0x80)
+//
+// チャネルID関連
+//
+uint8_t    CMD;
+uint32_t   CID;
 
 
-typedef struct u2f_hid_msg {
-    uint32_t cid;
-    union {
-        struct {
-            uint8_t cmd;
-            uint8_t bcnth;
-            uint8_t bcntl;
-            uint8_t payload[U2FHID_INIT_PAYLOAD_SIZE];
-        } init;
-        struct {
-            uint8_t seq;
-            uint8_t payload[U2FHID_CONT_PAYLOAD_SIZE];
-        } cont;
-    } pkt;
-} U2F_HID_MSG;
-
-typedef struct u2f_hid_init_response
+size_t get_payload_length(U2F_HID_MSG *recv_msg)
 {
-    uint8_t  nonce[8];
-    uint32_t cid;
-    uint8_t  version_id;
-    uint8_t  version_major;
-    uint8_t  version_minor;
-    uint8_t  version_build;
-    uint8_t  cflags;
-} U2F_HID_INIT_RES;
+    return ((recv_msg->pkt.init.bcnth << 8) & 0xff00) | (recv_msg->pkt.init.bcntl & 0x00ff);
+}
 
-#endif // U2F_HID_H_
-
-U2F_HID_INIT_RES  init_res;
-
-void dump_hid_packet(char *msg_header, size_t size, U2F_HID_MSG *recv_msg)
+void dump_hid_init_packet(char *msg_header, size_t size, U2F_HID_MSG *recv_msg)
 {
     printf("%s(%3d bytes) ", msg_header, size);
-    printf("CID: 0x%08x, ", recv_msg->cid);
-    printf("CMD: 0x%02x, ", recv_msg->pkt.init.cmd);
+    printf("CID: 0x%08x, ",  get_CID(recv_msg->cid));
+    printf("CMD: 0x%02x, ",  recv_msg->pkt.init.cmd);
 
-    size_t len = ((recv_msg->pkt.init.bcnth << 8) & 0xff00) | (recv_msg->pkt.init.bcntl & 0x00ff);
+    size_t len = get_payload_length(recv_msg);
     printf("Payload(%3d bytes): ", len);
-    for(size_t i = 0; i < len; i++) {
+    
+    size_t cnt = (len < U2FHID_INIT_PAYLOAD_SIZE) ? len : U2FHID_INIT_PAYLOAD_SIZE;
+    for(size_t i = 0; i < cnt; i++) {
         printf("%02x ", recv_msg->pkt.init.payload[i]);
     }
     printf("\r\n");
 }
 
-size_t generate_hid_init_response(U2F_HID_MSG *req, U2F_HID_MSG *res)
+void dump_hid_cont_packet(char *msg_header, size_t size, U2F_HID_MSG *recv_msg, size_t remain)
+{
+    printf("%s(%3d bytes) ", msg_header, size);
+    printf("CID: 0x%08x, ",  get_CID(recv_msg->cid));
+    printf("SEQ: 0x%02d, ",  recv_msg->pkt.cont.seq);
+    
+    size_t cnt = (remain < U2FHID_CONT_PAYLOAD_SIZE) ? remain : U2FHID_CONT_PAYLOAD_SIZE;
+    for(size_t i = 0; i < cnt; i++) {
+        printf("%02x ", recv_msg->pkt.cont.payload[i]);
+    }
+    printf("\r\n");
+}
+
+void set_send_report_data(uint8_t *payload_data, size_t payload_length)
 {
     // 送信パケット格納領域を初期化
     memset(&send_report, 0x00, sizeof(send_report));
     send_report.length = 32;
 
-    // パケットデータを編集 (17 bytes)
-    size_t len = 17;
-    memcpy(init_res.nonce, req->pkt.init.payload, 8);
-    init_res.cid           = 0x01330000;
-    init_res.version_id    = 2;
-    init_res.version_major = 1;
-    init_res.version_minor = 1;
-    init_res.version_build = 0;
-    init_res.cflags        = 1;
-    
+    // パケット格納領域を取得
+    U2F_HID_MSG *res = (U2F_HID_MSG *)send_report.data;
+
     // パケットヘッダーを編集 （7 bytes)
-    res->cid            = req->cid;
-    res->pkt.init.cmd   = req->pkt.init.cmd;
-    res->pkt.init.bcnth = (len >> 8) & 0x00ff;
-    res->pkt.init.bcntl = len & 0x00ff;
-    memcpy(res->pkt.init.payload, &init_res, len);
+    set_CID(res->cid, CID);
+    res->pkt.init.cmd   = CMD;
+    res->pkt.init.bcnth = (payload_length >> 8) & 0x00ff;
+    res->pkt.init.bcntl = payload_length & 0x00ff;
     
-    // パケットの全長を戻す
-    return len + 7;
+    // パケットデータを設定
+    memcpy(res->pkt.init.payload, payload_data, payload_length);
+}
+
+bool send_hid_report()
+{
+    U2F_HID_MSG *res = (U2F_HID_MSG *)send_report.data;
+    dump_hid_init_packet("Sent ", send_report.length, res);
+
+    if (u2fAuthenticator.send(&send_report) == false) {
+        printf("u2fAuthenticator.send failed. \r\n");
+        return false;
+    }
+    return true;
 }
 
 bool send_response_packet()
 {
-    if (recv_report.length == 0) {
-        return true;
-    }
-
-    U2F_HID_MSG *res = (U2F_HID_MSG *)send_report.data;
-    U2F_HID_MSG *req = (U2F_HID_MSG *)recv_report.data;
-    dump_hid_packet("Recv ", recv_report.length, req);
-    
-    // U2FHID_INITコマンドを処理する
-    if (U2FHID_IS_INIT(req->pkt.init.cmd) && req->pkt.init.cmd == U2FHID_INIT) {                
-        // パケットを生成して送信
-        generate_hid_init_response(req, res);
-        if (u2fAuthenticator.send(&send_report) == false) {
-            printf("u2fAuthenticator.send failed. \r\n");
+    if (CMD == U2FHID_INIT) {
+        // レスポンスデータを送信パケットに設定
+        generate_hid_init_response();
+        set_send_report_data(u2f_response_buffer, u2f_response_length);
+        if (send_hid_report() == false) {
             return false;
         }
-        dump_hid_packet("Sent ", send_report.length, res);
+    }
+
+    if (CMD == U2FHID_MSG) {
+        // u2f_request_buffer の先頭バイトを参照
+        //   [0]CLA [1]INS [2]P1 3[P2]
+        uint8_t ins = u2f_request_buffer[1];
+        if (ins == U2F_VERSION) {
+            // レスポンスデータを送信パケットに設定
+            generate_u2f_version_response();
+            set_send_report_data(u2f_response_buffer, u2f_response_length);
+            if (send_hid_report() == false) {
+                return false;
+            }
+        }
     }
 
     return true;
+}
+
+bool receive_request_data()
+{
+    static size_t pos;
+    static size_t payload_len;
+
+    if (recv_report.length == 0) {
+        return false;
+    }
+    
+    // チャネルIDを判定する
+    U2F_HID_MSG *req = (U2F_HID_MSG *)recv_report.data;
+    if (get_CID(req->cid) == U2FHID_RESERVED_CID) {
+        // TODO: 
+        //   U2F管理ツールからのリクエストの場合スキップ
+        //   （後日実装予定）
+        printf("receive_request_data: U2F Maintenance Tool is not suppored\r\n");
+        return false;
+    }
+
+    if (U2FHID_IS_INIT(req->pkt.init.cmd)) {
+        dump_hid_init_packet("Recv ", recv_report.length, req);
+
+        // payload長を取得
+        payload_len = get_payload_length(req);
+
+        // リクエストデータ領域に格納
+        pos = (payload_len < U2FHID_INIT_PAYLOAD_SIZE) ? payload_len : U2FHID_INIT_PAYLOAD_SIZE;
+        memcpy(u2f_request_buffer, req->pkt.init.payload, pos);
+
+        // CID、CMDを保持
+        CID = get_CID(req->cid);
+        CMD = req->pkt.init.cmd;
+
+    } else {
+        dump_hid_cont_packet("Recv ", recv_report.length, req, payload_len - pos);
+
+        // リクエストデータ領域に格納
+        size_t remain = payload_len - pos;
+        size_t cnt = (remain < U2FHID_CONT_PAYLOAD_SIZE) ? remain : U2FHID_CONT_PAYLOAD_SIZE;
+        memcpy(u2f_request_buffer + pos, req->pkt.cont.payload, cnt);
+        pos += cnt;
+    }
+
+    // リクエストデータを全て受信したらtrueを戻す
+    if (pos == payload_len) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 int main(void) 
@@ -147,7 +176,10 @@ int main(void)
 
     while (true) {
         if (u2fAuthenticator.readNB(&recv_report)) {
-            send_response_packet();
+            if (receive_request_data() == true) {
+                // リクエストを全て受領したらレスポンス
+                send_response_packet();
+            }
         }
         wait(0.1);
     }
