b8190683eb4d7fa32401b73cdc8428527ced08d3
makmorit <makmorit@outlook.jp>
2018-07-31 13:14:15 +0900

    実装追加 ・プログラムの多重起動を抑止するよう修正

--- U2FMaintenanceTool/WindowsExe/U2FMaintenanceToolGUI/U2FHelper/Program.cs
+++ U2FMaintenanceTool/WindowsExe/U2FMaintenanceToolGUI/U2FHelper/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace U2FHelper
 {
@@ -11,10 +12,25 @@ namespace U2FHelper
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            new MainForm();
-            Application.Run();
+            // Mutexを生成し、プログラムの多重起動を抑止
+            string mutexName = "U2FHelper.exe";
+            bool createdNew;
+            Mutex mutex = new Mutex(true, mutexName, out createdNew);
+            if (createdNew == false) {
+                MessageBox.Show("U2F Helperは既に起動されています.");
+                mutex.Close();
+                return;
+            }
+
+            try {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                new MainForm();
+                Application.Run();
+            } finally {
+                mutex.ReleaseMutex();
+                mutex.Close();
+            }
         }
     }
 }
