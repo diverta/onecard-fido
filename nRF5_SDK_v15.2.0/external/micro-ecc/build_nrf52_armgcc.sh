7ee57b29731594d74bf7ee31b59617708a4cce85
Makoto Morita <makmorit@outlook.jp>
2018-10-03 15:59:02 +0900

    nRF52840調査用のベースプロジェクトを作成
    ・Nordic提供のサンプルアプリ「ble_app_hrs」をベースに作成
    ・NetBeansによるビルド／ダウンロード、およびnRF52840 DKでの実機動作ともに正常であることを確認済み
    ・micro-eccライブラリーはビルド済み

---
+++ nRF5_SDK_v15.2.0/external/micro-ecc/build_nrf52_armgcc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This script will use git (must be in $PATH) and arm-none-eabi tools in combination with GNU Make
+# to both fetch and compile all variants of micro-ecc for the nRF5 families
+
+if ! [ -x "$(command -v git)" ]; then
+    echo 'git is not installed. Please install and append to PATH.' >&2
+    exit
+fi
+
+if [ ! -f micro-ecc/uECC.c ]; then
+    echo "micro-ecc not found! Let's pull it from HEAD."
+    git clone https://github.com/kmackay/micro-ecc.git
+fi
+
+make -C nrf52hf_armgcc/armgcc &&
+echo $?
+make -C nrf52nf_armgcc/armgcc &&
+echo $?
