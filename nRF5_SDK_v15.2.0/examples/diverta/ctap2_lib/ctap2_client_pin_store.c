9fb879fe63255e7dbe794c6d01733ffabe43a1d9
Makoto Morita <makmorit@outlook.jp>
2019-03-25 14:17:38 +0900

    clientPin設定処理を修正
    ・パラメーター 'clientPin' の値を、PIN登録状況に応じて動的に設定できるよう修正
    ・不必要なパラメーター 'uv' が与えられていたので削除

--- nRF5_SDK_v15.2.0/examples/diverta/ctap2_lib/ctap2_client_pin_store.c
+++ nRF5_SDK_v15.2.0/examples/diverta/ctap2_lib/ctap2_client_pin_store.c
@@ -116,3 +116,22 @@ bool ctap2_client_pin_store_hash_write(uint8_t *p_pin_code_hash, uint32_t retry_
 
     return true;
 }
+
+bool ctap2_client_pin_store_pin_code_exist(void)
+{
+    // PINコードをFlash ROMから読み出し
+    if (ctap2_client_pin_store_hash_read() == false) {
+        return false;
+    }
+
+    // PINコードハッシュがゼロ埋めされている場合は未登録と判断
+    uint8_t *pin_code_hash = ctap2_client_pin_store_pin_code_hash();
+    uint8_t  pin_code_hash_size = FIDO_PIN_STORE_HASH_RECORD_SIZE * 4;
+    uint8_t  i;
+    for (i = 0; i < pin_code_hash_size; i++) {
+        if (pin_code_hash[i] != 0) {
+            return true;
+        }
+    }
+    return false;
+}
