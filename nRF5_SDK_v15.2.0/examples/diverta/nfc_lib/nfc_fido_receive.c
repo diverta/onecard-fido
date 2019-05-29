c8f8355fcdd6d9f0507115e885110444922bead0
Makoto Morita <makmorit@outlook.jp>
2019-05-29 17:46:49 +0900

    APDUデータ長チェック処理を追加
    ・１フレームだけが送信されるケースに対応
    ・２フレーム以上に分割されて送信されるケースについては、後日仕様を再確認の上、追加実装予定です。

--- nRF5_SDK_v15.2.0/examples/diverta/nfc_lib/nfc_fido_receive.c
+++ nRF5_SDK_v15.2.0/examples/diverta/nfc_lib/nfc_fido_receive.c
@@ -6,6 +6,7 @@
  */
 #include "nfc_common.h"
 #include "nfc_fido_send.h"
+#include "fido_common.h"
 
 // for logging informations
 #define NRF_LOG_MODULE_NAME nfc_fido_receive
@@ -14,6 +15,9 @@ NRF_LOG_MODULE_REGISTER();
 
 // 現在選択されているアプリケーション
 static NFC_APPLETS selected_app;
+
+// FIDOリクエストAPDUを保持
+FIDO_APDU_T fido_apdu;
 
 static void select_applet(uint8_t *aid, int aid_size)
 {
@@ -57,6 +61,49 @@ static void perform_read_binary(APDU_HEADER *apdu)
     }
 }
 
+void perform_fido_ctap2_message(uint8_t *data, size_t data_size)
+{
+    APDU_HEADER *apdu = (APDU_HEADER *)data;
+    
+    if (selected_app != APP_FIDO) {
+        nfc_fido_send_response(SW_INS_INVALID);
+        return;
+    }
+    
+    // ヘッダー項目をコピー
+    fido_apdu.CLA = apdu->cla;
+    fido_apdu.INS = apdu->ins;
+    fido_apdu.P1 = apdu->p1;
+    fido_apdu.P2 = apdu->p2;
+
+    // APDUエンコーディングの判定を行う。
+    if (apdu->lc == 0x00) {
+        // Extended APDUと判断
+        fido_apdu.data = data + APDU_HEADER_SIZE + 2;
+        fido_apdu.Lc = (size_t)(
+            (data[APDU_HEADER_SIZE + 0] << 8 & 0xFF00) +
+             data[APDU_HEADER_SIZE + 1]
+            );
+        // データ長を計算
+        //   実際の受信データ長 - ヘッダー長(7バイト) - フッター長(2バイト)
+        fido_apdu.data_length = data_size - APDU_HEADER_SIZE - 4;
+        
+    } else {
+        // Short APDUと判断
+        fido_apdu.data = data + APDU_HEADER_SIZE;
+        fido_apdu.Lc = apdu->lc;
+        // データ長を計算
+        //   実際の受信データ長 - ヘッダー長(5バイト) - フッター長(1バイト)
+        fido_apdu.data_length = data_size - APDU_HEADER_SIZE - 1;
+    }
+
+    NRF_LOG_DEBUG("APDU: data size(%d), Lc(%d bytes)", fido_apdu.data_length, fido_apdu.Lc);
+    if (fido_apdu.data_length != fido_apdu.Lc) {
+        // データ長が Lcと異なる場合はエラー
+        nfc_fido_send_response(SW_WRONG_LENGTH);
+    }
+}
+
 void nfc_fido_receive_request_frame(uint8_t *buf, size_t len)
 {
     APDU_HEADER *apdu = (APDU_HEADER *)buf;
@@ -75,6 +122,11 @@ void nfc_fido_receive_request_frame(uint8_t *buf, size_t len)
             perform_read_binary(apdu);
             break;
 
+        case APDU_FIDO_NFCCTAP_MSG:
+            // FIDO 2.0機能へ振り分け
+            perform_fido_ctap2_message(buf, len);
+            break;
+            
         default:
             NRF_LOG_ERROR("Unknown INS %02x", apdu->ins);
 			nfc_fido_send_response(SW_INS_INVALID);
