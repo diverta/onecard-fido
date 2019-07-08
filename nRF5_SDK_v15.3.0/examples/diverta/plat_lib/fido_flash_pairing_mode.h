4df0589ff921d4d65332f7bb6a437fbb90e1cbd7
Makoto Morita <makmorit@outlook.jp>
2019-07-08 10:16:48 +0900

    事前リファクタリング作業
    ・ペアリング関連処理から、FDSに関連する部分を切り出し
    ・FDSイベントに関するログに、モジュール名が出力されるようにするため、NRF_LOG_xxxxの利用に戻す

---
+++ nRF5_SDK_v15.3.0/examples/diverta/plat_lib/fido_flash_pairing_mode.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   fido_flash_pairing_mode.h
+ * Author: makmorit
+ *
+ * Created on 2019/07/08, 9:32
+ */
+#ifndef FIDO_FLASH_PAIRING_MODE_H
+#define FIDO_FLASH_PAIRING_MODE_H
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool fido_flash_pairing_mode_flag(void);
+void fido_flash_pairing_mode_flag_set(void);
+void fido_flash_pairing_mode_flag_clear(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FIDO_FLASH_PAIRING_MODE_H */
