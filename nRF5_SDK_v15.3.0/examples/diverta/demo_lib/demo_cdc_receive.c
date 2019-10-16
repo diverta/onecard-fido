54efde917747e5af173689fc4fb3903dd8a2d940
Makoto Morita <makmorit@outlook.jp>
2019-10-16 13:10:26 +0900

    デモ機能をCDCに移行
    ・デモ機能（One Cardスキャン機能）を、CDCサービスに移行して恒久化
    ・CDCバッファに「onecard_scan_demo」というコマンドを入力すると実行されます。
    ・demo_cdc_receive.c 自体はプラットフォーム固有のコードであるため、nRF52840コード配下（demo_lib）に配置
    ・fido_board.c にあったデモ機能呼び出し処理は不要となったので削除
    ・ble_service_central_demo.c は不要となったので削除

--- nRF5_SDK_v15.3.0/examples/diverta/demo_lib/demo_cdc_receive.c
+++ nRF5_SDK_v15.3.0/examples/diverta/demo_lib/demo_cdc_receive.c
@@ -4,6 +4,12 @@
  *
  * Created on 2019/10/16, 11:12
  */
+#include <string.h>
+
+// プラットフォーム固有のインターフェース
+#include "ble_service_central.h"
+#include "ble_service_central_stat.h"
+
 // 業務処理／HW依存処理間のインターフェース
 #include "fido_platform.h"
 
@@ -42,7 +48,41 @@ void demo_cdc_receive_char_terminate(void)
     }
 }
 
+//
+// デモ機能
+//
+static void resume_function_after_scan(void)
+{
+    // 統計情報をデバッグ出力
+    ble_service_central_stat_debug_print();
+
+    //
+    // One cardダミーデバイスが見つかったらプリントして終了
+    // （422E0000-E141-11E5-A837-0800200C9A66）
+    //
+    char *one_card_uuid_string = "422E0000-E141-11E5-A837-0800200C9A66";
+    ADV_STAT_INFO_T *info = ble_service_central_stat_match_uuid(one_card_uuid_string);
+    if (info == NULL) {
+        fido_log_debug("onecard_scan_demo: One card peripheral device not found.");
+    } else {
+        fido_log_debug("onecard_scan_demo: One card peripheral device found (NAME=%s, ADDR=%s)", 
+            info->dev_name, ble_service_central_stat_btaddr_string(info->peer_addr));
+    }
+}
+
+static void onecard_scan_demo(void)
+{
+    // BLEダミーデバイスをスキャンし、
+    // 見つかった場合、ログをプリント
+    ble_service_central_scan_start(1000, resume_function_after_scan);
+}
+
 void demo_cdc_receive_on_request_received(void)
 {
     fido_log_debug("USB CDC recv [%s](%lu bytes)", m_cdc_buffer, m_cdc_buffer_size);
+
+    // デモ機能用のコマンドを解析
+    if (strcmp(m_cdc_buffer, "onecard_scan_demo") == 0) {
+        onecard_scan_demo();
+    }
 }
