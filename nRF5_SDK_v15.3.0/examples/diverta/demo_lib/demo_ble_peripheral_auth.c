78409d075888eec6a49a6908666dc7c11043b99c
Makoto Morita <makmorit@outlook.jp>
2019-11-06 11:52:48 +0900

    CDC経由の自動認証パラメーター設定を閉塞
    ・自動認証パラメーター設定は、今後管理ツールからHID経由で実行することとなるため、CDC経由で実行できないように修正

--- nRF5_SDK_v15.3.0/examples/diverta/demo_lib/demo_ble_peripheral_auth.c
+++ nRF5_SDK_v15.3.0/examples/diverta/demo_lib/demo_ble_peripheral_auth.c
@@ -21,7 +21,6 @@
 #include "fido_platform.h"
 
 // デモ機能インターフェース
-#include "demo_cdc_service.h"
 #include "demo_ble_peripheral_auth.h"
 
 // for debug log and hexdump
@@ -36,7 +35,6 @@
 #define UUID_STRING_LEN  36
 #define SCAN_SEC_DEFAULT 3
 #define SCAN_ENABLE_DEFAULT 0
-#define CDC_OK_RESPONSE  "OK\r\n"
 
 // スキャン対象サービスUUID（文字列形式）、スキャン秒数、自動認証有効化フラグを保持
 static char    service_uuid_string[UUID_STRING_LEN+1];
@@ -50,7 +48,7 @@ static void save_auth_param(void)
     uint32_t scan_enable   = (uint32_t)service_uuid_scan_enable;
 
     if (fido_flash_blp_auth_param_write(p_uuid_string, scan_sec, scan_enable) == false) {
-        demo_cdc_send_response_buffer_set("Failed to save parameter to flash ROM.\r\n");
+        fido_log_error("Failed to save BLE peripheral auth parameter to flash ROM");
     }
 }
 
@@ -74,94 +72,6 @@ static void restore_auth_param(void)
     service_uuid_scan_enable = (uint8_t)fido_flash_blp_auth_param_service_uuid_scan_enable();
 }
 
-static bool set_auth_uuid(char *p_cdc_buffer, size_t cdc_buffer_size)
-{
-    // デモ機能用のコマンドを解析して実行
-    size_t command_len = strlen(SET_AUTH_UUID_COMMAND);
-    if (strncmp(p_cdc_buffer, SET_AUTH_UUID_COMMAND, command_len) != 0) {
-        return false;
-    }
-
-    uint8_t len = command_len + 1;
-    if (strlen(p_cdc_buffer) == (len + UUID_STRING_LEN)) {
-        // パラメーターが入力されている場合は退避し、Flash ROMに保存
-        //   UUID文字列＝36文字分
-        memcpy(service_uuid_string, p_cdc_buffer + len, UUID_STRING_LEN);
-        save_auth_param();
-        demo_cdc_send_response_buffer_set(CDC_OK_RESPONSE);
-
-    } else {
-        // パラメーター入力エラー
-        demo_cdc_send_response_buffer_set("Invalid UUID parameter.\r\n");
-    }
-
-    return true;
-}
-
-static bool reset_auth_uuid(char *p_cdc_buffer, size_t cdc_buffer_size)
-{
-    if (strcmp(p_cdc_buffer, RESET_AUTH_UUID_COMMAND) != 0) {
-        return false;
-    }
-
-    // 格納領域を初期化し、パラメーターを保存
-    memset(service_uuid_string, 0, sizeof(service_uuid_string));
-    save_auth_param();
-    demo_cdc_send_response_buffer_set(CDC_OK_RESPONSE);
-    return true;
-}
-
-static bool set_auth_uuid_scan_sec(char *p_cdc_buffer, size_t cdc_buffer_size)
-{
-    // デモ機能用のコマンドを解析して実行
-    size_t command_len = strlen(SET_AUTH_UUID_SCAN_SEC_COMMAND);
-    if (strncmp(p_cdc_buffer, SET_AUTH_UUID_SCAN_SEC_COMMAND, command_len) != 0) {
-        return false;
-    }
-    
-    // パラメーターを解析
-    if (strlen(p_cdc_buffer) == command_len) {
-        // 引数がない場合はデフォルトにリセットし、
-        // パラメーターをFlash ROMに保存
-        service_uuid_scan_sec = SCAN_SEC_DEFAULT;
-        save_auth_param();
-        demo_cdc_send_response_buffer_set(CDC_OK_RESPONSE);
-
-    } else {
-        uint8_t sec = (uint32_t)atoi(p_cdc_buffer + command_len);
-        if (sec < 1 || sec > 9) {
-            // エラーメッセージを表示する
-            demo_cdc_send_response_buffer_set("Parameter must be in the range 1 to 9 (sec).\r\n");
-
-        } else {
-            // パラメーターをFlash ROMに保存
-            service_uuid_scan_sec = sec;
-            save_auth_param();
-            demo_cdc_send_response_buffer_set(CDC_OK_RESPONSE);
-        }        
-    }
-
-    return true;
-}
-
-static bool display_param(char *p_cdc_buffer, size_t cdc_buffer_size)
-{
-    if (strcmp(p_cdc_buffer, "auth_uuid") == 0) {
-        if (strlen(service_uuid_string) == 0) {
-            demo_cdc_send_response_buffer_set("Service UUID for scan: not specified\r\n");
-        } else {
-            demo_cdc_send_response_buffer_set("Service UUID for scan: %s\r\n", service_uuid_string);
-        }
-        return true;
-    }
-    if (strcmp(p_cdc_buffer, "auth_uuid_scan_sec") == 0) {
-        demo_cdc_send_response_buffer_set("Service UUID scanning time: %d sec\r\n", service_uuid_scan_sec);
-        return true;
-    }
-
-    return false;
-}
-
 void demo_ble_peripheral_auth_param_init(void)
 {
     // 初期値を設定
@@ -171,23 +81,6 @@ void demo_ble_peripheral_auth_param_init(void)
 
     // Flash ROMに設定されている場合は読み出す
     restore_auth_param();
-}
-
-bool demo_ble_peripheral_auth_param_set(char *p_cdc_buffer, size_t cdc_buffer_size)
-{
-    if (set_auth_uuid_scan_sec(p_cdc_buffer, cdc_buffer_size)) {
-        return true;
-    }
-    if (set_auth_uuid(p_cdc_buffer, cdc_buffer_size)) {
-        return true;
-    }
-    if (reset_auth_uuid(p_cdc_buffer, cdc_buffer_size)) {
-        return true;
-    }
-    if (display_param(p_cdc_buffer, cdc_buffer_size)) {
-        return true;
-    }
-    return false;
 }
 
 static void resume_function_after_scan(void)
