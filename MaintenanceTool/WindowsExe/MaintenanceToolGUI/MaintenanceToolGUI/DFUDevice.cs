7bd92a8e89a8af07b2cbf36ba90261ccb6e2a824
makmorit <makmorit@outlook.jp>
2020-04-06 16:17:22 +0900

    DFUデバイスクラスを修正 ・プログラムで使用する定数を追加

--- MaintenanceTool/WindowsExe/MaintenanceToolGUI/MaintenanceToolGUI/DFUDevice.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolGUI/MaintenanceToolGUI/DFUDevice.cs
@@ -5,6 +5,28 @@ using System.Threading;
 
 namespace MaintenanceToolGUI
 {
+    internal static class NRFDfuConst
+    {
+        // DFU関連の定義
+        public const int NRF_DFU_BYTE_EOM = 0xc0;
+        public const int NRF_DFU_BYTE_RESP_START = 0x60;
+        public const int NRF_DFU_BYTE_RESP_SUCCESS = 0x01;
+
+        // DFUオブジェクト種別
+        public const int NRF_DFU_BYTE_OBJ_INIT_CMD = 0x01;
+        public const int NRF_DFU_BYTE_OBJ_DATA = 0x02;
+
+        // DFUコマンド種別（nRF52用）
+        public const int NRF_DFU_OP_OBJECT_CREATE = 0x01;
+        public const int NRF_DFU_OP_RECEIPT_NOTIF_SET = 0x02;
+        public const int NRF_DFU_OP_CRC_GET = 0x03;
+        public const int NRF_DFU_OP_OBJECT_EXECUTE = 0x04;
+        public const int NRF_DFU_OP_OBJECT_SELECT = 0x06;
+        public const int NRF_DFU_OP_MTU_GET = 0x07;
+        public const int NRF_DFU_OP_OBJECT_WRITE = 0x08;
+        public const int NRF_DFU_OP_PING = 0x09;
+    }
+
     class DFUDevice
     {
         // DFU処理クラスの参照を保持
