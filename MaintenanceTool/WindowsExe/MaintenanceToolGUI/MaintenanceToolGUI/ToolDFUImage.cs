e87d3e7786755515adc1b481b725b5b37203bbd0
makmorit <makmorit@outlook.jp>
2020-04-08 11:13:55 +0900

    DFUイメージクラスを修正 ・書庫ファイルから、.datファイル、.binファイルを抽出する処理を実装 （macOS版と同等の処理） ・一部の変数名を修正 ・不要なログ出力処理を削除

--- MaintenanceTool/WindowsExe/MaintenanceToolGUI/MaintenanceToolGUI/ToolDFUImage.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolGUI/MaintenanceToolGUI/ToolDFUImage.cs
@@ -1,7 +1,9 @@
 ﻿using MaintenanceToolCommon;
 using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Text;
 
 namespace MaintenanceToolGUI
 {
@@ -11,13 +13,13 @@ namespace MaintenanceToolGUI
         // nRF52840アプリケーションファームウェアのバイナリーイメージを保持。
         // .dat=256バイト、.bin=512Kバイトと見積っています。
         //
-        private byte[] nrf52_app_dat = new byte[256];
-        private byte[] nrf52_app_bin = new byte[524288];
-        private byte[] nrf52_app_zip = new byte[524288];
+        private byte[] nrf52AppDat;
+        private byte[] nrf52AppBin;
+        private byte[] nrf52AppZip = new byte[524288];
 
-        public int nrf52_app_dat_size { get; set; }
-        public int nrf52_app_bin_size { get; set; }
-        public int nrf52_app_zip_size { get; set; }
+        public int nrf52AppDatSize { get; set; }
+        public int nrf52AppBinSize { get; set; }
+        public int nrf52AppZipSize { get; set; }
 
         // 更新イメージファイルのリソース名称
         private string DFUImageResourceName;
@@ -26,6 +28,11 @@ namespace MaintenanceToolGUI
         private const string ResourceNamePrefix = "MaintenanceToolGUI.Resources.app_dfu_package.";
         private const string ResourceNameSuffix = ".zip";
 
+        // DFU対象ファイル名
+        private const string NRF52_APP_DAT_FILE_NAME = "nrf52840_xxaa.dat";
+        private const string NRF52_APP_BIN_FILE_NAME = "nrf52840_xxaa.bin";
+        private const string NRF52_APP_ZIP_FILE_NAME = "app_dfu_package";
+
         public ToolDFUImage()
         {
             // ファームウェア更新イメージファイル名を取得
@@ -33,12 +40,23 @@ namespace MaintenanceToolGUI
 
             // ファームウェア更新イメージ(.zip)を配列に読込
             ReadDFUImage();
+
+            // 書庫を解析し、内包されているイメージを抽出
+            ExtractDFUImage();
         }
 
         public string GetUpdateVersionFromDFUImage()
         {
             // ファームウェア更新イメージ名称から、更新バージョンを取得
-            return ExtractUpdateVersion(DFUImageResourceName);
+            string UpdateVersion = ExtractUpdateVersion(DFUImageResourceName);
+
+            // ログ出力
+            AppCommon.OutputLogDebug(string.Format("ToolDFUImage: Firmware version {0}", UpdateVersion));
+            AppCommon.OutputLogDebug(string.Format("ToolDFUImage: {0}({1} bytes), {2}({3} bytes)",
+                NRF52_APP_DAT_FILE_NAME, nrf52AppDatSize,
+                NRF52_APP_BIN_FILE_NAME, nrf52AppBinSize
+                ));
+            return UpdateVersion;
         }
 
         private void GetDFUImageFileResourceName()
@@ -62,28 +80,88 @@ namespace MaintenanceToolGUI
 
         private void ReadDFUImage()
         {
+            // ファイルサイズをゼロクリア
+            nrf52AppDatSize = 0;
+            nrf52AppBinSize = 0;
+            nrf52AppZipSize = 0;
+
             // リソースファイルを開く
             Assembly assembly = Assembly.GetExecutingAssembly();
             Stream stream = assembly.GetManifestResourceStream(DFUImageResourceName);
             if (stream == null) {
                 return;
             }
-            AppCommon.OutputLogDebug(string.Format("ToolDFUImage.ReadDFUImage: DFUImageResourceName={0} Length={1}",
-                DFUImageResourceName, stream.Length));
 
             try {
                 // リソースファイルを配列に読込
-                nrf52_app_zip_size = stream.Read(nrf52_app_zip, 0, (int)stream.Length);
-                AppCommon.OutputLogDebug(string.Format("ToolDFUImage.ReadDFUImage: read {0} bytes",
-                    nrf52_app_zip_size));
+                nrf52AppZipSize = stream.Read(nrf52AppZip, 0, (int)stream.Length);
 
                 // リソースファイルを閉じる
                 stream.Close();
 
             } catch (Exception e) {
                 AppCommon.OutputLogError(string.Format("ToolDFUImage.ReadDFUImage: {0}", e.Message));
-                nrf52_app_zip_size = 0;
             }
+        }
+
+        private void ExtractDFUImage()
+        {
+            // 例外抑止
+            if (nrf52AppZipSize == 0) {
+                return;
+            }
+
+            // .zip書庫ファイルを解析し、内包されている.bin/.datイメージを抽出
+            int i = 0;
+            while (i < nrf52AppZipSize) {
+                if (nrf52AppZip[i + 0] == 0x50 && nrf52AppZip[i + 1] == 0x4B &&
+                    nrf52AppZip[i + 2] == 0x03 && nrf52AppZip[i + 3] == 0x04) {
+                    // 書庫エントリーのヘッダー（50 4B 03 04）が見つかった場合
+                    i += ParseImage(nrf52AppZip, i);
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        private int ParseImage(byte[] data, int index)
+        {
+            // ファイルのサイズ
+            int offset = 18;
+            int compressedSize = AppCommon.ToInt32(data, index + offset, false);
+            offset += 8;
+
+            // ファイル名のサイズ
+            int filenameSize = AppCommon.ToInt16(data, index + offset, false);
+            offset += 2;
+
+            // コメントのサイズ
+            int commentSize = AppCommon.ToInt16(data, index + offset, false);
+            offset += 2;
+
+            // ファイル名
+            byte[] file_name = data.Skip(index + offset).Take(filenameSize).ToArray();
+            string fileNameStr = Encoding.UTF8.GetString(file_name);
+            offset += filenameSize;
+
+            // コメント
+            offset += commentSize;
+
+            // ファイルの内容
+            if (fileNameStr.Equals(NRF52_APP_DAT_FILE_NAME)) {
+                // .datファイルのバイナリーイメージを配列に格納
+                nrf52AppDat = data.Skip(index + offset).Take(compressedSize).ToArray();
+                nrf52AppDatSize = compressedSize;
+
+            } else if (fileNameStr.Equals(NRF52_APP_BIN_FILE_NAME)) {
+                // .binファイルのバイナリーイメージを配列に格納
+                nrf52AppBin = data.Skip(index + offset).Take(compressedSize).ToArray();
+                nrf52AppBinSize = compressedSize;
+            }
+
+            // 書庫エントリーのサイズを戻す
+            offset += compressedSize;
+            return offset;
         }
 
         private string ExtractUpdateVersion(string resName)
