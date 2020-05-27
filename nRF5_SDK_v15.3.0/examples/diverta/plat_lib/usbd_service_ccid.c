e8ee22238e247c962a8b618ae12c39426646763b
Makoto Morita <makmorit@outlook.jp>
2020-05-27 16:53:11 +0900

    CCIDインターフェースの実装
    ・APDUの受信／送信ができるよう修正
    ・APDU処理の内容は仮実装です。
    ・ICCPOWERON、GETSLOTSTATUSに応答できることを実機確認済み
    ・ATRバイトの内容は仮実装です。
    （CCIDドライバーのサポート対象デバイスリストに記述した内容に整合予定）

--- nRF5_SDK_v15.3.0/examples/diverta/plat_lib/usbd_service_ccid.c
+++ nRF5_SDK_v15.3.0/examples/diverta/plat_lib/usbd_service_ccid.c
@@ -11,6 +11,7 @@
 #include "app_error.h"
 
 #include "usbd_service.h"
+#include "app_usbd_ccid.h"
 
 // for logging informations
 #define NRF_LOG_MODULE_NAME usbd_service_ccid
@@ -21,10 +22,93 @@ NRF_LOG_MODULE_REGISTER();
 // CCID関連
 //
 #include "app_usbd_ccid.h"
+//
+// Extended APDUフォーマットに対応するための一時バッファ
+//   512バイトを上限とします。
+//
+static uint8_t received_data[512];
+static size_t  received_data_size;
+
+// 送信用一時バッファ
+static uint8_t send_data[64];
+
+// ATR
+// Fi=372, Di=1, 372 cycles/ETU 10752 bits/s at 4.00 MHz
+// BWT = 5.7s
+static const uint8_t atr_ccid[] = {
+    0x3B, 0xF7, 0x11, 0x00, 0x00, 0x81, 0x31, 0xFE, 0x65, 
+    0x43, 0x61, 0x6E, 0x6F, 0x6B, 0x65, 0x79, 
+    0x99};
+
+//
+// APDU処理（仮実装）
+//
+static void icc_power_on(void)
+{
+    // レスポンス編集
+    memset(send_data, 0, sizeof(send_data));
+    // bMessageType
+    uint8_t bMessageType = received_data[0];
+    send_data[0] = bMessageType;
+    // dwLength
+    send_data[1] = sizeof(atr_ccid); 
+    // abData 
+    memcpy(send_data + 10, atr_ccid, sizeof(atr_ccid)); 
+    // bSeq
+    send_data[6] = received_data[6];
+
+    // レスポンス送信
+    if (received_data[6] < 0x0f) {
+        app_usbd_ccid_ep_input_from_buffer(send_data, 10 + sizeof(atr_ccid));
+    }
+}
+
+static void get_slot_status(void)
+{
+    // レスポンス編集
+    memset(send_data, 0, sizeof(send_data));
+    // bMessageType
+    send_data[0] = 0x61;
+    // bSeq
+    send_data[6] = received_data[6];
+
+    // レスポンス送信
+    if (received_data[6] < 0x0f) {
+        app_usbd_ccid_ep_input_from_buffer(send_data, 10);
+    }
+}
+
+static void apdu_received(void)
+{
+    uint8_t bMessageType = received_data[0];
+    switch (bMessageType) {
+        case 0x62:
+            // PC_TO_RDR_ICCPOWERON
+            icc_power_on();
+            break;
+        case 0x65:
+            // PC_TO_RDR_GETSLOTSTATUS
+            get_slot_status();
+            break;
+        default:
+            break;
+    }
+
+}
 
 static void ccid_user_ev_handler(app_usbd_class_inst_t const *p_inst, enum app_usbd_ccid_user_event_e event)
 {
     switch (event) {
+        case APP_USBD_CCID_USER_EVT_RX_DONE:
+            // フレームデータをバッファに退避
+            memcpy(received_data + received_data_size, 
+                app_usbd_ccid_ep_output_buffer(), app_usbd_ccid_ep_output_buffer_size());
+            received_data_size += app_usbd_ccid_ep_output_buffer_size();
+
+            // APDUの処理を実行
+            apdu_received();
+            received_data_size = 0;
+            break;
         default:
             break;
     }
@@ -40,6 +124,7 @@ void usbd_ccid_init(void)
         NRF_LOG_ERROR("app_usbd_class_append(class_ccid) returns 0x%02x ", ret);
     }
     APP_ERROR_CHECK(ret);
-
+    
+    received_data_size = 0;
     NRF_LOG_DEBUG("usbd_ccid_init() done");
 }
