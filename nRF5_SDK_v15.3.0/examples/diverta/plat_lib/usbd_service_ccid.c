689ffa3ec9e48f1e06f4b82dd73b6cb6062705ea
Makoto Morita <makmorit@outlook.jp>
2020-05-28 15:06:49 +0900

    CCIDプロトコルの実装
    ・APDU処理を正式実装化
    ・ただし、各種業務処理とプラットフォーム固有処理の切り分けは、リファクタリングという形で後日行います。

--- nRF5_SDK_v15.3.0/examples/diverta/plat_lib/usbd_service_ccid.c
+++ nRF5_SDK_v15.3.0/examples/diverta/plat_lib/usbd_service_ccid.c
@@ -10,7 +10,11 @@
 
 #include "app_error.h"
 
+//
+// CCID関連
+//
 #include "usbd_service.h"
+#include "usbd_service_ccid.h"
 #include "app_usbd_ccid.h"
 
 // for logging informations
@@ -19,18 +23,24 @@
 NRF_LOG_MODULE_REGISTER();
 
 //
-// CCID関連
-//
-#include "app_usbd_ccid.h"
-//
 // Extended APDUフォーマットに対応するための一時バッファ
-//   512バイトを上限とします。
+//   1208バイトを上限とします。
 //
-static uint8_t received_data[512];
-static size_t  received_data_size;
+static uint8_t bulkout_data[1280];
+static uint8_t bulkin_data[1280];
 
-// 送信用一時バッファ
-static uint8_t send_data[64];
+// データ受信用一時変数
+static size_t apdu_size_received;
+static size_t apdu_size_expected;
+static volatile uint8_t bulkout_state;
+
+static void clear_value(void)
+{
+    // モジュール変数を初期化
+    apdu_size_received = 0;
+    apdu_size_expected = 0;
+    bulkout_state = 0;
+}
 
 // ATR
 // Fi=372, Di=1, 372 cycles/ETU 10752 bits/s at 4.00 MHz
@@ -43,71 +53,172 @@ static const uint8_t atr_ccid[] = {
 //
 // APDU処理（仮実装）
 //
-static void icc_power_on(void)
+static uint32_t le_bytes_to_hex32(uint8_t *data)
 {
-    // レスポンス編集
-    memset(send_data, 0, sizeof(send_data));
-    // bMessageType
-    uint8_t bMessageType = received_data[0];
-    send_data[0] = bMessageType;
-    // dwLength
-    send_data[1] = sizeof(atr_ccid); 
-    // abData 
-    memcpy(send_data + 10, atr_ccid, sizeof(atr_ccid)); 
-    // bSeq
-    send_data[6] = received_data[6];
-
-    // レスポンス送信
-    if (received_data[6] < 0x0f) {
-        app_usbd_ccid_ep_input_from_buffer(send_data, 10 + sizeof(atr_ccid));
-    }
+    uint32_t u;
+    memcpy(&u, data, 4);
+    return u;
 }
 
-static void get_slot_status(void)
+static void hex32_to_le_bytes(uint32_t u, uint8_t *data)
+{
+    memcpy(data, &u, 4);
+}
+
+static void icc_power_on(uint8_t *received_data)
 {
     // レスポンス編集
-    memset(send_data, 0, sizeof(send_data));
     // bMessageType
-    send_data[0] = 0x61;
-    // bSeq
-    send_data[6] = received_data[6];
+    bulkin_data[0] = RDR_TO_PC_DATABLOCK;
+
+    // dwLength
+    uint32_t u = sizeof(atr_ccid);
+    hex32_to_le_bytes(u, bulkin_data + 1);
+    // APDU
+    uint8_t const *apdu = atr_ccid;
+    size_t apdu_size = sizeof(atr_ccid);
+    memcpy(bulkin_data + CCID_CMD_HEADER_SIZE, apdu, apdu_size);
+
+    // bStatus
+    // BM_COMMAND_STATUS_NO_ERROR(0x00) | BM_ICC_PRESENT_ACTIVE(0x00)
+    bulkin_data[7] = (0x00 | 0x00);
+    // bError
+    bulkin_data[8] = SLOT_NO_ERROR;
+    // bSpecific
+    bulkin_data[9] = 0;
 
     // レスポンス送信
-    if (received_data[6] < 0x0f) {
-        app_usbd_ccid_ep_input_from_buffer(send_data, 10);
+    usbd_ccid_send_data_frame(bulkin_data, CCID_CMD_HEADER_SIZE + apdu_size);
+}
+
+static void get_slot_status(uint8_t error, uint8_t *received_data)
+{
+    // レスポンス編集
+    // bMessageType
+    bulkin_data[0] = RDR_TO_PC_SLOTSTATUS;
+    // dwLength
+    uint32_t u = 0;
+    hex32_to_le_bytes(u, bulkin_data + 1);
+
+    // bStatus
+    // BM_COMMAND_STATUS_NO_ERROR(0x00) | BM_ICC_PRESENT_ACTIVE(0x00)
+    bulkin_data[7] = (0x00 | 0x00);
+    // bError
+    bulkin_data[8] = error;
+    // bSpecific
+    bulkin_data[9] = 0;
+
+    // レスポンス送信
+    if (received_data[6] < 0x05) {
+        usbd_ccid_send_data_frame(bulkin_data, CCID_CMD_HEADER_SIZE);
     }
 }
 
 static void apdu_received(void)
 {
-    uint8_t bMessageType = received_data[0];
+    uint8_t bMessageType = bulkout_data[0];
     switch (bMessageType) {
-        case 0x62:
-            // PC_TO_RDR_ICCPOWERON
-            icc_power_on();
+        case PC_TO_RDR_ICCPOWERON:
+            NRF_LOG_DEBUG("Slot power on");
+            icc_power_on(bulkout_data);
             break;
-        case 0x65:
-            // PC_TO_RDR_GETSLOTSTATUS
-            get_slot_status();
+        case PC_TO_RDR_ICCPOWEROFF:
+            // TODO: 後日実装
+            NRF_LOG_DEBUG("Slot power off");
+            break;
+        case PC_TO_RDR_GETSLOTSTATUS:
+            get_slot_status(SLOT_NO_ERROR, bulkout_data);
+            break;
+        case PC_TO_RDR_XFRBLOCK:
+            // TODO: 後日実装
+            NRF_LOG_DEBUG("PC_TO_RDR_XFRBLOCK requested");
+            break;
+        case PC_TO_RDR_GETPARAMETERS:
+            // TODO: 後日実装
+            NRF_LOG_DEBUG("Slot get param");
             break;
         default:
+            get_slot_status(SLOTERROR_CMD_NOT_SUPPORTED, bulkout_data);
             break;
     }
+}
 
+static void usbd_ccid_data_frame_received(void)
+{
+    // 受信フレームデータを取得
+    uint8_t *data = app_usbd_ccid_ep_output_buffer();
+    size_t len = app_usbd_ccid_ep_output_buffer_size();
+
+    NRF_LOG_DEBUG("usbd_ccid_data_frame_received(%d bytes):", len);
+    NRF_LOG_HEXDUMP_DEBUG(data, len);
+
+    // APDU格納領域
+    uint8_t *apdu_data = bulkout_data + CCID_CMD_HEADER_SIZE;
+
+    // 初回フレームの場合
+    if (bulkout_state == 0) {
+        if (len < CCID_CMD_HEADER_SIZE) {
+            // 受信フレームにヘッダー（10バイト）が含まれていない場合は終了
+            return;
+        }
+
+        // フレームデータをバッファに退避
+        apdu_size_received = len - CCID_CMD_HEADER_SIZE;
+        memcpy(bulkout_data, data, CCID_CMD_HEADER_SIZE);
+        memcpy(apdu_data, data + CCID_CMD_HEADER_SIZE, apdu_size_received);
+
+        // dwLength Offset = 1 (4 bytes)
+        apdu_size_expected = (size_t)le_bytes_to_hex32(bulkout_data + 1);
+
+        // 送信データ領域を初期化
+        memset(bulkin_data, 0, sizeof(bulkin_data));
+        
+        // 送信データ領域に初期値を設定
+        // bSlot Offset = 5
+        // bSeq  Offset = 6
+        bulkin_data[5] = bulkout_data[5];
+        bulkin_data[6] = bulkout_data[6];
+
+        if (apdu_size_received == apdu_size_expected) {
+            // APDUの処理を実行
+            apdu_received();
+
+        } else if (apdu_size_received < apdu_size_expected) {
+            if (apdu_size_expected > ABDATA_SIZE) {
+                // 受信データは無効
+                bulkout_state = 0;
+            } else {
+                // 継続フレームを受信
+                bulkout_state = 1;
+            }
+        } else {
+            // 無効とする
+            bulkout_state = 0;
+        }
+
+    // 継続フレームの場合
+    } else if (bulkout_state == 1) {
+        if (apdu_size_received + len < apdu_size_expected) {
+            memcpy(apdu_data + apdu_size_received, data, len);
+            apdu_size_received += len;
+
+        } else if (apdu_size_received + len == apdu_size_expected) {
+            memcpy(apdu_data + apdu_size_received, data, len);
+            bulkout_state = 0;
+            // APDUの処理を実行
+            apdu_received();
+
+        } else {
+            bulkout_state = 0;
+        }
+    }
 }
 
 static void ccid_user_ev_handler(app_usbd_class_inst_t const *p_inst, enum app_usbd_ccid_user_event_e event)
 {
     switch (event) {
         case APP_USBD_CCID_USER_EVT_RX_DONE:
-            // フレームデータをバッファに退避
-            memcpy(received_data + received_data_size, 
-                app_usbd_ccid_ep_output_buffer(), app_usbd_ccid_ep_output_buffer_size());
-            received_data_size += app_usbd_ccid_ep_output_buffer_size();
-
-            // APDUの処理を実行
-            apdu_received();
-            received_data_size = 0;
+            usbd_ccid_data_frame_received();
             break;
         default:
             break;
@@ -124,7 +235,16 @@ void usbd_ccid_init(void)
         NRF_LOG_ERROR("app_usbd_class_append(class_ccid) returns 0x%02x ", ret);
     }
     APP_ERROR_CHECK(ret);
-    
-    received_data_size = 0;
+
+    // モジュール変数を初期化
+    clear_value();
     NRF_LOG_DEBUG("usbd_ccid_init() done");
+}
+
+void usbd_ccid_send_data_frame(uint8_t *p_data, size_t size)
+{
+    app_usbd_ccid_ep_input_from_buffer(p_data, size);
+
+    NRF_LOG_DEBUG("usbd_ccid_send_data_frame(%d bytes)", size);
+    NRF_LOG_HEXDUMP_DEBUG(p_data, size);
 }
--- nRF5_SDK_v15.3.0/examples/diverta/plat_lib/usbd_service_ccid.h
+++ nRF5_SDK_v15.3.0/examples/diverta/plat_lib/usbd_service_ccid.h
@@ -11,7 +11,43 @@
 extern "C" {
 #endif
 
+//
+// CCID関連定義
+//
+#define CCID_CMD_HEADER_SIZE 10
+
+// コマンド (PC-->Reader)
+#define PC_TO_RDR_ICCPOWERON        0x62
+#define PC_TO_RDR_ICCPOWEROFF       0x63
+#define PC_TO_RDR_GETSLOTSTATUS     0x65
+#define PC_TO_RDR_XFRBLOCK          0x6F
+#define PC_TO_RDR_GETPARAMETERS     0x6C
+#define PC_TO_RDR_RESETPARAMETERS   0x6D
+#define PC_TO_RDR_SETPARAMETERS     0x61
+#define PC_TO_RDR_ESCAPE            0x6B
+#define PC_TO_RDR_ICCCLOCK          0x6E
+#define PC_TO_RDR_T0APDU            0x6A
+#define PC_TO_RDR_SECURE            0x69
+#define PC_TO_RDR_MECHANICAL        0x71
+#define PC_TO_RDR_ABORT             0x72
+#define PC_TO_RDR_SETDATARATEANDCLOCKFREQUENCY 0x73
+
+// コマンド (Reader-->PC)
+#define RDR_TO_PC_DATABLOCK         0x80
+#define RDR_TO_PC_SLOTSTATUS        0x81
+#define RDR_TO_PC_PARAMETERS        0x82
+#define RDR_TO_PC_ESCAPE            0x83
+#define RDR_TO_PC_DATARATEANDCLOCKFREQUENCY 0x84
+
+// エラーステータス
+#define SLOT_NO_ERROR               0x81
+#define SLOTERROR_CMD_NOT_SUPPORTED 0x00
+
+//
+// 関数群
+//
 void usbd_ccid_init(void);
+void usbd_ccid_send_data_frame(uint8_t *p_data, size_t size);
 
 #ifdef __cplusplus
 }
