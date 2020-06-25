82b29054040bf5c71dd41cedec8a3ce3a16eb06e
Makoto Morita <makmorit@outlook.jp>
2020-06-25 12:28:27 +0900

    CCID I/Fに関するソースコードを統合
    ・app_usbd_ccid_internal.hをapp_usbd_ccid.hに統合

--- nRF5_SDK_v15.3.0/examples/diverta/plat_lib/app_usbd_ccid.h
+++ nRF5_SDK_v15.3.0/examples/diverta/plat_lib/app_usbd_ccid.h
@@ -12,10 +12,76 @@ extern "C" {
 #include "app_usbd_class_base.h"
 #include "app_usbd.h"
 #include "app_usbd_core.h"
+#include "app_util.h"
 
 #include "ccid.h"
-#include "app_usbd_ccid_internal.h"
 
+//
+// app_usbd_ccid_internal
+//
+APP_USBD_CLASS_FORWARD(app_usbd_ccid);
+
+extern const app_usbd_class_methods_t app_usbd_ccid_class_methods;
+
+#define APP_USBD_CCID_DEFAULT_INTERVAL 0x00
+
+#define APP_USBD_CCID_CONFIG(iface_data, epin_data, epout_data) \
+        ((iface_data, epin_data, epout_data))
+
+#define APP_USBD_CCID_INST_CONFIG(user_event_handler, \
+                                  data_ifc,           \
+                                  data_ein,           \
+                                  data_eout,          \
+                                  ep_list)            \
+        .inst = {                                     \
+            .user_ev_handler = user_event_handler,    \
+            .data_interface  = data_ifc,              \
+            .data_epin       = data_ein,              \
+            .data_epout      = data_eout,             \
+            .p_ep_interval   = ep_list                \
+        }
+
+#define APP_USBD_CCID_GLOBAL_DEF_INTERNAL(instance_name,                     \
+                                          user_ev_handler,                   \
+                                          data_ifc,                          \
+                                          data_ein,                          \
+                                          data_eout)                         \
+        static uint8_t CONCAT_2(instance_name, _ep) = {                      \
+            (APP_USBD_CCID_DEFAULT_INTERVAL)};                               \
+        APP_USBD_CLASS_INST_GLOBAL_DEF(                                      \
+                instance_name,                                               \
+                app_usbd_ccid,                                               \
+                &app_usbd_ccid_class_methods,                                \
+                APP_USBD_CCID_CONFIG(data_ifc, data_ein, data_eout),         \
+                (APP_USBD_CCID_INST_CONFIG(user_ev_handler,                  \
+                                           data_ifc,                         \
+                                           data_ein,                         \
+                                           data_eout,                        \
+                                           &CONCAT_2(instance_name, _ep)))   \
+                )
+
+enum app_usbd_ccid_user_event_e;
+
+typedef void (*app_usbd_ccid_user_ev_handler_t)(
+    app_usbd_class_inst_t const *p_inst, enum app_usbd_ccid_user_event_e event);
+
+// CCID I/Fインスタンス情報
+typedef struct {
+    uint8_t  data_interface;
+    uint8_t  data_epout;
+    uint8_t  data_epin;
+    app_usbd_ccid_user_ev_handler_t user_ev_handler;
+    uint8_t *p_ep_interval;
+} app_usbd_ccid_inst_t;
+#define APP_USBD_CCID_INSTANCE_SPECIFIC_DEC app_usbd_ccid_inst_t inst;
+
+// CCID I/F共有情報
+//   現状、使用していません
+typedef struct {
+    uint8_t dummy;
+} app_usbd_ccid_ctx_t;
+#define APP_USBD_CCID_DATA_SPECIFIC_DEC app_usbd_ccid_ctx_t ctx;
+    
 /**
  * @brief CCID class instance type.
  *
