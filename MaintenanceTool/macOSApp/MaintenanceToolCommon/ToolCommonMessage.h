a6549f532e18a80c24fb1cd43fd387321495c500
Makoto Morita <makmorit@outlook.jp>
2020-07-15 17:16:37 +0900

    事前メッセージの表示処理
    ・表示させるメッセージ文言を追加

--- MaintenanceTool/macOSApp/MaintenanceToolCommon/ToolCommonMessage.h
+++ MaintenanceTool/macOSApp/MaintenanceToolCommon/ToolCommonMessage.h
@@ -103,6 +103,8 @@
 #define MSG_HCHK_U2F_AUTHENTICATE_COMMENT2  @"  FIDO認証器上のユーザー所在確認LEDが点滅したら、"
 #define MSG_HCHK_U2F_AUTHENTICATE_COMMENT3  @"  MAIN SWを１回押してください."
 #define MSG_HCHK_U2F_AUTHENTICATE_SUCCESS   @"U2F Authenticateが成功しました。"
+#define MSG_PROMPT_START_HCHK_BLE_AUTH      @"自動認証で使用するBLEデバイスを近づけてください。"
+#define MSG_COMMENT_START_HCHK_BLE_AUTH     @"BLE自動認証機能が有効化されている場合は、BLEデバイスによりユーザー所在確認を行います。\nスキャン対象サービスUUIDを持つBLEデバイスを始動させ、FIDO認証器に近づけてください。\n\nYesをクリックすると、ヘルスチェックを実行します。"
 
 #pragma mark - コマンドテスト関連メッセージ
 #define MSG_CMDTST_INVALID_NONCE            @"CTAPHID_INITコマンドが失敗しました。"
