3a390af01cb767defcc0b677ed3c2308c835b955
Makoto Morita <makmorit@outlook.jp>
2020-07-23 12:56:49 +0900

    リファクタリング
    ・仮実装部分と業務処理を分離するよう修正
    ・一部関数の名称と引数／戻り値を修正

--- CCID/ccid_lib/ccid_pin.c
+++ CCID/ccid_lib/ccid_pin.c
@@ -8,59 +8,62 @@
 
 #include "ccid_pin.h"
 
-// リトライカウンター
-static uint16_t default_retry_counter = PIN_DEFAULT_RETRY_CNT;
-static uint16_t retry_counter = PIN_DEFAULT_RETRY_CNT;
-
-// PINコードを保持
-static uint8_t m_pin_buffer[PIN_DEFAULT_SIZE];
+//
+// 仮の実装です。
+//
+// リトライカウンター／PINコードの永続化領域
+//
+static uint16_t m_default_retries;
+static uint16_t m_current_retries;
+static uint8_t  m_pin_buffer[PIN_DEFAULT_SIZE];
+//
+// 関数群
+//
+static bool save_pin_retries_default(uint8_t retries)
+{
+    m_default_retries = retries;
+    return true;
+}
+static bool save_pin_retries_current(uint8_t retries)
+{
+    m_current_retries = retries;
+    return true;
+}
+static bool restore_pin_retries(uint8_t *def, uint8_t *curr)
+{
+    // パラメーターチェック
+    if (def == NULL || curr == NULL) {
+        return false;
+    }
+    // 登録されているリトライカウンターを取得
+    *def = m_default_retries;
+    *curr = m_current_retries;
+    return true;
+}
+static bool save_pin_code(uint8_t *pin_buf, size_t pin_buf_size)
+{
+    // 登録されているPINを取得
+    memset(m_pin_buffer, 0xff, sizeof(m_pin_buffer));
+    memcpy(m_pin_buffer, pin_buf, pin_buf_size);
+    return true;
+}
+static bool restore_pin_code(uint8_t *pin_buf, size_t pin_buf_size)
+{
+    // 登録されているPINを取得
+    memset(pin_buf, 0xff, pin_buf_size);
+    memcpy(pin_buf, m_pin_buffer, pin_buf_size);
+    return true;
+}
+//
+// 仮の実装です（ここまで）
+//
 
 // 一時読込み用領域
 static uint8_t pin_buf[PIN_DEFAULT_SIZE];
 
 //
-// 仮の実装です。
+// 業務処理群
 //
-#define RETRY_ATTR 0
-#define DEFAULT_RETRY_ATTR 1
-static int read_attr(const char *path, uint8_t attr, void *buf, size_t len) 
-{
-    if (attr == DEFAULT_RETRY_ATTR) {
-        memcpy(buf, &default_retry_counter, sizeof(default_retry_counter));
-    } else {
-        memcpy(buf, &retry_counter, sizeof(retry_counter));
-    }
-    return 0;
-}
-static int write_attr(const char *path, uint8_t attr, const void *buf, size_t len) 
-{
-    if (attr == RETRY_ATTR) {
-        memcpy(&retry_counter, buf, sizeof(retry_counter));
-    }
-    return 0;
-}
-static int read_file(void *buf, size_t len) 
-{
-    memset(m_pin_buffer, 0xff, sizeof(m_pin_buffer));
-    memcpy(buf, m_pin_buffer, len);
-    return len;
-}
-static int write_file(const char *path, const void *buf, size_t off, size_t len, uint8_t trunc) 
-{
-    memset(m_pin_buffer, 0xff, sizeof(m_pin_buffer));
-    memcpy(m_pin_buffer, buf, len);
-    return 0;
-}
-
-static bool restore_pin_code(uint8_t *pin_buf, size_t pin_buf_size)
-{
-    // 登録されているPINを取得
-    if (read_file(pin_buf, pin_buf_size) < 0) {
-        return false;
-    }
-    return true;
-}
-
 static bool pin_code_is_equal(const void *buf, uint8_t len, bool *is_equal)
 {
     // パラメーターチェック
@@ -115,16 +118,16 @@ bool ccid_pin_verify(const void *buf, uint8_t len, uint8_t *retries, bool *auth_
         return false;
     }
 
-    // リトライカウンターの現在値を参照
-    uint8_t ctr;
-    int err = read_attr(NULL, RETRY_ATTR, &ctr, sizeof(ctr));
-    if (err < 0) {
+    // リトライカウンターのデフォルト／現在値を参照
+    uint8_t default_cnt;
+    uint8_t current_cnt;
+    if (restore_pin_retries(&default_cnt, &current_cnt) == false) {
         return false;
     }
-    *retries = ctr;
+    *retries = current_cnt;
 
     // リトライカウンターが０であれば認証失敗
-    if (ctr == 0) {
+    if (current_cnt == 0) {
         *auth_failed = true;
         return true;
     }
@@ -136,11 +139,9 @@ bool ccid_pin_verify(const void *buf, uint8_t len, uint8_t *retries, bool *auth_
     }
     if (is_equal == false) {
         // NGの場合はリトライカウンターを１減らす
-        --ctr;
-        *retries = ctr;
-        // 内部で保持しているリトライカウンターを更新
-        err = write_attr(NULL, RETRY_ATTR, &ctr, sizeof(ctr));
-        if (err < 0) {
+        *retries = --current_cnt;
+        // 現在のリトライカウンターを更新
+        if (save_pin_retries_current(current_cnt) == false) {
             return false;
         }
         *auth_failed = true;
@@ -148,12 +149,7 @@ bool ccid_pin_verify(const void *buf, uint8_t len, uint8_t *retries, bool *auth_
     }
 
     // 認証が成功したら、リトライカウンターをデフォルト値に再設定
-    err = read_attr(NULL, DEFAULT_RETRY_ATTR, &ctr, sizeof(ctr));
-    if (err < 0) {
-        return false;
-    }
-    err = write_attr(NULL, RETRY_ATTR, &ctr, sizeof(ctr));
-    if (err < 0) {
+    if (save_pin_retries_current(default_cnt) == false) {
         return false;
     }
     *auth_failed = false;
@@ -177,12 +173,15 @@ bool ccid_pin_get_retries(uint8_t *retries)
         return true;
     }
 
-    // リトライカウンターの現在値を参照
-    int err = read_attr(NULL, RETRY_ATTR, retries, sizeof(uint8_t));
-    if (err < 0) {
+    // リトライカウンターのデフォルト／現在値を参照
+    uint8_t default_cnt;
+    uint8_t current_cnt;
+    if (restore_pin_retries(&default_cnt, &current_cnt) == false) {
         return false;
     }
 
+    // リトライカウンターの現在地を設定して戻す
+    *retries = current_cnt;
     return true;
 }
 
@@ -192,20 +191,17 @@ bool ccid_pin_get_retries(uint8_t *retries)
 bool ccid_pin_create(const void *buf, uint8_t len, uint8_t max_retries) 
 {
     // PINを設定
-    int err = write_file(NULL, buf, 0, len, 1);
-    if (err < 0) {
+    if (save_pin_code((uint8_t *)buf, len) == false) {
         return false;
     }
 
     // 内部保持用のリトライカウンターを設定
-    err = write_attr(NULL, RETRY_ATTR, &max_retries, sizeof(max_retries));
-    if (err < 0) {
+    if (save_pin_retries_current(max_retries) == false) {
         return false;
     }
 
     // デフォルトのリトライカウンターを設定
-    err = write_attr(NULL, DEFAULT_RETRY_ATTR, &max_retries, sizeof(max_retries));
-    if (err < 0) {
+    if (save_pin_retries_default(max_retries) == false) {
         return false;
     }
 
