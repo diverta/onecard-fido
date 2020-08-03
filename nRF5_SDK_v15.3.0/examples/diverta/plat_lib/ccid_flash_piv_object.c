3e7ff61098e85e7f19156dc8bd0f8cea2eda7f2f
Makoto Morita <makmorit@outlook.jp>
2020-08-03 17:29:22 +0900

    永続化処理の実装
    ・PIVデータオブジェクトを永続化するための専用モジュールを新設
    ・中身はこれから実装予定です。

---
+++ nRF5_SDK_v15.3.0/examples/diverta/plat_lib/ccid_flash_piv_object.c
@@ -0,0 +1,7 @@
+/* 
+ * File:   ccid_flash_piv_object.c
+ * Author: makmorit
+ *
+ * Created on 2020/08/03, 17:23
+ */
+#include "ccid_flash_piv_object.h"
---
+++ nRF5_SDK_v15.3.0/examples/diverta/plat_lib/ccid_flash_piv_object.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   ccid_flash_piv_object.h
+ * Author: makmorit
+ *
+ * Created on 2020/08/03, 17:23
+ */
+#ifndef CCID_FLASH_PIV_OBJECT_H
+#define CCID_FLASH_PIV_OBJECT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CCID_FLASH_PIV_OBJECT_H */
