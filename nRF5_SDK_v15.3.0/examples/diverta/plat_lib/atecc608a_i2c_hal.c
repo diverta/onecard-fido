7cdc7b18d09c5775c8853ecd6080975acb7911b3
Makoto Morita <makmorit@outlook.jp>
2020-08-12 11:12:08 +0900

    事前リファクタリング
    ・命名規約を、cryptoauthlibのものから、オリジナルの規約に変更
    ・fido_twi_init の戻り値を参照し、falseの場合処理を中止するよう修正

--- nRF5_SDK_v15.3.0/examples/diverta/plat_lib/atecc608a_i2c_hal.c
+++ nRF5_SDK_v15.3.0/examples/diverta/plat_lib/atecc608a_i2c_hal.c
@@ -19,7 +19,7 @@ NRF_LOG_MODULE_REGISTER();
 //
 // ATECC608A HAL関連
 //
-#include "hal/atca_hal.h"
+#include "atecc_iface.h"
 
 // HAL structure, use this to store data
 typedef struct {
@@ -34,28 +34,31 @@ typedef struct {
 
 static i2c_hal_data_t m_hal_data;
 
-ATCA_STATUS hal_i2c_init(void *hal, ATCAIfaceCfg *cfg) 
+ATECC_STATUS hal_i2c_init(ATECC_IFACE iface) 
 {
-    fido_twi_init();
+    if (fido_twi_init() == false) {
+        return ATECC_COMM_FAIL;
+    }
 
+    ATECC_IFACE_CFG *cfg = iface->mIfaceCFG;
     m_hal_data.active = true;
-    m_hal_data.slave_address = cfg->atcai2c.slave_address;
-    m_hal_data.bus = cfg->atcai2c.bus;
-    m_hal_data.baud = cfg->atcai2c.baud;
+    m_hal_data.slave_address = cfg->i2c.slave_address;
+    m_hal_data.bus = cfg->i2c.bus;
+    m_hal_data.baud = cfg->i2c.baud;
     m_hal_data.wake_delay = cfg->wake_delay;
     m_hal_data.rx_retries = cfg->rx_retries;
     m_hal_data.p_instance = (nrf_drv_twi_t const *)fido_twi_instance_ref();
-    ((ATCAHAL_t*)hal)->hal_data = &m_hal_data;
+    iface->hal_data = &m_hal_data;
 
-    return ATCA_SUCCESS;
+    return ATECC_SUCCESS;
 }
 
-ATCA_STATUS hal_i2c_post_init(ATCAIface iface) {
-    UNUSED_PARAMETER(iface);
-    return ATCA_SUCCESS;
+ATECC_STATUS hal_i2c_post_init(ATECC_IFACE iface) {
+    (void)iface;
+    return ATECC_SUCCESS;
 }
 
-static uint8_t get_twi_address(ATCAIface iface)
+static uint8_t get_twi_address(ATECC_IFACE iface)
 {
     // 7bitアドレスを取得
     i2c_hal_data_t *hal_data = (i2c_hal_data_t *)(iface->hal_data);
@@ -63,20 +66,20 @@ static uint8_t get_twi_address(ATCAIface iface)
     return address;
 }
 
-ATCA_STATUS hal_i2c_send(ATCAIface iface, uint8_t *data, int length) 
+ATECC_STATUS hal_i2c_send(ATECC_IFACE iface, uint8_t *data, int length) 
 {
     // 先頭バイトを差替えて送信
     data[0] = 0x3;
     length++;
     if (fido_twi_write(get_twi_address(iface), data, length) == false) {
         NRF_LOG_ERROR("hal_i2c_send failed");
-        return ATCA_TX_FAIL;
+        return ATECC_TX_FAIL;
     }
 
-    return ATCA_SUCCESS;
+    return ATECC_SUCCESS;
 }
 
-ATCA_STATUS hal_i2c_receive(ATCAIface iface, uint8_t *rx_data, uint16_t *rx_length) 
+ATECC_STATUS hal_i2c_receive(ATECC_IFACE iface, uint8_t *rx_data, uint16_t *rx_length) 
 {
     // read procedure is:
     // 1. read 1 byte, this will be the length of the package
@@ -90,21 +93,21 @@ ATCA_STATUS hal_i2c_receive(ATCAIface iface, uint8_t *rx_data, uint16_t *rx_leng
     }
     if (r == false) {
         NRF_LOG_ERROR("hal_i2c_receive failed: length read timeout");
-        return ATCA_RX_TIMEOUT;
+        return ATECC_RX_TIMEOUT;
     }
 
     // NACKの場合（応答データが受信できなかった場合）
     if (length_package[0] == 0) {
         NRF_LOG_WARNING("hal_i2c_receive: NACK has occurred");
         *rx_length = 0;
-        return ATCA_SUCCESS;
+        return ATECC_SUCCESS;
     }
     
     // データの１バイト目に、受信できるバイト数が格納されています
     uint8_t bytes_to_read = length_package[0] - 1;
     if (bytes_to_read > *rx_length) {
         NRF_LOG_ERROR("hal_i2c_receive buffer too small, requested %u, but have %u", bytes_to_read, *rx_length);
-        return ATCA_SMALL_BUFFER;
+        return ATECC_SMALL_BUFFER;
     }
 
     // データ長を格納
@@ -119,14 +122,14 @@ ATCA_STATUS hal_i2c_receive(ATCAIface iface, uint8_t *rx_data, uint16_t *rx_leng
     }
     if (r == false) {
         NRF_LOG_ERROR("hal_i2c_receive failed: package read timeout");
-        return ATCA_RX_TIMEOUT;
+        return ATECC_RX_TIMEOUT;
     }
     *rx_length = length_package[0];
 
-    return ATCA_SUCCESS;
+    return ATECC_SUCCESS;
 }
 
-ATCA_STATUS hal_i2c_wake(ATCAIface iface) 
+ATECC_STATUS hal_i2c_wake(ATECC_IFACE iface) 
 {
     // steps to wake the chip up...
     // 1. switch to 100KHz
@@ -135,7 +138,7 @@ ATCA_STATUS hal_i2c_wake(ATCAIface iface)
 
     // 3. Wait for wake_delay
     i2c_hal_data_t *hal_data = (i2c_hal_data_t*)(iface->hal_data);
-    nrf_delay_us(hal_data->wake_delay);
+    atecc_delay_us(hal_data->wake_delay);
 
     // 4. Read from normal slave_address
     uint8_t rx_buffer[4] = {0};
@@ -145,8 +148,8 @@ ATCA_STATUS hal_i2c_wake(ATCAIface iface)
         r = fido_twi_read(get_twi_address(iface), rx_buffer, sizeof(rx_buffer));
     }
     if (r == false) {
-        NRF_LOG_ERROR("hal_i2c_wake failed: ATCA_RX_TIMEOUT");
-        return ATCA_RX_TIMEOUT;
+        NRF_LOG_ERROR("hal_i2c_wake failed: read timeout");
+        return ATECC_RX_TIMEOUT;
     }
 
     // 5. Set frequency back to requested one
@@ -154,65 +157,75 @@ ATCA_STATUS hal_i2c_wake(ATCAIface iface)
     const uint8_t selftest_fail_resp[4] = {0x04, 0x07, 0xC4, 0x40};
 
     if (memcmp(rx_buffer, expected_response, sizeof(expected_response)) == 0) {
-        return ATCA_SUCCESS;
+        return ATECC_SUCCESS;
 
     } else if (memcmp(rx_buffer, selftest_fail_resp, sizeof(selftest_fail_resp)) == 0) {
         NRF_LOG_ERROR("hal_i2c_wake failed: selftest error");
-        return ATCA_STATUS_SELFTEST_ERROR;
+        return ATECC_STATUS_SELFTEST_ERROR;
 
     } else {
         NRF_LOG_ERROR("hal_i2c_wake failed: unknown error");
-        return ATCA_WAKE_FAILED;
+        return ATECC_WAKE_FAILED;
     }
 }
 
-ATCA_STATUS hal_i2c_idle(ATCAIface iface) 
+ATECC_STATUS hal_i2c_idle(ATECC_IFACE iface) 
 {
     // idle word address value
     uint8_t buffer[1] = {0x2};
     fido_twi_write(get_twi_address(iface), buffer, sizeof(buffer));
 
-    return ATCA_SUCCESS;
+    return ATECC_SUCCESS;
 }
 
-ATCA_STATUS hal_i2c_sleep(ATCAIface iface) 
+ATECC_STATUS hal_i2c_sleep(ATECC_IFACE iface) 
 {
     // sleep word address value
     uint8_t buffer[1] = {0x1};
     fido_twi_write(get_twi_address(iface), buffer, sizeof(buffer));
 
-    return ATCA_SUCCESS;
+    return ATECC_SUCCESS;
 }
 
-ATCA_STATUS hal_i2c_release(void *hal_data) {
+ATECC_STATUS hal_i2c_release(void *hal_data) {
     UNUSED_PARAMETER(hal_data);
-    return ATCA_SUCCESS;
+    return ATECC_SUCCESS;
 }
 
-ATCA_STATUS hal_i2c_discover_buses(int i2c_buses[], int max_buses) {
-    UNUSED_PARAMETER(i2c_buses);
-    UNUSED_PARAMETER(max_buses);
-    return ATCA_UNIMPLEMENTED;
+ATECC_STATUS hal_iface_init(ATECC_IFACE iface)
+{
+    if (iface == NULL) {
+        return ATECC_COMM_FAIL;
+    }
+
+    iface->init_func     = &hal_i2c_init;
+    iface->postinit_func = &hal_i2c_post_init;
+    iface->send_func     = &hal_i2c_send;
+    iface->receive_func  = &hal_i2c_receive;
+    iface->wake_func     = &hal_i2c_wake;
+    iface->sleep_func    = &hal_i2c_sleep;
+    iface->idle_func     = &hal_i2c_idle;
+    iface->hal_data      = NULL;
+
+    return ATECC_SUCCESS;
 }
 
-ATCA_STATUS hal_i2c_discover_devices(int bus_num, ATCAIfaceCfg *cfg, int *found) {
-    UNUSED_PARAMETER(bus_num);
-    UNUSED_PARAMETER(cfg);
-    UNUSED_PARAMETER(found);
-    return ATCA_UNIMPLEMENTED;
+ATECC_STATUS hal_iface_release(void *hal_data)
+{
+    return hal_i2c_release(hal_data);
 }
 
-void atca_delay_us(uint32_t delay)
+void atecc_delay_us(uint32_t delay)
 {
     nrf_delay_us(delay);
 }
 
-void atca_delay_10us(uint32_t delay)
+void atecc_delay_10us(uint32_t delay)
 {
     nrf_delay_us(delay * 10);
 }
 
-void atca_delay_ms(uint32_t delay)
+void atecc_delay_ms(uint32_t delay)
 {
     nrf_delay_ms(delay);
 }
