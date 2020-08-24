2131512815b90bb07e7d2899ced69c664551ffe6
Makoto Morita <makmorit@outlook.jp>
2020-08-24 15:05:45 +0900

    モジュール追加
    ・専用モジュール atecc_aes.c を新設

---
+++ SECUREIC/atecc_lib/atecc_aes.c
@@ -0,0 +1,15 @@
+/* 
+ * File:   atecc_aes.c
+ * Author: makmorit
+ *
+ * Created on 2020/08/24, 14:58
+ */
+#include "atecc_aes.h"
+
+// 業務処理／HW依存処理間のインターフェース
+#include "fido_platform.h"
+
+void atecc_aes_test(void)
+{
+    fido_log_info("atecc_aes_test done");
+}
---
+++ SECUREIC/atecc_lib/atecc_aes.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   atecc_aes.h
+ * Author: makmorit
+ *
+ * Created on 2020/08/24, 14:58
+ */
+#ifndef ATECC_AES_H
+#define ATECC_AES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//
+// 関数群
+//
+void atecc_aes_test(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ATECC_AES_H */
