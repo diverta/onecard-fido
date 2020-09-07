b44095f1bc868945f022110c43fcf3979c5f02f5
Makoto Morita <makmorit@outlook.jp>
2020-09-07 16:41:31 +0900

    RTCCモジュールの評価
    ・RTC2を始動させる処理を実装
    ・実機確認済み
    （１秒ごとにイベントが発生し、rtc2_handler が呼び出されます）

--- nRF5_SDK_v15.3.0/examples/diverta/fido2_authenticator_app/main.c
+++ nRF5_SDK_v15.3.0/examples/diverta/fido2_authenticator_app/main.c
@@ -34,6 +34,9 @@
 // for FDS event handle
 #include "fido_flash.h"
 
+// for RTCC
+#include "rtc2.h"
+
 #define APP_BLE_CONN_CFG_TAG                1           /**< A tag identifying the SoftDevice BLE configuration. */
 #define APP_BLE_OBSERVER_PRIO               3           /**< Application's BLE observer priority. You shouldn't need to modify this value. */
 #define DEAD_BEEF                           0xDEADBEEF  /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
@@ -196,6 +199,7 @@ int main(void)
     timers_init();
     power_management_init();
     flash_storage_init();
+    rtc2_init();
 
     // BLE関連の初期化
     ble_stack_init();
