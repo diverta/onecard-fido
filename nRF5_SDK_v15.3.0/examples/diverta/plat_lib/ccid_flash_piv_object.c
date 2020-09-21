21c76f990663ed74d215ae2c56af7510d65804f4
Makoto Morita <makmorit@outlook.jp>
2020-09-21 13:21:10 +0900

    Flash ROMレコードの可変長化対応
    ・パスワード管理関連の処理を、PIVオブジェクト汎用Read/Write関数で置き換え
    ・ヘッダーファイル内の不要な定義を削除

--- nRF5_SDK_v15.3.0/examples/diverta/plat_lib/ccid_flash_piv_object.c
+++ nRF5_SDK_v15.3.0/examples/diverta/plat_lib/ccid_flash_piv_object.c
@@ -257,6 +257,19 @@ static bool write_piv_object_data_to_fds(uint8_t obj_tag, uint8_t obj_alg, uint8
     }
 }
 
+static void copy_object_data_from_buffer(uint8_t *obj_alg, uint8_t *obj_data_buf, size_t *obj_data_size)
+{
+    // オブジェクトの属性を取得
+    uint8_t *rec_bytes = ccid_flash_piv_object_read_buffer();
+    uint32_t size32_t;
+    *obj_alg = rec_bytes[1];
+    memcpy(&size32_t, rec_bytes + 4, sizeof(uint32_t));
+
+    // データを引数の領域にコピー
+    memcpy(obj_data_buf, rec_bytes + 8, size32_t);
+    *obj_data_size = size32_t;
+}
+
 //
 // 管理用パスワード関連
 //
@@ -264,58 +277,28 @@ bool ccid_flash_piv_object_card_admin_key_read(uint8_t *key, size_t *key_size, u
 {
     // Flash ROMから既存データを読込み、
     // 既存データがあれば、データをバッファに読込む
-    if (fido_flash_fds_record_read(PIV_DATA_OBJ_9B_FILE_ID, PIV_DATA_OBJ_9B_RECORD_KEY, PIV_DATA_OBJ_9B_RECORD_SIZE, m_record_buf_R, is_exist) == false) {
+    uint8_t key_tag = TAG_CERT_KEYMN;
+    if (read_piv_object_data_from_fds(key_tag, *key_alg, is_exist) == false) {
         return false;
     }
     // 既存データがなければここで終了
     if (*is_exist == false) {
         return true;
     }
-
-    // Flash ROM書込み用データの一時格納領域
-    //   レコードサイズ = 9 ワード
-    //     パスワード: 8ワード（32バイト)
-    //     属性データ: 1ワード（4バイト）
-    //       0    : パスワード長（1バイト）
-    //       1    : パスワードのアルゴリズム（1バイト）
-    //       2 - 3: 予備（2バイト）
-    uint8_t *rec_bytes = (uint8_t *)m_record_buf_R;
-    uint8_t size = rec_bytes[32];
-    uint8_t alg  = rec_bytes[33];
-
-    if (size == 0xff || alg == 0xff) {
-        // データが不正な場合は終了
-        return false;
-    }
-
-#if LOG_HEXDUMP_DEBUG
-    NRF_LOG_DEBUG("Management key from Flash ROM (%d bytes) alg=0x%02x", size, alg);
-    NRF_LOG_HEXDUMP_DEBUG(rec_bytes, size);
-#endif
-
     // データを引数の領域にコピー
-    *key_size = size;
-    *key_alg  = alg;
-    memcpy(key, rec_bytes, size);
+    copy_object_data_from_buffer(key_alg, key, key_size);
     return true;
 }
 
 bool ccid_flash_piv_object_card_admin_key_write(uint8_t *key, size_t key_size, uint8_t key_alg)
 {
-    // 引数のデータを、Flash ROM書込み用データの一時格納領域にコピー
-    uint8_t *rec_bytes = (uint8_t *)m_record_buf_W;
-    rec_bytes[32] = (uint8_t)key_size;
-    rec_bytes[33] = key_alg;
-    memcpy(rec_bytes, key, key_size);
+    bool is_exist;
+    uint8_t key_tag = TAG_CERT_KEYMN;
 
-#if LOG_HEXDUMP_DEBUG
-    NRF_LOG_DEBUG("Management key to Flash ROM (%d bytes) alg=0x%02x", key_size, key_alg);
-    NRF_LOG_HEXDUMP_DEBUG(key, key_size);
-#endif
-
-    // データをFlash ROMに書込
+    // 引数のデータを、Flash ROM書込み用データの一時格納領域にコピーし、
+    // Flash ROMに書込
     m_flash_func = (void *)ccid_flash_piv_object_card_admin_key_write;
-    return fido_flash_fds_record_write(PIV_DATA_OBJ_9B_FILE_ID, PIV_DATA_OBJ_9B_RECORD_KEY, PIV_DATA_OBJ_9B_RECORD_SIZE, m_record_buf_R, m_record_buf_W);
+    return write_piv_object_data_to_fds(key_tag, key_alg, key, key_size, &is_exist);
 }
 
 //
