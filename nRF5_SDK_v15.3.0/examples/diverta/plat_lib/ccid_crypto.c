3e040b87dd857562c01597e25a3e38ae2c617f1a
Makoto Morita <makmorit@outlook.jp>
2020-11-12 10:05:36 +0900

    RSA-2048認証対応
    ・RSA-2048認証関連のモジュールを新設

---
+++ nRF5_SDK_v15.3.0/examples/diverta/plat_lib/ccid_crypto.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   ccid_crypto.c
+ * Author: makmorit
+ *
+ * Created on 2020/11/12, 9:32
+ */
+#include "ccid_crypto.h"
+
+bool ccid_crypto_rsa_private(uint8_t *rsa_private_key_raw, uint8_t *input, uint8_t *output)
+{
+    //
+    // 仮の実装です。
+    //
+    (void)rsa_private_key_raw;
+    (void)input;
+    (void)output;
+
+    return false;
+}
