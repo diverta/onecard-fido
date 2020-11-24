16a44a5bc20fbf21ab60d65b4d179fff03b30404
Makoto Morita <makmorit@outlook.jp>
2020-11-24 09:37:33 +0900

    PIN認証コマンドの組込み
    ・PIN認証コマンドを受け入れることができるよう修正
    ・PIN番号がNULLの時（＝リトライカウンター照会時）もコマンドが実行できるよう実装
    ・PIN認証時は必ずPIVアプレットSELECTを通すように実装
    ・呼び出し元を、メイン画面ではなく、他の画面でも対応できるよう修正
    （delegateオブジェクトのid参照管理とします）
    ・画面に制御を戻す部分は、仮の実装となっています。

--- MaintenanceTool/macOSApp/CCID/ToolCCIDCommon.h
+++ MaintenanceTool/macOSApp/CCID/ToolCCIDCommon.h
@@ -8,5 +8,8 @@
 #define ToolCCIDCommon_h
 
 #define PIV_INS_SELECT_APPLICATION  0xa4
+#define PIV_INS_VERIFY              0x20
+
+#define SW_SUCCESS                  0x9000
 
 #endif /* ToolCCIDCommon_h */
