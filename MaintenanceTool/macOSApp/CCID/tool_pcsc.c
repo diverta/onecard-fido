7d9d1caa261938ade2dc57c5fc8a7af0266f40d8
Makoto Morita <makmorit@outlook.jp>
2020-12-10 12:23:24 +0900

    CCID I/F操作専用モジュールの新設
    ・１フレーム分の送受信処理を追加実装

--- MaintenanceTool/macOSApp/CCID/tool_pcsc.c
+++ MaintenanceTool/macOSApp/CCID/tool_pcsc.c
@@ -5,6 +5,7 @@
 //  Created by Makoto Morita on 2020/12/09.
 //
 #include <stdlib.h>
+#include <string.h>
 #include <PCSC/winscard.h>
 
 #include "debug_log.h"
@@ -16,6 +17,13 @@ static SCARDHANDLE  m_card;
 
 // 接続先のSCardスロット名を保持
 static const char *m_slot_name = "Diverta Inc. Secure Dongle";
+
+// 送受信APDUを保持
+static uint8_t  command_apdu[264];
+static uint32_t command_apdu_size;
+static uint8_t  response_apdu[264];
+static uint32_t response_apdu_size;
+static uint16_t response_sw;
 
 const char *tool_pcsc_scard_slot_name(void)
 {
@@ -103,4 +111,49 @@ void tool_pcsc_scard_disconnect(void)
         SCardReleaseContext(m_context);
     }
     tool_pcsc_scard_init();
+}
+
+void tool_pcsc_scard_set_command_apdu(uint8_t cla, uint8_t ins, uint8_t p1, uint8_t p2, uint8_t lc, uint8_t *data, uint16_t le)
+{
+    // 送信APDUを生成
+    command_apdu[0] = cla;
+    command_apdu[1] = ins;
+    command_apdu[2] = p1;
+    command_apdu[3] = p2;
+    command_apdu[4] = lc;
+    memcpy(command_apdu + 5, data, lc);
+    // 送信APDUサイズを設定
+    command_apdu_size = lc + 5;
+    // 受信APDUサイズを設定
+    response_apdu_size = le;
+}
+
+bool tool_pcsc_scard_send_command_apdu(void)
+{
+    // データを送信（command_apduには、CLA/INS/P1/P2/Lcの５バイトを含むものとします）
+    uint32_t size = response_apdu_size;
+    int32_t rc = SCardTransmit(m_card, SCARD_PCI_T1, command_apdu, command_apdu_size, NULL, response_apdu, &size);
+    if (rc != SCARD_S_SUCCESS) {
+        log_debug("SCardTransmit failed for '%s', rc=0x%08x", m_slot_name, rc);
+        response_sw = 0;
+        response_apdu_size = 0;
+        return false;
+    }
+    // ステータスワード、受信サイズ長（ステータスワードの２バイトを除く）を設定
+    if (size >= 2) {
+        response_sw = (response_apdu[size - 2] << 8) | response_apdu[size - 1];
+        response_apdu_size = size - 2;
+    } else {
+        response_sw = 0;
+        response_apdu_size = 0;
+    }
+    return true;
+}
+
+uint8_t *tool_pcsc_scard_response_apdu_data(size_t *apdu_data_size, uint16_t *sw)
+{
+    // 受信データサイズ、ステータスワード、受信データの参照を戻す
+    *sw = response_sw;
+    *apdu_data_size = (size_t)response_apdu_size;
+    return response_apdu;
 }
--- MaintenanceTool/macOSApp/CCID/tool_pcsc.h
+++ MaintenanceTool/macOSApp/CCID/tool_pcsc.h
@@ -16,5 +16,8 @@ bool             tool_pcsc_scard_connect(void);
 bool             tool_pcsc_scard_begin_transaction(void);
 void             tool_pcsc_scard_end_transaction(void);
 void             tool_pcsc_scard_disconnect(void);
+void             tool_pcsc_scard_set_command_apdu(uint8_t cla, uint8_t ins, uint8_t p1, uint8_t p2, uint8_t lc, uint8_t *data, uint16_t le);
+bool             tool_pcsc_scard_send_command_apdu(void);
+uint8_t         *tool_pcsc_scard_response_apdu_data(size_t *apdu_data_size, uint16_t *sw);
 
 #endif /* tool_pcsc_h */
