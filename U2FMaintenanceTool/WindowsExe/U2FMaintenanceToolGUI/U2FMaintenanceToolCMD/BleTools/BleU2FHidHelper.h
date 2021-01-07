8f5cd8727f60e1f331e3f756e998d14722783b4c
Makoto Morita <makmorit@outlook.jp>
2021-01-07 12:11:07 +0900

    リソースの整理
    ・不要リソースを削除

--- U2FMaintenanceTool/WindowsExe/U2FMaintenanceToolGUI/U2FMaintenanceToolCMD/BleTools/BleU2FHidHelper.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BLE_U2F_HID_HELPER_H_
-#define _BLE_U2F_HID_HELPER_H_
-
-#include "BleApi.h"
-
-extern bool BleU2FHidHelper_ProcessXferMessage(char *recv_hid_message, pBleDevice dev);
-
-#endif /* _BLE_U2F_HID_HELPER_H_ */
