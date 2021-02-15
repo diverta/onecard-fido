45472ff113634adbc81be623a9dcb7b6f1ba9ee8
Makoto Morita <makmorit@outlook.jp>
2021-02-15 12:05:02 +0900

    リトライカウンター永続化対応
    ・ccid_flash_object.c の新設
    （PIVと共通利用できる処理を外出しするための措置）

---
+++ nRF52840_app/examples/diverta/plat_lib/ccid_flash_object.c
@@ -0,0 +1,6 @@
+/* 
+ * File:   ccid_flash_object.c
+ * Author: makmorit
+ *
+ * Created on 2021/02/15, 11:58
+ */
---
+++ nRF52840_app/examples/diverta/plat_lib/ccid_flash_object.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   ccid_flash_object.h
+ * Author: makmorit
+ *
+ * Created on 2021/02/15, 11:58
+ */
+#ifndef CCID_FLASH_OBJECT_H
+#define CCID_FLASH_OBJECT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CCID_FLASH_OBJECT_H */
