035fa2ab2c3db7dab4b1df51bbd8ba5bfdb380c6
Makoto Morita <makmorit@outlook.jp>
2021-04-07 12:34:08 +0900

    Flash ROM関連処理の実装
    ・Flash ROMのデータ管理処理を、NVSではなく、LittleFSとするよう修正
    （NVSは機能が貧弱すぎるため、採用を見送りました）
    ・重複しているconfigを削除

--- pyvenvs/ncs/nRF5340_app/secure_device_app/src/app_flash.c
+++ pyvenvs/ncs/nRF5340_app/secure_device_app/src/app_flash.c
@@ -9,17 +9,30 @@
 #include <device.h>
 #include <drivers/flash.h>
 #include <storage/flash_map.h>
-#include <fs/nvs.h>
-
-// nvs file system
-static struct nvs_fs            fs;
-static struct flash_pages_info  info;
+#include <fs/fs.h>
+#include <fs/littlefs.h>
 
 #include "app_flash.h"
 
 #define LOG_LEVEL LOG_LEVEL_DBG
 #include <logging/log.h>
 LOG_MODULE_REGISTER(app_flash);
+
+// 初期動作確認用
+#define WIPE_STORAGE_ON_INIT        false
+
+//
+// LittleFS file system
+//
+#define PARTITION_NODE DT_NODELABEL(lfs1)
+
+FS_LITTLEFS_DECLARE_DEFAULT_CONFIG(storage);
+static struct fs_mount_t lfs_storage_mnt = {
+    .type = FS_LITTLEFS,
+    .fs_data = &storage,
+    .storage_dev = (void *)FLASH_AREA_ID(storage),
+    .mnt_point = "/lfs",
+};
 
 //
 // アプリケーション状態を保持
@@ -28,27 +41,34 @@ static bool flash_initialized = false;
 
 void app_flash_initialize(void)
 {
-    // Flash ROMエリアの開始位置、ページサイズを取得
-    fs.offset = FLASH_AREA_OFFSET(storage);
-    const struct device *dev = device_get_binding(DT_CHOSEN_ZEPHYR_FLASH_CONTROLLER_LABEL);
-    int rc = flash_get_page_info_by_offs(dev, fs.offset, &info);
-    if (rc != 0) {
-        LOG_ERR("Unable to get page info (flash_get_page_info_by_offs returns %d)", rc);
+    // マウントポイントを取得
+    struct fs_mount_t *mp = &lfs_storage_mnt;
+
+    // マウントポイントがあるFlash領域のIDを取得
+    unsigned int id = (uintptr_t)mp->storage_dev;
+    const struct flash_area *pfa;
+
+    // Flash領域を開く
+    int rc = flash_area_open(id, &pfa);
+    if (rc < 0) {
+        LOG_ERR("Unable to find flash area %u (flash_area_open returns %d)", id, rc);
         return;
     }
 
-    // NVSファイルシステムのセクターサイズ／数を設定
-    fs.sector_size = info.size;
-    fs.sector_count = 2;
-
-    // NVSファイルシステムを初期化
-    rc = nvs_init(&fs, DT_CHOSEN_ZEPHYR_FLASH_CONTROLLER_LABEL);
-    if (rc != 0) {
-        LOG_ERR("Flash initialize failed (nvs_init returns %d)", rc);
+#if WIPE_STORAGE_ON_INIT
+    // Flash領域を消去
+    rc = flash_area_erase(pfa, 0, pfa->fa_size);
+    if (rc < 0) {
+        LOG_ERR("Unable to erase flash area %u (flash_area_erase returns %d)", id, rc);
         return;
     }
+    LOG_INF("Erasing flash area end (rc=%d)", rc);
+#endif
+
+    // Flash ROM領域を閉じる
+    LOG_INF("Flash initialized (offset=0x%08x, size=%u)", (unsigned int)pfa->fa_off, (unsigned int)pfa->fa_size);
+    flash_area_close(pfa);
 
     // 初期化完了
-    LOG_DBG("Flash initialized (offset=0x%08x, sector size=%d, sector count=%d)", fs.offset, fs.sector_size, fs.sector_count);
     flash_initialized = true;
 }
