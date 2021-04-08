d167bd5dcabad9d6257d74ece2f478b6701c05ef
Makoto Morita <makmorit@outlook.jp>
2021-04-08 17:48:24 +0900

    タイマー関連処理の実装
    ・専用モジュールを新設
    ・スレッド間の排他制御は後日追加実装予定

---
+++ pyvenvs/ncs/nRF5340_app/secure_device_app/src/app_timer.c
@@ -0,0 +1,108 @@
+/* 
+ * File:   app_timer.c
+ * Author: makmorit
+ *
+ * Created on 2021/04/07, 15:04
+ */
+#include <zephyr/types.h>
+#include <zephyr.h>
+#include <device.h>
+#include <drivers/counter.h>
+
+#include "app_timer.h"
+#include "app_timer_define.h"
+
+// ログ出力制御
+#define LOG_LEVEL LOG_LEVEL_DBG
+#include <logging/log.h>
+LOG_MODULE_REGISTER(app_timer);
+
+#define LOG_TIMER_START_STOP    false
+
+//
+// タイマー共通処理
+//
+#define TIMER DT_LABEL(DT_NODELABEL(rtc0))
+
+static const struct device *m_counter_dev;
+static void counter_interrupted(const struct device *counter_dev, uint8_t chan_id, uint32_t ticks, void *user_data);
+
+void app_timer_initialize(void) 
+{
+    m_counter_dev = device_get_binding(TIMER);
+    if (m_counter_dev != NULL) {
+        counter_start(m_counter_dev);
+        LOG_INF("Alarm counter for timer started (timer=%s)", TIMER);
+
+    } else {
+        LOG_ERR("Timer (%s) device not found", TIMER);
+    }
+}
+
+static void app_timer_start(uint8_t chan_id, TIMER_CFG *cfg)
+{
+    cfg->alarm_cfg.flags = 0;
+    cfg->alarm_cfg.ticks = counter_us_to_ticks(m_counter_dev, cfg->timeout_ms * 1000);
+    cfg->alarm_cfg.callback = counter_interrupted;
+    cfg->alarm_cfg.user_data = cfg;
+
+    int err = counter_set_channel_alarm(m_counter_dev, chan_id, &cfg->alarm_cfg);
+    if (-EINVAL == err) {
+        LOG_ERR("Alarm settings invalid");
+    } else if (-ENOTSUP == err) {
+        LOG_ERR("Alarm setting request not supported");
+    } else if (err != 0) {
+        LOG_ERR("Error counter_set_channel_alarm returns %d", err);
+    }
+
+#if LOG_TIMER_START_STOP
+    LOG_DBG("Set alarm in %u msec (chan_id=%d)",
+            (uint32_t)(counter_ticks_to_us(m_counter_dev, cfg->alarm_cfg.ticks) / USEC_PER_MSEC), chan_id);
+#endif
+}
+
+static void app_timer_stop(uint8_t chan_id)
+{
+    counter_cancel_channel_alarm(m_counter_dev, chan_id);
+
+#if LOG_TIMER_START_STOP
+    LOG_DBG("Canceled alarm (chan_id=%d)", chan_id);
+#endif
+}
+
+//
+// タイムアウト時の処理
+//
+static void counter_interrupted(const struct device *counter_dev, uint8_t chan_id, uint32_t ticks, void *user_data)
+{
+#if LOG_TIMER_START_STOP
+    LOG_DBG("Alarm by counter interrupt (chan_id=%d)", chan_id);
+#endif
+
+    // 業務処理スレッドにイベントを引き渡す
+    TIMER_CFG *cfg = (TIMER_CFG *)user_data;
+    app_event_notify(cfg->callback_event);
+    
+    // 繰返しタイマーの場合は再度タイマーを開始
+    if (cfg->is_repeat) {
+        app_timer_start(chan_id, user_data);
+    }
+}
+
+//
+// ボタンが長押しされたことを検知するタイマー
+//
+TIMER_CFG cfg_longpush;
+
+void app_timer_start_for_longpush(uint32_t timeout_ms, APP_EVENT_T event)
+{
+    cfg_longpush.timeout_ms = timeout_ms;
+    cfg_longpush.callback_event = event;
+    cfg_longpush.is_repeat = false;
+    app_timer_start(CHID_FOR_LONGPUSH, &cfg_longpush);
+}
+
+void app_timer_stop_for_longpush(void)
+{
+    app_timer_stop(CHID_FOR_LONGPUSH);
+}
---
+++ pyvenvs/ncs/nRF5340_app/secure_device_app/src/app_timer_define.h
@@ -0,0 +1,36 @@
+/* 
+ * File:   app_timer_define.h
+ * Author: makmorit
+ *
+ * Created on 2021/04/08, 16:49
+ */
+#ifndef APP_TIMER_DEFINE_H
+#define APP_TIMER_DEFINE_H
+
+#include "app_event.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//
+// タイマーのチャネルID
+// 
+#define CHID_FOR_LONGPUSH           0
+
+//
+// タイマーで使用する各種データを保持
+//
+typedef struct {
+    uint8_t     chan_id;
+    uint32_t    timeout_ms;
+    APP_EVENT_T callback_event;
+    bool        is_repeat;
+    struct counter_alarm_cfg alarm_cfg;
+} TIMER_CFG;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* APP_TIMER_DEFINE_H */
