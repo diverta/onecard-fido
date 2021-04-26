22d8c0681dcef427e380ca0a78c3a697002e3a03
Makoto Morita <makmorit@outlook.jp>
2021-04-26 15:52:39 +0900

    事前リファクタリング
    ・デフォルトのアドバタイジングモードを、nRF52840アプリケーションに整合させる
    （BT_LE_AD_LIMITEDは、ペアリングモード時に明示的に指定させるよう、後日修正します）

--- pyvenvs/ncs/nRF5340_app/secure_device_app/bluetooth/app_bluetooth.c
+++ pyvenvs/ncs/nRF5340_app/secure_device_app/bluetooth/app_bluetooth.c
@@ -24,7 +24,7 @@ static struct k_work advertise_work;
 
 // advertising data
 static const struct bt_data ad[] = {
-    BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_LIMITED | BT_LE_AD_NO_BREDR)),
+    BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
     BT_DATA_BYTES(BT_DATA_UUID128_ALL, 0x84, 0xaa, 0x60, 0x74, 0x52, 0x8a, 0x8b, 0x86, 0xd3, 0x4c, 0xb7, 0x1d, 0x1d, 0xdc, 0x53, 0x8d),
 };
 
