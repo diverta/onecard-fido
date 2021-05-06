ccb11b18e7a388fc096f2b8b4a6ade89572fd926
Makoto Morita <makmorit@outlook.jp>
2021-05-06 12:07:24 +0900

    リファクタリング
    ・app_data_event.c --> app_event.c に統合（移設）

--- pyvenvs/ncs/nRF5340_app/secure_device_app/src/app_event.c
+++ pyvenvs/ncs/nRF5340_app/secure_device_app/src/app_event.c
@@ -22,11 +22,19 @@ LOG_MODULE_REGISTER(app_event);
 // イベント待ち行列の管理
 //
 K_FIFO_DEFINE(app_main_fifo);
+K_FIFO_DEFINE(app_data_fifo);
 
 typedef struct {
     void           *fifo_reserved;
     APP_EVENT_T     event;
 } APP_MAIN_FIFO_T;
+
+typedef struct {
+    void           *fifo_reserved;
+    DATA_EVENT_T    event;
+    uint8_t         data[64];
+    size_t          size;
+} APP_DATA_FIFO_T;
 
 bool app_event_notify(APP_EVENT_T event)
 {
@@ -45,6 +53,28 @@ bool app_event_notify(APP_EVENT_T event)
     // イベントデータを待ち行列にセット
     p_fifo->event = event;
     k_fifo_put(&app_main_fifo, p_fifo);
+    return true;
+}
+
+bool app_event_notify_for_data(DATA_EVENT_T event, uint8_t *data, size_t data_size)
+{
+#if LOG_NOTIFIED_EVENT
+    LOG_DBG("App data event notified (event type=%d)", event);
+#endif
+
+    // 領域を確保
+    size_t size = sizeof(APP_DATA_FIFO_T);
+    APP_DATA_FIFO_T *p_fifo = (APP_DATA_FIFO_T *)k_malloc(size);
+    if (p_fifo == NULL) {
+        LOG_ERR("APP_DATA_FIFO_T allocation failed");
+        return false;
+    }
+
+    // イベントデータを待ち行列にセット
+    p_fifo->event = event;
+    p_fifo->size = data_size;
+    memcpy(p_fifo->data, data, data_size);
+    k_fifo_put(&app_data_fifo, p_fifo);
     return true;
 }
 
@@ -76,8 +106,40 @@ static void app_main_thread(void)
     }
 }
 
+//
+// データ関連イベント処理
+//
+static void app_data_event_process(void)
+{
+    // イベント検知まで待機
+    APP_DATA_FIFO_T *fifo = k_fifo_get(&app_data_fifo, K_FOREVER);
+
+    // イベントに対応する処理を実行
+    app_process_for_data_event(fifo->event, fifo->data, fifo->size);
+
+    // FIFOデータを解放
+    k_free(fifo);
+}
+
+static void app_data_thread(void)
+{
+    while (true) {
+        // アプリケーション初期化前の場合はイベントを無視
+        if (app_main_initialized() == false) {
+            continue;
+        }
+
+        // 各種イベントを処理
+        app_data_event_process();
+    }
+}
+
+//
+// スレッド本体
+//
 // STACKSIZE: size of stack area used by thread
 // PRIORITY:  scheduling priority used by thread
 #define STACKSIZE   1024
 #define PRIORITY    7
 K_THREAD_DEFINE(app_main_thread_id, STACKSIZE, app_main_thread, NULL, NULL, NULL, PRIORITY, 0, 0);
+K_THREAD_DEFINE(app_data_thread_id, STACKSIZE, app_data_thread, NULL, NULL, NULL, PRIORITY, 0, 0);
