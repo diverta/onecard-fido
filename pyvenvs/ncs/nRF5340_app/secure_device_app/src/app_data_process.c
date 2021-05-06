db361aeb2b56e8873abfd6634b3bfb65ab5caec2
Makoto Morita <makmorit@outlook.jp>
2021-05-06 11:18:02 +0900

    リファクタリング
    ・業務処理実装用のカスタムモジュールを新設
    ・app_data_process の処理を、カスタムモジュールに移設
    （app_data_process は後日、app_process モジュールに吸収予定）

--- pyvenvs/ncs/nRF5340_app/secure_device_app/src/app_data_process.c
+++ pyvenvs/ncs/nRF5340_app/secure_device_app/src/app_data_process.c
@@ -8,31 +8,22 @@
 #include <zephyr.h>
 
 #include "app_data_event.h"
+#include "app_custom.h"
 
 // ログ出力制御
 #define LOG_LEVEL LOG_LEVEL_DBG
 #include <logging/log.h>
 LOG_MODULE_REGISTER(app_data_process);
 
-#define LOG_DEBUG_HID_REPORT        false
-
-//
-// データ処理イベント関連
-//
-static void hid_report_received(uint8_t *data, size_t size)
-{
-#if LOG_DEBUG_HID_REPORT
-    LOG_DBG("received %d bytes", size);
-    LOG_HEXDUMP_DBG(data, size, "HID report");
-#endif
-}
-
 void app_data_process_for_event(DATA_EVENT_T event, uint8_t *data, size_t size)
 {
     // イベントに対応する処理を実行
     switch (event) {
         case DATEVT_HID_REPORT_RECEIVED:
-            hid_report_received(data, size);
+            app_custom_hid_report_received(data, size);
+            break;
+        case DATEVT_HID_REPORT_SENT:
+            app_custom_hid_report_sent();
             break;
         default:
             break;
