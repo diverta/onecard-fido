8299dbc3b0f3ce044f26fb39b42676d4da413ea4
Makoto Morita <makmorit@outlook.jp>
2021-05-07 16:33:26 +0900

    USB／BLE自動切替対応
    ・USB接続／開始／切断の各イベントを、スレッドに通知できるよう修正

--- pyvenvs/ncs/nRF5340_app/secure_device_app/src/app_usb.c
+++ pyvenvs/ncs/nRF5340_app/secure_device_app/src/app_usb.c
@@ -8,6 +8,7 @@
 #include <zephyr.h>
 #include <usb/usb_device.h>
 
+#include "app_event.h"
 #include "app_usb_hid.h"
 
 #define LOG_LEVEL LOG_LEVEL_DBG
@@ -21,10 +22,15 @@ static void status_cb(enum usb_dc_status_code status, const uint8_t *param)
 {
     (void)param;
     switch (status) {
+        case USB_DC_CONNECTED:
+            app_event_notify(APEVT_USB_CONNECTED);
+            break;
         case USB_DC_CONFIGURED:
+            app_event_notify(APEVT_USB_CONFIGURED);
             app_usb_hid_configured(param);
             break;
         case USB_DC_DISCONNECTED:
+            app_event_notify(APEVT_USB_DISCONNECTED);
             break;
         case USB_DC_SOF:
             break;
