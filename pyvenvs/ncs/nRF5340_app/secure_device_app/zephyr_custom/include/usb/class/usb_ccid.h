ec4d98b2788619b089065545665477a08a9044de
Makoto Morita <makmorit@outlook.jp>
2021-05-10 16:52:13 +0900

    事前準備
    ・USB CCIDインターフェース専用の実装コードを準備

---
+++ pyvenvs/ncs/nRF5340_app/secure_device_app/zephyr_custom/include/usb/class/usb_ccid.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   usb_ccid.h
+ * Author: makmorit
+ *
+ * Created on 2021/05/10, 14:25
+ */
+#ifndef USB_CCID_H
+#define USB_CCID_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* USB_CCID_H */
---
+++ pyvenvs/ncs/nRF5340_app/secure_device_app/zephyr_custom/subsys/usb/class/usb_ccid.c
@@ -0,0 +1,6 @@
+/* 
+ * File:   usb_ccid.c
+ * Author: makmorit
+ *
+ * Created on 2021/05/10, 14:25
+ */
