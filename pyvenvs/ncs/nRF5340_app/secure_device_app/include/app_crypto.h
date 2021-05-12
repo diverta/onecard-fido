12bd0a162e8a009aa51f1eacafbeee2e5ea83207
Makoto Morita <makmorit@outlook.jp>
2021-05-12 10:08:23 +0900

    暗号化関連対応
    ・専用モジュールを新設

---
+++ pyvenvs/ncs/nRF5340_app/secure_device_app/include/app_crypto.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   app_crypto.h
+ * Author: makmorit
+ *
+ * Created on 2021/05/12, 9:59
+ */
+#ifndef APP_CRYPTO_H
+#define APP_CRYPTO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//
+// 関数群
+//
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* APP_CRYPTO_H */
---
+++ pyvenvs/ncs/nRF5340_app/secure_device_app/src/app_crypto.c
@@ -0,0 +1,6 @@
+/* 
+ * File:   app_crypto.c
+ * Author: makmorit
+ *
+ * Created on 2021/05/12, 9:59
+ */
