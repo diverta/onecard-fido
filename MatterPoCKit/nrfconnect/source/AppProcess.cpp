913b9be46d62ac123766f9c94d9e85908aef2225
Makoto Morita <makmorit@outlook.jp>
2021-07-22 11:16:16 +0900

    BLEアドバタイズ状態表示対応
    ・管理用HIDコマンドにより、リセット処理を実行できるよう修正
    ・他コマンドの実行用バイト値を１つずらす
    （0xce: Toggleコマンド手動実行、0xcd: 解錠後の自動施錠実行設定を変更）

--- MatterPoCKit/nrfconnect/source/AppProcess.cpp
+++ MatterPoCKit/nrfconnect/source/AppProcess.cpp
@@ -284,10 +284,14 @@ void AppProcessUSBHIDCommand(uint8_t command)
     // USB HID I/Fからコマンド投入時の処理
     switch (command) {
         case 0xcf:
+            // リセット処理を行う（工場出荷状態に戻す）
+            ConfigurationMgr().InitiateFactoryReset();
+            break;
+        case 0xce:
             // CHIPから解錠・施錠コマンドを受信したのと等価の処理を行う
             AppBoltLockerSimulateLockAction();
             break;
-        case 0xce:
+        case 0xcd:
             // 解錠後の自動施錠実行設定（On/Off）を変更
             ToggleAutoRelockEnabled();
             break;
