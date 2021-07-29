76e2a50087e3df0ff294debdbb4bc026c83cf646
Makoto Morita <makmorit@outlook.jp>
2021-07-29 14:56:13 +0900

    業務処理の実装
    ・コミッショニング処理を追加

--- MatterPoCKit/iOS/mattercontroller/business/MatterCommand.m
+++ MatterPoCKit/iOS/mattercontroller/business/MatterCommand.m
@@ -15,7 +15,11 @@
     // 非同期処理用のキュー（画面用）
     @property (nonatomic) dispatch_queue_t mainQueue;
     // Matterライブラリーのオブジェクト参照
-    @property (readwrite) CHIPDeviceController *chipController;
+    @property (readwrite) CHIPDeviceController     *chipController;
+    @property (nonatomic) CHIPNetworkCommissioning *cluster;
+    // コミッショニングに必要なパラメーターを保持
+    @property (nonatomic) NSData               *operationalDataset;
+    @property (nonatomic) NSData               *networkId;
 
 @end
 
@@ -34,6 +38,7 @@
             [self setMainQueue:dispatch_get_main_queue()];
             // Matter関連の初期化
             [self setupChipDeviceController];
+            [self setupCommissioningParameters];
         }
         NSLog(@"initWithViewControllerRef done");
         return self;
@@ -44,6 +49,32 @@
         dispatch_queue_t callbackQueue = dispatch_queue_create("jp.co.diverta.matter.vc.callback", DISPATCH_QUEUE_SERIAL);
         [self setChipController:InitializeCHIP()];
         [[self chipController] setPairingDelegate:self queue:callbackQueue];
+    }
+
+    - (void)setupCommissioningParameters {
+        // コミッショニングに必要なパラメーターを設定
+        //  channel         0x00000f
+        //  Network PANID   0x1234
+        //  Network XPANID  0x1111111122222222
+        //  Master Key      0x00112233445566778899aabbccddeeff
+        [self setOperationalDataset:[self generateHexBytesFrom:@"000300000f0102123402081111111122222222051000112233445566778899aabbccddeeff"]];
+        [self setNetworkId:[self generateHexBytesFrom:@"1111111122222222"]];
+    }
+
+    - (NSData *)generateHexBytesFrom:(NSString *)hexString {
+        unsigned int  hexInt;
+        unsigned char byte;
+        
+        // 与えられたHEX文字列を２文字ずつ切り出し、バイトデータに変換する
+        NSMutableData *convertedBytes = [[NSMutableData alloc] init];
+        for (int i = 0; i < [hexString length]; i+=2) {
+            NSString *tmp = [hexString substringWithRange:NSMakeRange(i, 2)];
+            [[NSScanner scannerWithString:tmp] scanHexInt:&hexInt];
+            byte = (unsigned char)hexInt;
+            [convertedBytes appendBytes:&byte length:sizeof(byte)];
+        }
+        
+        return convertedBytes;
     }
 
     - (void)handleRendezVousBLE:(uint16_t)discriminator setupPINCode:(uint32_t)setupPINCode {
@@ -80,8 +111,89 @@
     }
 
     - (void)pairingCompleted:(bool)success {
-        // 画面に制御を戻す
-        [[self delegate] didPairingComplete:success];
+        if (success == false) {
+            // ペアリング失敗時は画面に制御を戻す
+            [[self delegate] didPairingComplete:false];
+            return;
+        }
+        // For debug
+        [self printDeviceList];
+        // コミッショニング処理を続行
+        if ([self addThreadNetwork] == false) {
+            NSLog(@"Status: Failed to trigger the connection with the device");
+            [[self delegate] didPairingComplete:false];
+            return;
+        }
+    }
+
+    - (bool)addThreadNetwork {
+        bool success = CHIPGetConnectedDevice(^(CHIPDevice * _Nullable chipDevice, NSError * _Nullable error) {
+            if (chipDevice == nil) {
+                NSLog(@"Status: Failed to establish a connection with the device");
+                [[self delegate] didPairingComplete:false];
+                return;
+            }
+
+            [self setCluster:[[CHIPNetworkCommissioning alloc] initWithDevice:chipDevice endpoint:0 queue:dispatch_get_main_queue()]];
+
+            __weak typeof(self) weakSelf = self;
+            uint64_t breadcrumb = 0;
+            uint32_t timeoutMs = 3000;
+            [[self cluster] addThreadNetwork:[self operationalDataset]
+                breadcrumb:breadcrumb timeoutMs:timeoutMs
+                responseHandler:^(NSError *error, NSDictionary *values) {
+                [weakSelf onAddNetworkResponse:error isWiFi:NO];
+            }];
+        });
+        return success;
+    }
+
+    - (void)onAddNetworkResponse:(NSError *)error isWiFi:(BOOL)isWiFi {
+        if (error != nil) {
+            NSLog(@"Error adding network: %@", error);
+            [[self delegate] didPairingComplete:false];
+            return;
+        }
+
+        NSData * networkId;
+        if (isWiFi) {
+            NSString * ssid = CHIPGetDomainValueForKey(kCHIPToolDefaultsDomain, kNetworkSSIDDefaultsKey);
+            networkId = [ssid dataUsingEncoding:NSUTF8StringEncoding];
+        } else {
+            networkId = [self networkId];
+        }
+
+        __weak typeof(self) weakSelf = self;
+        uint64_t breadcrumb = 0;
+        uint32_t timeoutMs = 3000;
+        [[self cluster] enableNetwork:networkId
+            breadcrumb:breadcrumb timeoutMs:timeoutMs
+            responseHandler:^(NSError * err, NSDictionary * values) {
+            [weakSelf onEnableNetworkResponse:err];
+        }];
+    }
+
+    - (void)onEnableNetworkResponse:(NSError *)error {
+        if (error != nil) {
+            NSLog(@"Error enabling network: %@", error);
+            [[self delegate] didPairingComplete:false];
+            return;
+        }
+        // コミッション全工程完了
+        uint64_t deviceId = CHIPGetNextAvailableDeviceID() - 1;
+        CHIPDeviceController *controller = [CHIPDeviceController sharedController];
+        [controller updateDevice:deviceId fabricId:0];
+        [[self delegate] didPairingComplete:true];
+    }
+
+    - (void)printDeviceList {
+        // For debug
+        uint64_t nextDeviceID = CHIPGetNextAvailableDeviceID();
+        for (uint64_t i = 0; i < nextDeviceID; i++) {
+            if (CHIPIsDevicePaired(i)) {
+                NSLog(@"Paired Matter device (%d)", (int)i);
+            }
+        }
     }
 
 @end
