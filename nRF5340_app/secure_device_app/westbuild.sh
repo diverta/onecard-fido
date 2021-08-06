cf149bb4b37d9d0bb8efa8118b4dd38350eed97b
Makoto Morita <makmorit@outlook.jp>
2021-08-06 15:22:22 +0900

    リファクタリング
    ・nRF5340アプリケーションの格納場所を変更
    （nRF Connect SDKのディレクトリーと分離）

--- nRF5340_app/secure_device_app/westbuild.sh
+++ nRF5340_app/secure_device_app/westbuild.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Build target
+#   nrf5340dk_nrf5340_cpuapp
+#   nrf52840dk_nrf52840
+export BUILD_TARGET=nrf5340dk_nrf5340_cpuapp
+
+# Build target
+#   None for Nordic boards
+#   MDBT50Q_dongle_rev2
+export BOARD_TARGET=
+
 # Environment variables for the GNU Arm Embedded toolchain
 export ZEPHYR_TOOLCHAIN_VARIANT=gnuarmemb
 export GNUARMEMB_TOOLCHAIN_PATH="${HOME}/opt/gcc-arm-none-eabi-9-2020-q2-update"
@@ -11,28 +21,29 @@ export PATH=${PATH}:${HOME}/opt/nRF-Command-Line-Tools_10_9_0_OSX/nrfjprog
 # bash completion
 export REPO_HOME=${HOME}/GitHub/onecard-fido
 export NCS_HOME=${REPO_HOME}/pyvenvs/ncs
+export ZEPHYR_BASE=${NCS_HOME}/zephyr
 source ${NCS_HOME}/west-completion.bash
 
 # Enter Python3 venv
 source ${NCS_HOME}/bin/activate
 
 if [ "$1" == "-f" ]; then
-    # Flash for nRF5340 DK
+    # Flash for nRF5340/nRF52840
     ${NCS_HOME}/bin/west -v flash -d build_signed
     if [ `echo $?` -ne 0 ]; then
         deactivate
         exit 1
     fi
 else
-    # Build for nRF5340 DK
-    rm -rfv build_signed
-    ${NCS_HOME}/bin/west build -c -b nrf5340dk_nrf5340_cpuapp -d build_signed -- -DCONFIG_MCUBOOT_SIGNATURE_KEY_FILE=\"bootloader/mcuboot/root-rsa-2048.pem\" -DOVERLAY_CONFIG=overlay-smp.conf
+    # Build for nRF5340/nRF52840
+    rm -rf build_signed
+    ${NCS_HOME}/bin/west build -c -b ${BUILD_TARGET} -d build_signed -- -DCONFIG_MCUBOOT_SIGNATURE_KEY_FILE=\"bootloader/mcuboot/root-rsa-2048.pem\" -DOVERLAY_CONFIG=overlay-smp.conf
     if [ `echo $?` -ne 0 ]; then
         deactivate
         exit 1
     fi
     # Deploy binary file for DFU
-    cp -pv build_signed/zephyr/app_update.bin ${REPO_HOME}/nRF5340_app/firmwares/secure_device_app/
+    cp -pv build_signed/zephyr/app_update.bin ../firmwares/secure_device_app/
     echo Application binary for secure bootloader is now available.
 fi
 
