3df6854f7b9a4b722b6d3b2e3cb29d27fa2c1daa
Makoto Morita <makmorit@outlook.jp>
2021-08-26 15:53:23 +0900

    BLEチャネル開通対応
    ・定義体（UUID）を作成

---
+++ nRF5340_app/secure_device_app/include/app_ble_fido.h
@@ -0,0 +1,47 @@
+/* 
+ * File:   app_ble_fido.h
+ * Author: makmorit
+ *
+ * Created on 2021/08/26, 11:50
+ */
+#ifndef APP_BLE_FIDO_H
+#define APP_BLE_FIDO_H
+
+#include <zephyr/types.h>
+#include <bluetooth/conn.h>
+#include <bluetooth/gatt.h>
+#include <bluetooth/uuid.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//
+// サービス／キャラクタリスティックのUUID
+//
+// FIDO BLE Service             FFFD
+// FIDO U2F Control Point(RX)   F1D0FFF1-DEAA-ECEE-B42F-C9BA7ED623BB
+// FIDO U2F Status(TX)          F1D0FFF2-DEAA-ECEE-B42F-C9BA7ED623BB
+//
+#define BT_UUID_FIDO_VAL 0xfffd
+
+#define BT_UUID_FIDO_RX_VAL \
+        BT_UUID_128_ENCODE(0xf1d0fff1, 0xdeaa, 0xecee, 0xb42f, 0xc9ba7ed623bb)
+
+#define BT_UUID_FIDO_TX_VAL \
+        BT_UUID_128_ENCODE(0xf1d0fff2, 0xdeaa, 0xecee, 0xb42f, 0xc9ba7ed623bb)
+
+#define BT_UUID_FIDO_SERVICE        BT_UUID_DECLARE_16(BT_UUID_FIDO_VAL)
+#define BT_UUID_FIDO_RX             BT_UUID_DECLARE_128(BT_UUID_FIDO_RX_VAL)
+#define BT_UUID_FIDO_TX             BT_UUID_DECLARE_128(BT_UUID_FIDO_TX_VAL)
+
+//
+// 関数群
+//
+int         app_ble_fido_send_data(struct bt_conn *conn, const uint8_t *data, uint16_t len);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* APP_BLE_FIDO_H */
