463fc4943023c4080588051a9af9fc9ee02a3aa2
Makoto Morita <makmorit@outlook.jp>
2021-10-07 14:58:30 +0900

    事前準備
    ・BLE接続／切断専用クラスを新設
    ・内容は未実装

---
+++ MaintenanceTool/macOSApp/MaintenanceToolCommon/ToolBLEConnectionHelper.h
@@ -0,0 +1,13 @@
+//
+//  ToolBLEConnectionHelper.h
+//  MaintenanceTool
+//
+//  Created by Makoto Morita on 2021/10/07.
+//
+#ifndef ToolBLEConnectionHelper_h
+#define ToolBLEConnectionHelper_h
+
+#import <Foundation/Foundation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+#endif /* ToolBLEConnectionHelper_h */
---
+++ MaintenanceTool/macOSApp/MaintenanceToolCommon/ToolBLEConnectionHelper.m
@@ -0,0 +1,7 @@
+//
+//  ToolBLEConnectionHelper.m
+//  MaintenanceTool
+//
+//  Created by Makoto Morita on 2021/10/07.
+//
+#import "ToolBLEConnectionHelper.h"
