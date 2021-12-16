ebc3ffb91402a03c16a13427a26761f324801efb
Makoto Morita <makmorit@outlook.jp>
2021-12-16 15:38:06 +0900

    GPGコマンドプロセッサーの制作
    ・処理の入り口となるコマンドクラスのテンプレートを作成

---
+++ MaintenanceTool/macOSApp/GPG/ToolGPGCommand.h
@@ -0,0 +1,16 @@
+//
+//  ToolGPGCommand.h
+//  MaintenanceTool
+//
+//  Created by Makoto Morita on 2021/12/16.
+//
+#ifndef ToolGPGCommand_h
+#define ToolGPGCommand_h
+
+@interface ToolGPGCommand : NSObject
+
+    - (id)initWithDelegate:(id)delegate;
+
+@end
+
+#endif /* ToolGPGCommand_h */
---
+++ MaintenanceTool/macOSApp/GPG/ToolGPGCommand.m
@@ -0,0 +1,32 @@
+//
+//  ToolGPGCommand.m
+//  MaintenanceTool
+//
+//  Created by Makoto Morita on 2021/12/16.
+//
+#import "ToolAppCommand.h"
+#import "ToolGPGCommand.h"
+
+@interface ToolGPGCommand ()
+
+    // 上位クラスの参照を保持
+    @property (nonatomic, weak) ToolAppCommand     *toolAppCommand;
+
+@end
+
+@implementation ToolGPGCommand
+
+    - (id)init {
+        return [self initWithDelegate:nil];
+    }
+
+    - (id)initWithDelegate:(id)delegate {
+        self = [super init];
+        if (self) {
+            // 上位クラスの参照を保持
+            [self setToolAppCommand:(ToolAppCommand *)delegate];
+        }
+        return self;
+    }
+
+@end
