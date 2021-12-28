5166190aa941e4423dc27866c3ceda50dc3400a7
Makoto Morita <makmorit@outlook.jp>
2021-12-28 16:36:06 +0900

    リセット機能の実装
    ・リセット機能に関する定義を追加

--- MaintenanceTool/macOSApp/MaintenanceToolCommon/ToolCommonMessage.h
+++ MaintenanceTool/macOSApp/MaintenanceToolCommon/ToolCommonMessage.h
@@ -70,6 +70,7 @@
 #define PROCESS_NAME_CCID_PIV_SET_CHUID             @"PIV CHUID設定"
 #define PROCESS_NAME_CCID_PIV_STATUS                @"PIV設定情報取得"
 #define PROCESS_NAME_OPENPGP_INSTALL_KEYS           @"PGP秘密鍵インストール"
+#define PROCESS_NAME_OPENPGP_RESET                  @"OpenPGP機能リセット"
 
 #pragma mark - ToolCommandクラス専用メッセージ
 #define MSG_INVALID_SKEY_LENGTH_IN_PEM      @"鍵ファイルに格納された秘密鍵の長さが不正です。"
