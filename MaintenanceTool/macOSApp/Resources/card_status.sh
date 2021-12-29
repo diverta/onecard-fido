9fb85ec9ac0e68e088a0af33e617ad0ccce7ced8
Makoto Morita <makmorit@outlook.jp>
2021-12-29 15:47:55 +0900

    ステータス照会機能の実装
    ・ステータス照会実行用スクリプトを新設

---
+++ MaintenanceTool/macOSApp/Resources/card_status.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#
+# Card status command
+#
+/usr/local/bin/gpg --card-status
+RC=`echo $?`
+if [ ${RC} -ne 0 ]; then
+    echo "Card status command fail"
+    exit 1
+fi
+
+echo "Execute script for gnupg success"
+exit 0
