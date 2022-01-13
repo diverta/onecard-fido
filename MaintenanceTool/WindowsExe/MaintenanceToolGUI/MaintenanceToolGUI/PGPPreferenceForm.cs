9c2a09774779ebc17ba56bf46991435e76b04187
makmorit <makmorit@outlook.jp>
2022-01-13 10:59:10 +0900

    OpenPGPパラメーター入力画面の新設
    ・画面項目の初期化処理を実装

--- MaintenanceTool/WindowsExe/MaintenanceToolGUI/MaintenanceToolGUI/PGPPreferenceForm.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolGUI/MaintenanceToolGUI/PGPPreferenceForm.cs
@@ -15,6 +15,7 @@ namespace MaintenanceToolGUI
         {
             // 画面項目の初期化
             InitializeComponent();
+            InitFieldValue();
 
             // 処理クラスの参照を保持
             ToolPGPRef = toolPGP;
@@ -29,8 +30,75 @@ namespace MaintenanceToolGUI
         private void TerminateWindow(DialogResult dialogResult)
         {
             // 画面項目を初期化し、この画面を閉じる
+            InitFieldValue();
             DialogResult = dialogResult;
             Close();
+        }
+
+        //
+        // PGP鍵管理タブ関連の処理
+        //
+        private void InitFieldValue()
+        {
+            // PGP鍵管理タブ内の入力項目を初期化（このタブが選択状態になります）
+            tabPreference.SelectedTab = tabPagePGPKeyManagement;
+            InitTabPGPKeyManagement();
+        }
+
+        private void InitTabPGPKeyManagement()
+        {
+            // テキストボックスの初期化
+            InitTabPGPKeyPathFields();
+            InitTabPGPKeyEntryFields();
+        }
+
+        private void InitTabPGPKeyPathFields()
+        {
+            // ファイルパスのテキストボックスを初期化
+            textPubkeyFolderPath.Text = "";
+            textBackupFolderPath.Text = "";
+        }
+
+        private void InitTabPGPKeyEntryFields()
+        {
+            // テキストボックスを初期化
+            textRealName.Text = "";
+            textMailAddress.Text = "";
+            textComment.Text = "";
+            textPin.Text = "";
+            textPinConfirm.Text = "";
+
+            // テキストボックスのカーソルを先頭の項目に配置
+            textRealName.Focus();
+        }
+
+        void EnableButtons(bool enabled)
+        {
+            // ボタンや入力欄の使用可能／不可制御
+            buttonClose.Enabled = enabled;
+            buttonPGPStatus.Enabled = enabled;
+            buttonPGPReset.Enabled = enabled;
+            buttonFirmwareReset.Enabled = enabled;
+
+            // 現在選択中のタブ内も同様に制御を行う
+            TabPage page = tabPreference.SelectedTab;
+            if (page == tabPagePGPKeyManagement) {
+                EnableButtonsInTabPGPKeyManagement(enabled);
+            }
+        }
+
+        void EnableButtonsInTabPGPKeyManagement(bool enabled)
+        {
+            // ボタンや入力欄の使用可能／不可制御
+            textRealName.Enabled = enabled;
+            textMailAddress.Enabled = enabled;
+            textComment.Enabled = enabled;
+            textPin.Enabled = enabled;
+            textPinConfirm.Enabled = enabled;
+
+            buttonPubkeyFolderPath.Enabled = enabled;
+            buttonBackupFolderPath.Enabled = enabled;
+            buttonInstallPGPKey.Enabled = enabled;
         }
     }
 }
