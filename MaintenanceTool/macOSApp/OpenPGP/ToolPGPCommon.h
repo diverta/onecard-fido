d64de0898f67dc957cd6ef219c5d6eb320b55cbc
Makoto Morita <makmorit@outlook.jp>
2022-03-02 10:48:59 +0900

    管理用PIN認証コマンド対応
    ・コマンド内で実行されるINSバイトの定義を追加

---
+++ MaintenanceTool/macOSApp/OpenPGP/ToolPGPCommon.h
@@ -0,0 +1,13 @@
+//
+//  ToolPGPCommon.h
+//  MaintenanceTool
+//
+//  Created by Makoto Morita on 2022/03/02.
+//
+#ifndef ToolPGPCommon_h
+#define ToolPGPCommon_h
+
+#define OPENPGP_INS_SELECT                  0xA4
+#define OPENPGP_INS_VERIFY                  0x20
+
+#endif /* ToolPGPCommon_h */
