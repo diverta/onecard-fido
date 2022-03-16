86f15a33d5a8da512b0c27414fa287648d1cc37b
Makoto Morita <makmorit@outlook.jp>
2022-03-16 16:32:43 +0900

    ハードウェア依存コードの移行（BLE以外）
    ・非推奨となったマクロを、推奨されるマクロに置換え

--- nRF5340_app/secure_device_app/src/app_usb_ccid.c
+++ nRF5340_app/secure_device_app/src/app_usb_ccid.c
@@ -198,7 +198,7 @@ static int usb_ccid_handler(struct usb_setup_packet *setup, int32_t *len, uint8_
     return -ENOTSUP;
 }
 
-USBD_CFG_DATA_DEFINE(primary, ccid) struct usb_cfg_data usb_ccid_config_data = {
+USBD_DEFINE_CFG_DATA(primary_ccid) = {
     .usb_device_description = NULL,
     .interface_config = usb_ccid_interface_config,
     .interface_descriptor = &usb_ccid_cfg.if0,
