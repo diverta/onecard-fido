44925f6f51ec5cbe519ca154f8ac332185deac18
Makoto Morita <makmorit@outlook.jp>
2022-03-24 14:37:11 +0900

    BLEペアリング用パスキー対応
    ・ハードウェアIDの下６桁を、BLEペアリング用パスキーとして設定するよう修正
    ・BLEペアリング用パスキーは、UART出力で参照できるよう実装

--- nRF5340_app/secure_device_app/src/app_bluetooth.c
+++ nRF5340_app/secure_device_app/src/app_bluetooth.c
@@ -25,6 +25,39 @@ static char addr_str_buf_1[BT_ADDR_LE_STR_LEN];
 static char addr_str_buf_2[BT_ADDR_LE_STR_LEN];
 
 //
+// パスキー関連
+//
+#include <drivers/hwinfo.h>
+
+// Work for hardware ID & passkey
+static uint8_t  m_hwid[8];
+static uint32_t m_passkey;
+
+static void set_passkey_for_pairing(void)
+{
+    // BLEペアリング用のパスキーを設定
+    uint8_t *p = (uint8_t *)&m_passkey;
+
+    if (hwinfo_get_device_id(m_hwid, sizeof(m_hwid)) > 0) {
+        // ハードウェアIDの下位４バイト分を抽出
+        for (int i = 0; i < 4; i++) {
+            p[3 - i] = m_hwid[i + 4];
+        }
+        
+        // 抽出されたハードウェアID（１０進）の下６桁をパスキーに設定
+        m_passkey %= 1000000;
+
+    } else {
+        // ハードウェアIDが抽出できなかった場合は、
+        // パスキーを'000000'に設定
+        m_passkey = 0;
+    }
+    
+    LOG_INF("Passkey for BLE pairing: %06u", m_passkey);
+    bt_passkey_set((unsigned int)m_passkey);
+}
+
+//
 // アドバタイズ関連
 //
 // work queue for advertise
@@ -182,6 +215,9 @@ static void bt_ready(int err)
 
     // Bluetooth初期処理完了
     LOG_INF("Bluetooth initialized");
+
+    // BLEペアリング用のパスキーを設定
+    set_passkey_for_pairing();
 
     // BLE使用可能イベントを業務処理スレッドに引き渡す
     app_event_notify(APEVT_BLE_AVAILABLE);
