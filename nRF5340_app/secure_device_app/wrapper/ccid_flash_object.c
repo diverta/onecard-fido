8a2b85384e4212709d5196211ed91d8874641e78
Makoto Morita <makmorit@outlook.jp>
2022-04-20 10:12:42 +0900

    ラッパーモジュール実装対応
    ・共通関数を実装
    （nRF52840アプリケーションと実装を合わせる）

--- nRF5340_app/secure_device_app/wrapper/ccid_flash_object.c
+++ nRF5340_app/secure_device_app/wrapper/ccid_flash_object.c
@@ -4,26 +4,49 @@
  *
  * Created on 2022/04/19, 10:12
  */
-#include "ccid_flash_object.h"
+//
+// プラットフォーム非依存コード
+//
+#include "fido_flash_define.h"
+
+// 業務処理／HW依存処理間のインターフェース
+#include "fido_platform.h"
+
+#ifdef FIDO_ZEPHYR
+fido_log_module_register(ccid_flash_object);
+#endif
+
+// レコード格納領域
+//   バッファ長（MAX_BUF_SIZE）は、
+//   このモジュールで管理する
+//   最大のレコードサイズに合わせます。
+#define MAX_BUF_SIZE     (PIV_DATA_OBJ_DATA_WORDS_MAX+1)
+static uint32_t          m_record_buf_R[MAX_BUF_SIZE];
+static uint32_t          m_record_buf_W[MAX_BUF_SIZE];
 
 uint8_t *ccid_flash_object_read_buffer(void)
 {
-    return 0;
+    return (uint8_t *)m_record_buf_R;
 }
 
 uint8_t *ccid_flash_object_write_buffer(void)
 {
-    return 0;
+    return (uint8_t *)m_record_buf_W;
 }
 
 size_t ccid_flash_object_rw_buffer_size(void)
 {
-    return 0;
+    return MAX_BUF_SIZE;
 }
 
 size_t ccid_flash_object_calculate_words(size_t record_bytes)
 {
-    return 0;
+    // オブジェクトの長さから、必要ワード数を計算
+    size_t record_words = record_bytes / 4;
+    if (record_bytes % 4 > 0) {
+        record_words++;
+    }
+    return record_words;
 }
 
 void ccid_flash_object_failed(void)
