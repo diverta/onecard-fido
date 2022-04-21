d78706eef224c375b58f6090dd0000f467925234
Makoto Morita <makmorit@outlook.jp>
2022-04-21 11:18:24 +0900

    ラッパーモジュール実装対応
    ・PIV機能で使用するRSAラッパー関数を実装
    ・Eの実装を、既存の実装に置換え

--- nRF5340_app/secure_device_app/wrapper/ccid_crypto.c
+++ nRF5340_app/secure_device_app/wrapper/ccid_crypto.c
@@ -4,7 +4,7 @@
  *
  * Created on 2022/04/19, 10:12
  */
-#include "ccid_crypto.h"
+#include "app_crypto_rsa.h"
 
 // 業務処理／HW依存処理間のインターフェース
 #include "fido_platform.h"
@@ -13,29 +13,27 @@
 fido_log_module_register(ccid_crypto);
 #endif
 
-//
-// E は定数として、このモジュール内で管理
-//
-static uint8_t E[] = {0, 1, 0, 1};
-
 uint8_t *ccid_crypto_rsa_e_bytes(void)
 {
-    return E;
+    return app_crypto_rsa_e_bytes();
 }
 
 uint8_t ccid_crypto_rsa_e_size(void)
 {
-    return sizeof(E);
+    return app_crypto_rsa_e_size();
 }
 
 bool ccid_crypto_rsa_private(uint8_t *rsa_private_key_raw, uint8_t *input, uint8_t *output)
 {
-    return false;
+    return app_crypto_rsa_private(rsa_private_key_raw, input, output);
 }
 
 bool ccid_crypto_rsa_import(uint8_t *rsa_private_key_raw, uint8_t *rsa_public_key_raw, unsigned int nbits)
 {
-    return false;
+    if (nbits != 2048) {
+        return false;
+    }
+    return app_crypto_rsa_import_pubkey_from_prvkey(rsa_private_key_raw, rsa_public_key_raw);
 }
 
 bool ccid_crypto_rsa_generate_key(uint8_t *rsa_private_key_raw, uint8_t *rsa_public_key_raw, unsigned int nbits)
