3c25cacf32c1239987ce49ceb5b7dad2d3eadec1
Makoto Morita <makmorit@outlook.jp>
2022-04-22 12:55:00 +0900

    ラッパーモジュール実装対応
    ・コールバック関数群を実装

--- nRF5340_app/secure_device_app/wrapper/ccid_flash_openpgp_object.c
+++ nRF5340_app/secure_device_app/wrapper/ccid_flash_openpgp_object.c
@@ -4,8 +4,21 @@
  *
  * Created on 2022/04/19, 10:12
  */
-#include "ccid_flash_openpgp_object.h"
+#include <stdbool.h>
+#include <stddef.h>
+//
+// プラットフォーム非依存コード
+//
+#include "ccid_openpgp.h"
+#include "ccid_openpgp_object.h"
+#include "fido_flash_define.h"
 
+// Flash ROM書込み時に実行した関数の参照を保持
+static void *m_flash_func = NULL;
+
+//
+// オブジェクトのRead／Write
+//
 bool ccid_flash_openpgp_object_read(CCID_APPLET applet_id, uint16_t obj_tag, bool *is_exist, uint8_t *buff, size_t *size)
 {
     return false;
@@ -21,6 +34,9 @@ bool ccid_flash_openpgp_object_delete_all(CCID_APPLET applet_id)
     return false;
 }
 
+//
+// コールバック関数群
+//
 void ccid_flash_openpgp_object_failed(void)
 {
 }
@@ -31,8 +47,34 @@ void ccid_flash_openpgp_object_gc_done(void)
 
 void ccid_flash_openpgp_object_record_updated(void)
 {
+    if (m_flash_func == NULL) {
+        return;
+    }
+
+    // 判定用の参照を初期化
+    void *flash_func = m_flash_func;
+    m_flash_func = NULL;
+
+    // 正常系の後続処理を実行
+    if (flash_func == ccid_flash_openpgp_object_write || 
+        flash_func == ccid_flash_openpgp_object_delete_all) {
+        ccid_openpgp_object_write_resume(true);
+    }
 }
 
 void ccid_flash_openpgp_object_record_deleted(void)
 {
+    if (m_flash_func == NULL) {
+        return;
+    }
+
+    // 判定用の参照を初期化
+    void *flash_func = m_flash_func;
+    m_flash_func = NULL;
+
+    // 正常系の後続処理を実行
+    if (flash_func == ccid_flash_openpgp_object_write || 
+        flash_func == ccid_flash_openpgp_object_delete_all) {
+        ccid_openpgp_object_write_resume(true);
+    }
 }
--- nRF5340_app/secure_device_app/wrapper/ccid_flash_piv_object.c
+++ nRF5340_app/secure_device_app/wrapper/ccid_flash_piv_object.c
@@ -4,8 +4,23 @@
  *
  * Created on 2022/04/19, 10:12
  */
-#include "ccid_flash_piv_object.h"
+#include <stdbool.h>
+#include <stddef.h>
+//
+// プラットフォーム非依存コード
+//
+#include "ccid_piv_authenticate.h"
+#include "ccid_piv_object.h"
+#include "ccid_piv_object_import.h"
+#include "ccid_ykpiv.h"
+#include "fido_flash_define.h"
 
+// Flash ROM書込み時に実行した関数の参照を保持
+static void *m_flash_func = NULL;
+
+//
+// オブジェクトのRead／Write
+//
 bool ccid_flash_piv_object_card_admin_key_read(uint8_t *key, size_t *key_size, uint8_t *key_alg, bool *is_exist)
 {
     return false;
@@ -46,6 +61,9 @@ bool ccid_flash_piv_object_data_erase(void)
     return false;
 }
 
+//
+// コールバック関数群
+//
 void ccid_flash_piv_object_failed(void)
 {
 }
@@ -56,8 +74,41 @@ void ccid_flash_piv_object_gc_done(void)
 
 void ccid_flash_piv_object_record_updated(void)
 {
+    if (m_flash_func == NULL) {
+        return;
+    }
+
+    // 判定用の参照を初期化
+    void *flash_func = m_flash_func;
+    m_flash_func = NULL;
+
+    // 正常系の後続処理を実行
+    if (flash_func == (void *)ccid_flash_piv_object_card_admin_key_write) {
+        ccid_ykpiv_ins_set_mgmkey_resume(true);
+    }
+    if (flash_func == (void *)ccid_flash_piv_object_private_key_write) {
+        ccid_ykpiv_ins_import_key_resume(true);
+    }
+    if (flash_func == (void *)ccid_flash_piv_object_data_write) {
+        ccid_piv_object_import_resume(true);
+    }
+    if (flash_func == (void *)ccid_flash_piv_object_pin_write) {
+        ccid_piv_object_pin_set_resume(true);
+    }
 }
 
 void ccid_flash_piv_object_record_deleted(void)
 {
+    if (m_flash_func == NULL) {
+        return;
+    }
+
+    // 判定用の参照を初期化
+    void *flash_func = m_flash_func;
+    m_flash_func = NULL;
+
+    // 正常系の後続処理を実行
+    if (flash_func == (void *)ccid_flash_piv_object_data_erase) {
+        ccid_ykpiv_ins_reset_resume(true);
+    }
 }
