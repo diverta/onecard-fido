f36ca905b3b20c00d19f92fe427904989d4ce9ca
Makoto Morita <makmorit@outlook.jp>
2022-04-29 10:50:46 +0900

    OATH対応事前作業
    ・OATH関連処理のエントリーモジュール／ヘッダーを新設

---
+++ CCID/oath_lib/ccid_oath.c
@@ -0,0 +1,6 @@
+/* 
+ * File:   ccid_oath.c
+ * Author: makmorit
+ *
+ * Created on 2022/04/29, 10:42
+ */
---
+++ CCID/oath_lib/ccid_oath.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   ccid_oath.h
+ * Author: makmorit
+ *
+ * Created on 2022/04/29, 10:42
+ */
+#ifndef CCID_OATH_H
+#define CCID_OATH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//
+// 関数群
+//
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CCID_OATH_H */
