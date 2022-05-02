f2a7e97552016fa1cd41ed9de260f56ff26caba4
Makoto Morita <makmorit@outlook.jp>
2022-05-02 16:23:07 +0900

    事前準備
    ・INSの定義を追加

---
+++ CCID/oath_lib/ccid_oath_define.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   ccid_oath_define.h
+ * Author: makmorit
+ *
+ * Created on 2022/05/02, 16:19
+ */
+#ifndef CCID_OATH_DEFINE_H
+#define CCID_OATH_DEFINE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define OATH_INS_PUT                0x01
+#define OATH_INS_SELECT             0xA4
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CCID_OATH_DEFINE_H */
