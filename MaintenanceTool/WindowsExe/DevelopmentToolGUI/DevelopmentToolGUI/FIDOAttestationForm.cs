3e838d71bf4c4805a948638a94b491563aca1533
makmorit <makmorit@outlook.jp>
2022-05-09 16:31:19 +0900

    FIDO認証器開発ツールの新設
    ・プログラムコードをエントリー
    ・再構築が必要なクラスについては、コメントアウトしています。
    （`HidMain`、`FormUtil`の２点を参照している部分）

---
+++ MaintenanceTool/WindowsExe/DevelopmentToolGUI/DevelopmentToolGUI/FIDOAttestationForm.cs
@@ -0,0 +1,138 @@
+﻿using MaintenanceToolGUI;
+using System;
+using System.Windows.Forms;
+
+namespace DevelopmentToolGUI
+{
+    public partial class FIDOAttestationForm : Form
+    {
+        // 親画面の参照を保持
+        private MainForm mainForm;
+
+        // 入力されたパラメーターを保持
+        public string CommandTitle = "";
+        public string KeyPath = "";
+        public string CertPath = "";
+
+        public FIDOAttestationForm(MainForm mf)
+        {
+            InitializeComponent();
+            InitFieldValue();
+            mainForm = mf;
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            // Cancelボタンにフォーカス
+            buttonCancel.Focus();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            // 画面項目を初期化し、この画面を閉じる
+            TerminateWindow(DialogResult.Cancel);
+        }
+
+        private void TerminateWindow(DialogResult dialogResult)
+        {
+            // 画面項目を初期化し、この画面を閉じる
+            InitFieldValue();
+            DialogResult = dialogResult;
+            Close();
+        }
+
+        private void InitFieldValue()
+        {
+            // 画面項目を初期値に設定
+            textKeyPath.Text = "";
+            textCertPath.Text = "";
+        }
+
+        private bool checkPathEntry(TextBox textBox, string errorMessage)
+        {
+            // 入力済みの場合はチェックOK
+            if (textBox.Text.Length > 0) {
+                return true;
+            }
+
+            // 未入力の場合はポップアップメッセージを表示して
+            // テキストボックスにフォーカスを移す
+            // FormUtil.ShowWarningMessage(this, MainForm.GetMaintenanceToolTitle(), errorMessage);
+            textBox.Focus();
+
+            return false;
+        }
+
+        private void buttonSelectKeyPath_Click(object sender, EventArgs e)
+        {
+            // FormUtil.SelectFilePath(openFileDialog1, ToolGUICommon.MSG_PROMPT_SELECT_PKEY_PATH, ToolGUICommon.FILTER_SELECT_PEM_PATH, textKeyPath);
+        }
+
+        private void buttonSelectCertPath_Click(object sender, EventArgs e)
+        {
+            // FormUtil.SelectFilePath(openFileDialog1, ToolGUICommon.MSG_PROMPT_SELECT_CRT_PATH, ToolGUICommon.FILTER_SELECT_CRT_PATH, textCertPath);
+        }
+
+        private void buttonInstall_Click(object sender, EventArgs e)
+        {
+            // USB HID接続がない場合はエラーメッセージを表示
+            if (mainForm.CheckUSBDeviceDisconnected()) {
+                return;
+            }
+            // ファイルパス入力チェック
+            if (checkPathEntry(textKeyPath, ToolGUICommon.MSG_PROMPT_SELECT_PKEY_PATH) == false) {
+                return;
+            }
+            if (checkPathEntry(textCertPath, ToolGUICommon.MSG_PROMPT_SELECT_CRT_PATH) == false) {
+                return;
+            }
+            // プロンプトで表示されるメッセージ
+            string message = string.Format("{0}\n\n{1}",
+                ToolGUICommon.MSG_INSTALL_SKEY_CERT,
+                ToolGUICommon.MSG_PROMPT_INSTL_SKEY_CERT);
+
+            // プロンプトを表示し、Yesの場合だけ処理を行う
+            /*
+            if (FormUtil.DisplayPromptPopup(this, message) == false) {
+                return;
+            }
+             */
+
+            // 画面入力値をパラメーターに保持
+            KeyPath = textKeyPath.Text;
+            CertPath = textCertPath.Text;
+            CommandTitle = ToolGUICommon.PROCESS_NAME_INSTALL_SKEY_CERT;
+
+            // 画面項目を初期化し、この画面を閉じる
+            TerminateWindow(DialogResult.OK);
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            // USB HID接続がない場合はエラーメッセージを表示
+            if (mainForm.CheckUSBDeviceDisconnected()) {
+                return;
+            }
+            // プロンプトで表示されるメッセージ
+            string message = string.Format("{0}\n\n{1}",
+                ToolGUICommon.MSG_ERASE_SKEY_CERT,
+                ToolGUICommon.MSG_PROMPT_ERASE_SKEY_CERT);
+
+            // 鍵・証明書削除
+            // プロンプトを表示し、Yesの場合だけ処理を行う
+            /*
+            if (FormUtil.DisplayPromptPopup(this, message) == false) {
+                return;
+            }
+             */
+
+            // 画面入力値をパラメーターに保持
+            KeyPath = "";
+            CertPath = "";
+            CommandTitle = ToolGUICommon.PROCESS_NAME_ERASE_SKEY_CERT;
+
+            // 画面項目を初期化し、この画面を閉じる
+            TerminateWindow(DialogResult.OK);
+        }
+    }
+}
---
+++ MaintenanceTool/WindowsExe/DevelopmentToolGUI/DevelopmentToolGUI/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace DevelopmentToolGUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonFIDO = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Location = new System.Drawing.Point(462, 12);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(110, 25);
+            this.buttonQuit.TabIndex = 7;
+            this.buttonQuit.Text = "終了";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 52);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(560, 322);
+            this.textBox1.TabIndex = 10;
+            // 
+            // buttonFIDO
+            // 
+            this.buttonFIDO.Location = new System.Drawing.Point(12, 12);
+            this.buttonFIDO.Name = "buttonFIDO";
+            this.buttonFIDO.Size = new System.Drawing.Size(180, 25);
+            this.buttonFIDO.TabIndex = 2;
+            this.buttonFIDO.Text = "FIDO設定";
+            this.buttonFIDO.UseVisualStyleBackColor = true;
+            this.buttonFIDO.Click += new System.EventHandler(this.buttonFIDO_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 386);
+            this.Controls.Add(this.buttonFIDO);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonQuit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonQuit;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonFIDO;
+    }
+}
+
