0e317474dc1e3833ec1ec2047d89156e16e68822
Makoto Morita <makmorit@outlook.jp>
2022-05-24 12:13:44 +0900

    Arduinoサンプルのリファクタリング
    ・`Adafruit_ST7735`に`Adafruit_ST77xx`の内容をマージして１本化

--- Research/TinyTFTLib/tft_sample/TFT_ST7735.cpp
+++ Research/TinyTFTLib/tft_sample/TFT_ST7735.cpp
@@ -1,5 +1,12 @@
 #include "TFT_ST7735.h"
-#include "Adafruit_ST77xx.h"
+#include <limits.h>
+#ifndef ARDUINO_STM32_FEATHER
+#include "pins_arduino.h"
+#include "wiring_private.h"
+#endif
+#include <SPI.h>
+
+#define SPI_DEFAULT_FREQ 32000000 ///< Default SPI data clock frequency
 
 /*!
     @brief  Instantiate ST7735 driver with default hardware SPI
@@ -11,6 +18,120 @@
 */
 TFT_ST7735::TFT_ST7735(uint16_t w, uint16_t h, int8_t cs, int8_t dc, int8_t rst)
     : Adafruit_SPITFT(w, h, cs, dc, rst) {}
+
+/*!
+    @brief  Companion code to the initiliazation tables. Reads and issues
+            a series of LCD commands stored in PROGMEM byte array.
+    @param  addr  Flash memory array with commands and data to send
+*/
+void TFT_ST7735::displayInit(const uint8_t *addr) {
+
+  uint8_t numCommands, cmd, numArgs;
+  uint16_t ms;
+
+  numCommands = pgm_read_byte(addr++); // Number of commands to follow
+  while (numCommands--) {              // For each command...
+    cmd = pgm_read_byte(addr++);       // Read command
+    numArgs = pgm_read_byte(addr++);   // Number of args to follow
+    ms = numArgs & ST_CMD_DELAY;       // If hibit set, delay follows args
+    numArgs &= ~ST_CMD_DELAY;          // Mask out delay bit
+    sendCommand(cmd, addr, numArgs);
+    addr += numArgs;
+
+    if (ms) {
+      ms = pgm_read_byte(addr++); // Read post-command delay time (ms)
+      if (ms == 255)
+        ms = 500; // If 255, delay for 500 ms
+      delay(ms);
+    }
+  }
+}
+
+/*!
+    @brief  Initialize ST77xx chip. Connects to the ST77XX over SPI and
+            sends initialization procedure commands
+    @param  freq  Desired SPI clock frequency
+*/
+void TFT_ST7735::begin(uint32_t freq) {
+  if (!freq) {
+    freq = SPI_DEFAULT_FREQ;
+  }
+  _freq = freq;
+
+  invertOnCommand = ST77XX_INVON;
+  invertOffCommand = ST77XX_INVOFF;
+
+  initSPI(freq, spiMode);
+}
+
+/*!
+    @brief  Initialization code common to all ST77XX displays
+    @param  cmdList  Flash memory array with commands and data to send
+*/
+void TFT_ST7735::commonInit(const uint8_t *cmdList) {
+  begin();
+
+  if (cmdList) {
+    displayInit(cmdList);
+  }
+}
+
+/*!
+  @brief  SPI displays set an address window rectangle for blitting pixels
+  @param  x  Top left corner x coordinate
+  @param  y  Top left corner x coordinate
+  @param  w  Width of window
+  @param  h  Height of window
+*/
+void TFT_ST7735::setAddrWindow(uint16_t x, uint16_t y, uint16_t w,
+                                    uint16_t h) {
+  x += _xstart;
+  y += _ystart;
+  uint32_t xa = ((uint32_t)x << 16) | (x + w - 1);
+  uint32_t ya = ((uint32_t)y << 16) | (y + h - 1);
+
+  writeCommand(ST77XX_CASET); // Column addr set
+  SPI_WRITE32(xa);
+
+  writeCommand(ST77XX_RASET); // Row addr set
+  SPI_WRITE32(ya);
+
+  writeCommand(ST77XX_RAMWR); // write to RAM
+}
+
+/*!
+    @brief  Set origin of (0,0) of display with offsets
+    @param  col  The offset from 0 for the column address
+    @param  row  The offset from 0 for the row address
+*/
+void TFT_ST7735::setColRowStart(int8_t col, int8_t row) {
+  _colstart = col;
+  _rowstart = row;
+}
+
+/*!
+ @brief  Change whether display is on or off
+ @param  enable True if you want the display ON, false OFF
+ */
+void TFT_ST7735::enableDisplay(boolean enable) {
+  sendCommand(enable ? ST77XX_DISPON : ST77XX_DISPOFF);
+}
+
+/*!
+ @brief  Change whether TE pin output is on or off
+ @param  enable True if you want the TE pin ON, false OFF
+ */
+void TFT_ST7735::enableTearing(boolean enable) {
+  sendCommand(enable ? ST77XX_TEON : ST77XX_TEOFF);
+}
+
+/*!
+ @brief  Change whether sleep mode is on or off
+ @param  enable True if you want sleep mode ON, false OFF
+ */
+void TFT_ST7735::enableSleep(boolean enable) {
+  sendCommand(enable ? ST77XX_SLPIN : ST77XX_SLPOUT);
+}
 
 // SCREEN INITIALIZATION ***************************************************
 // Rather than a bazillion writecommand() and writedata() calls, screen
--- Research/TinyTFTLib/tft_sample/TFT_ST7735.h
+++ Research/TinyTFTLib/tft_sample/TFT_ST7735.h
@@ -1,7 +1,64 @@
 #ifndef _TFT_ST7735H_
 #define _TFT_ST7735H_
 
+#include "Arduino.h"
+#include "Print.h"
+#include <Adafruit_GFX.h>
 #include <Adafruit_SPITFT.h>
+#include <Adafruit_SPITFT_Macros.h>
+
+#define ST7735_TFTWIDTH_80 80
+#define ST7735_TFTHEIGHT_160 160
+
+// special signifier for command lists
+#define ST_CMD_DELAY 0x80
+
+#define ST77XX_NOP 0x00
+#define ST77XX_SWRESET 0x01
+#define ST77XX_RDDID 0x04
+#define ST77XX_RDDST 0x09
+
+#define ST77XX_SLPIN 0x10
+#define ST77XX_SLPOUT 0x11
+#define ST77XX_PTLON 0x12
+#define ST77XX_NORON 0x13
+
+#define ST77XX_INVOFF 0x20
+#define ST77XX_INVON 0x21
+#define ST77XX_DISPOFF 0x28
+#define ST77XX_DISPON 0x29
+#define ST77XX_CASET 0x2A
+#define ST77XX_RASET 0x2B
+#define ST77XX_RAMWR 0x2C
+#define ST77XX_RAMRD 0x2E
+
+#define ST77XX_PTLAR 0x30
+#define ST77XX_TEOFF 0x34
+#define ST77XX_TEON 0x35
+#define ST77XX_MADCTL 0x36
+#define ST77XX_COLMOD 0x3A
+
+#define ST77XX_MADCTL_MY 0x80
+#define ST77XX_MADCTL_MX 0x40
+#define ST77XX_MADCTL_MV 0x20
+#define ST77XX_MADCTL_ML 0x10
+#define ST77XX_MADCTL_RGB 0x00
+
+#define ST77XX_RDID1 0xDA
+#define ST77XX_RDID2 0xDB
+#define ST77XX_RDID3 0xDC
+#define ST77XX_RDID4 0xDD
+
+// Some ready-made 16-bit ('565') color settings:
+#define ST77XX_BLACK 0x0000
+#define ST77XX_WHITE 0xFFFF
+#define ST77XX_RED 0xF800
+#define ST77XX_GREEN 0x07E0
+#define ST77XX_BLUE 0x001F
+#define ST77XX_CYAN 0x07FF
+#define ST77XX_MAGENTA 0xF81F
+#define ST77XX_YELLOW 0xFFE0
+#define ST77XX_ORANGE 0xFC00
 
 // Some register settings
 #define ST7735_MADCTL_BGR 0x08
@@ -40,10 +97,25 @@ class TFT_ST7735 : public Adafruit_SPITFT {
 public:
   TFT_ST7735(uint16_t w, uint16_t h, int8_t CS, int8_t RS, int8_t RST = -1);
 
+  void setAddrWindow(uint16_t x, uint16_t y, uint16_t w, uint16_t h);
+  void enableDisplay(boolean enable);
+  void enableTearing(boolean enable);
+  void enableSleep(boolean enable);
+  
   // Differences between displays (usu. identified by colored tab on
   // plastic overlay) are odd enough that we need to do this 'by hand':
   void initR(void); 
   void setRotation(uint8_t m);
+
+protected:
+  uint8_t _colstart = 0,   ///< Some displays need this changed to offset
+      _rowstart = 0,       ///< Some displays need this changed to offset
+      spiMode = SPI_MODE0; ///< Certain display needs MODE3 instead
+
+  void begin(uint32_t freq = 0);
+  void commonInit(const uint8_t *cmdList);
+  void displayInit(const uint8_t *addr);
+  void setColRowStart(int8_t col, int8_t row);
 };
 
 #endif // _TFT_ST7735H_
