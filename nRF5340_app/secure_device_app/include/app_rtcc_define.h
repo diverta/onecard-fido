82c6dd187827a913204d32e98ffb0ccaa3103c85
Makoto Morita <makmorit@outlook.jp>
2022-06-01 16:19:33 +0900

    現在時刻取得対応
    ・初期設定：設定時刻の永続化処理を実装

--- nRF5340_app/secure_device_app/include/app_rtcc_define.h
+++ nRF5340_app/secure_device_app/include/app_rtcc_define.h
@@ -15,7 +15,49 @@ extern "C" {
 #define RV3028C7_ADDRESS                0x52
 
 // レジスターアドレス
+#define RV3028C7_REG_STATUS             0x0e
+#define RV3028C7_REG_CONTROL_1          0x0f
 #define RV3028C7_REG_CONTROL_2          0x10
+#define RV3028C7_REG_CLOCK_SECONDS      0x00
+#define RV3028C7_REG_EEPROM_ADDR        0x25
+#define RV3028C7_REG_EEPROM_DATA        0x26
+#define RV3028C7_REG_EEPROM_CMD         0x27
+#define RV3028C7_REG_EEPROM_CLKOUT      0x35
+#define RV3028C7_REG_EEPROM_BACKUP      0x37
+
+// Bits in Status Register
+#define RV3028C7_BIT_STATUS_EEBUSY      7
+#define RV3028C7_BIT_STATUS_CLKF        6
+#define RV3028C7_BIT_STATUS_BSF         5
+#define RV3028C7_BIT_STATUS_UF          4
+#define RV3028C7_BIT_STATUS_TF          3
+#define RV3028C7_BIT_STATUS_AF          2
+#define RV3028C7_BIT_STATUS_EVF         1
+#define RV3028C7_BIT_STATUS_PORF        0
+
+// Bits in Control1 Register
+#define RV3028C7_BIT_CTRL1_TRPT         7
+#define RV3028C7_BIT_CTRL1_WADA         5
+#define RV3028C7_BIT_CTRL1_USEL         4
+#define RV3028C7_BIT_CTRL1_EERD         3
+#define RV3028C7_BIT_CTRL1_TE           2
+#define RV3028C7_BIT_CTRL1_TD1          1
+#define RV3028C7_BIT_CTRL1_TD0          0
+
+// Commands for EEPROM Command Register
+#define RV3028C7_CMD_EEPROM_FIRST               0x00
+#define RV3028C7_CMD_EEPROM_UPDATE              0x11
+#define RV3028C7_CMD_EEPROM_REFRESH             0x12
+#define RV3028C7_CMD_EEPROM_WRITE_SINGLE        0x21
+#define RV3028C7_CMD_EEPROM_READ_SINGLE         0x22
+
+// Bits in EEPROM Backup Register
+#define RV3028C7_BIT_EEPROM_BACKUP_TCE          5       // Trickle Charge Enable Bit
+#define RV3028C7_BIT_EEPROM_BACKUP_FEDE         4       // Fast Edge Detection Enable Bit (for Backup Switchover Mode)
+#define RV3028C7_SHIFT_EEPROM_BACKUP_BSM        2       // Backup Switchover Mode shift
+#define RV3028C7_SHIFT_EEPROM_BACKUP_TCR        0       // Trickle Charge Resistor shift
+#define RV3028C7_MASK_EEPROM_BACKUP_BSM_CLEAR   0xf3    // 0b11110011 = Backup Switchover Mode clear
+#define RV3028C7_MASK_EEPROM_BACKUP_TCR_CLEAR   0xfc    // 0b11111100 = Trickle Charge Resistor clear
 
 #ifdef __cplusplus
 }
--- nRF5340_app/secure_device_app/src/app_rtcc.c
+++ nRF5340_app/secure_device_app/src/app_rtcc.c
@@ -49,6 +49,182 @@ static bool read_register(uint8_t reg_addr, uint8_t *reg_val)
     return true;
 }
 
+static bool write_register(uint8_t reg_addr, uint8_t reg_val)
+{
+    write_buff[0] = reg_addr;
+    write_buff[1] = reg_val;
+
+    // Write to device. STOP after this
+    msgs[0].buf = write_buff;
+    msgs[0].len = 2U;
+    msgs[0].flags = I2C_MSG_WRITE | I2C_MSG_STOP;
+
+    if (i2c_transfer(i2c_dev, &msgs[0], 2, RV3028C7_ADDRESS) != 0) {
+        LOG_DBG("i2c_transfer error");
+        return false;
+    }
+
+    return true;
+}
+
+static bool wait_for_eeprom()
+{
+    uint8_t reg_val;
+    for (uint8_t c = 0; c < 10; c++) {
+        // ステータスレジスターの値を取得
+        if (read_register(RV3028C7_REG_STATUS, &reg_val) == false) {
+            return false;
+        }
+        if ((reg_val & (1 << RV3028C7_BIT_STATUS_EEBUSY)) == 0) {
+            // ステータスがBUSYでなければ終了
+            return true;
+        }
+        // 10ms wait
+        k_sleep(K_MSEC(10));
+    }
+    // タイムアウトの場合はfalse
+    return false;
+}
+
+static bool disable_auto_refresh_with_eerd_bit(uint8_t *ctr1_reg_val)
+{
+    // Disable auto refresh by writing 1 to EERD control bit in CTRL1 register
+    if (read_register(RV3028C7_REG_CONTROL_1, ctr1_reg_val) == false) {
+        return false;
+    }
+    *ctr1_reg_val |= (1 << RV3028C7_BIT_CTRL1_EERD);
+    if (write_register(RV3028C7_REG_CONTROL_1, *ctr1_reg_val) == false) {
+        return false;
+    }
+    return true;
+}
+
+static bool reenable_auto_refresh_with_eerd_bit(uint8_t *ctr1_reg_val)
+{
+    // Reenable auto refresh by writing 0 to EERD control bit in CTRL1 register
+    if (read_register(RV3028C7_REG_CONTROL_1, ctr1_reg_val) == false) {
+        return false;
+    }
+    if (*ctr1_reg_val == 0x00) {
+        return false;
+    }
+    *ctr1_reg_val &= ~(1 << RV3028C7_BIT_CTRL1_EERD);
+    if (write_register(RV3028C7_REG_CONTROL_1, *ctr1_reg_val) == false) {
+        return false;
+    }
+    return true;
+}
+bool read_eeprom_backup_register(uint8_t reg_addr, uint8_t *reg_val)
+{
+    if (wait_for_eeprom() == false) {
+        return false;
+    }
+
+    // Disable auto refresh by writing 1 to EERD control bit in CTRL1 register
+    uint8_t ctrl1;
+    if (disable_auto_refresh_with_eerd_bit(&ctrl1) == false) {
+        return false;
+    }
+
+    // Read EEPROM Register
+    if (write_register(RV3028C7_REG_EEPROM_ADDR, reg_addr) == false) {
+        return false;
+    }
+    if (write_register(RV3028C7_REG_EEPROM_CMD, RV3028C7_CMD_EEPROM_FIRST) == false) {
+        return false;
+    }
+    if (write_register(RV3028C7_REG_EEPROM_CMD, RV3028C7_CMD_EEPROM_READ_SINGLE) == false) {
+        return false;
+    }
+    if (wait_for_eeprom() == false) {
+        return false;
+    }
+    if (read_register(RV3028C7_REG_EEPROM_DATA, reg_val) == false) {
+        return false;
+    }
+    if (wait_for_eeprom() == false) {
+        return false;
+    }
+
+    // Reenable auto refresh by writing 0 to EERD control bit in CTRL1 register
+    if (reenable_auto_refresh_with_eerd_bit(&ctrl1) == false) {
+        return false;
+    }
+
+    return true;
+}
+
+static bool write_eeprom_backup_register(uint8_t reg_addr, uint8_t reg_val)
+{
+    if (wait_for_eeprom() == false) {
+        return false;
+    }
+
+    // Disable auto refresh by writing 1 to EERD control bit in CTRL1 register
+    uint8_t ctrl1;
+    if (disable_auto_refresh_with_eerd_bit(&ctrl1) == false) {
+        return false;
+    }
+
+    // Write Configuration RAM Register
+    if (write_register(reg_addr, reg_val) == false) {
+        return false;
+    }
+
+    // Update EEPROM (All Configuration RAM -> EEPROM)
+    if (write_register(RV3028C7_REG_EEPROM_CMD, RV3028C7_CMD_EEPROM_FIRST) == false) {
+        return false;
+    }
+    if (write_register(RV3028C7_REG_EEPROM_CMD, RV3028C7_CMD_EEPROM_UPDATE) == false) {
+        return false;
+    }
+    if (wait_for_eeprom() == false) {
+        return false;
+    }
+
+    // Reenable auto refresh by writing 0 to EERD control bit in CTRL1 register
+    if (reenable_auto_refresh_with_eerd_bit(&ctrl1) == false) {
+        return false;
+    }
+    if (wait_for_eeprom() == false) {
+        return false;
+    }
+
+    return true;
+}
+
+static bool set_backup_switchover_mode(uint8_t val)
+{
+    if (val > 3) {
+        return false;
+    }
+
+    // Read EEPROM Backup Register (0x37)
+    uint8_t backup_reg_val;
+    if (read_eeprom_backup_register(RV3028C7_REG_EEPROM_BACKUP, &backup_reg_val) == false) {
+        LOG_ERR("Read EEPROM backup register fail");
+        return false;
+    }
+    if (backup_reg_val == 0xFF) {
+        LOG_ERR("Invalid EEPROM backup register value");
+        return false;
+    }
+
+    // Ensure FEDE Bit is set to 1
+    backup_reg_val |= (1 << RV3028C7_BIT_EEPROM_BACKUP_FEDE);
+    // Set BSM Bits (Backup Switchover Mode)
+    //  Clear BSM Bits of EEPROM Backup Register
+    backup_reg_val &= RV3028C7_MASK_EEPROM_BACKUP_BSM_CLEAR;
+    //  Shift values into EEPROM Backup Register
+    backup_reg_val |= (val << RV3028C7_SHIFT_EEPROM_BACKUP_BSM);
+
+    // Write EEPROM Backup Register
+    if (write_eeprom_backup_register(RV3028C7_REG_EEPROM_BACKUP, backup_reg_val) == false) {
+        LOG_ERR("Write EEPROM backup register fail");
+        return false;
+    }
+    return true;
+}
 //
 // RTCCの初期化
 //
@@ -61,6 +237,20 @@ bool app_rtcc_initialize(void)
     }
     if (c2 != 0x00) {
         LOG_ERR("RTCC is not available");
+        return false;
+    }
+    //
+    // 設定時刻の永続化のため、
+    // VDD_NRFからの電源供給がなくなった場合、
+    // 自動的に外部バックアップ電源に切り替えるよう設定
+    //
+    // 0 = Switchover disabled
+    // 1 = Direct Switching Mode
+    // 2 = Standby Mode
+    // 3 = Level Switching Mode
+    uint8_t val = 0x03;
+    if (set_backup_switchover_mode(val) == false) {
+        LOG_ERR("RTCC backup switchover mode is not available");
         return false;
     }
     return true;
