1df4f1ec816df672d54cd93e6b3ecc82f06ec629
Makoto Morita <makmorit@outlook.jp>
2022-06-09 17:14:07 +0900

    低レベル関数群の実装
    ・ライブラリー`tiny_tft_lib`、モジュール`tiny_tft`を新設

---
+++ TFT/tiny_tft_lib/tiny_tft.c
@@ -0,0 +1,6 @@
+/* 
+ * File:   tiny_tft.c
+ * Author: makmorit
+ *
+ * Created on 2022/06/09, 17:11
+ */
---
+++ TFT/tiny_tft_lib/tiny_tft.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   tiny_tft.h
+ * Author: makmorit
+ *
+ * Created on 2022/06/09, 17:11
+ */
+#ifndef TINY_TFT_H
+#define TINY_TFT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TINY_TFT_H */
