86d982c4723c5e390f5e9696053ea7fa941a20bc
Makoto Morita <makmorit@outlook.jp>
2022-06-13 11:24:01 +0900

    ダミースレーブアプリの制作
    ・初版
    ・サンプルアプリの`tft.setRotation(3)`実行文までの92バイトを、シリアルモニター上にダンプさせます。

---
+++ TFT/dummy_slave/dummy_slave.ino
@@ -0,0 +1,55 @@
+#include<SPI.h>
+
+byte bytes[100];
+uint32_t cnt = 0;
+
+volatile boolean received;
+volatile byte Slavereceived;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("");
+  Serial.println("SPI dummy slave start");
+
+  // Sets MISO as OUTPUT
+  // Have to Send data to Master IN
+  pinMode(MISO, OUTPUT);
+
+  //Turn on SPI in Slave Mode
+  SPCR |= _BV(SPE);
+
+  //Interrupt ON is set for SPI commnucation
+  received = false;
+  SPI.attachInterrupt();
+}
+
+ISR (SPI_STC_vect) {
+  // Value received from master 
+  // if store in variable slavereceived
+  Slavereceived = SPDR;
+  // Sets received as True
+  received = true;
+}
+
+void loop() {
+  if (received) {
+    bytes[cnt++] = Slavereceived;
+    received= false;
+    if (cnt == 92) {
+      dump_byte_array(bytes, cnt);
+    }
+  }
+}
+
+void dump_byte_array(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+    if (i % 16 == 15) {
+      Serial.println("");
+    } else {
+      Serial.print(" ");
+    }
+  }
+}
