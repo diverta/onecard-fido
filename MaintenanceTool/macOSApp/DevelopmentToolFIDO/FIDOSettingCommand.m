89b73801e3a4d5955a18fa6e2563b92adce79212
Makoto Morita <makmorit@outlook.jp>
2022-06-14 16:27:31 +0900

    FIDO設定機能の移設
    ・FIDO鍵・証明書設定画面の表示処理を実装

--- MaintenanceTool/macOSApp/DevelopmentToolFIDO/FIDOSettingCommand.m
+++ MaintenanceTool/macOSApp/DevelopmentToolFIDO/FIDOSettingCommand.m
@@ -6,6 +6,7 @@
 //
 #import "AppCommonMessage.h"
 #import "AppDefine.h"
+#import "FIDOAttestationWindow.h"
 #import "FIDOSettingCommand.h"
 #import "FIDOSettingWindow.h"
 #import "ToolLogFile.h"
@@ -16,6 +17,7 @@
     @property (nonatomic) NSWindow                     *parentWindow;
     // 画面の参照を保持
     @property (nonatomic) FIDOSettingWindow            *fidoSettingWindow;
+    @property (nonatomic) FIDOAttestationWindow        *fidoAttestationWindow;
 
 @end
 
@@ -26,6 +28,9 @@
         if (self) {
             // 画面のインスタンスを生成
             [self setFidoSettingWindow:[[FIDOSettingWindow alloc] initWithWindowNibName:@"FIDOSettingWindow"]];
+            [self setFidoAttestationWindow:[[FIDOAttestationWindow alloc] initWithWindowNibName:@"FIDOAttestationWindow"]];
+            // このクラスの参照を引き渡し
+            [[self fidoAttestationWindow] setCommandRef:self];
         }
         return self;
     }
@@ -52,7 +57,8 @@
         // 実行コマンドにより処理分岐
         switch ([[self fidoSettingWindow] commandToPerform]) {
             case COMMAND_FIDO_ATTESTATION:
-                // TODO: FIDO鍵・証明書インストール画面を表示
+                // FIDO鍵・証明書インストール画面を表示
+                [self fidoAttestationWindowWillOpen];
                 break;
             case COMMAND_FIDO_ATTESTATION_RESET:
                 // TODO: FIDO鍵・証明書を消去
@@ -64,6 +70,33 @@
     }
 
 #pragma mark - For FIDO attestation window
+
+    - (void)fidoAttestationWindowWillOpen {
+        // 画面に親画面参照をセット
+        NSWindow *parentWindow = [self parentWindow];
+        [[self fidoAttestationWindow] setParentWindowRef:parentWindow];
+        // ダイアログをモーダルで表示
+        NSWindow *dialog = [[self fidoAttestationWindow] window];
+        FIDOSettingCommand * __weak weakSelf = self;
+        [parentWindow beginSheet:dialog completionHandler:^(NSModalResponse response){
+            // ダイアログが閉じられた時の処理
+            [weakSelf fidoAttestationWindowDidClose:weakSelf modalResponse:response];
+        }];
+    }
+
+    - (void)fidoAttestationWindowDidClose:(id)sender modalResponse:(NSInteger)modalResponse {
+        // 画面を閉じる
+        [[self fidoAttestationWindow] close];
+        // 実行コマンドにより処理分岐
+        switch ([[self fidoAttestationWindow] commandToPerform]) {
+            case COMMAND_FIDO_ATTESTATION_INSTALL:
+                // TODO: FIDO鍵・証明書をインストール
+                break;
+            default:
+                // メイン画面に制御を戻す
+                break;
+        }
+    }
 
     - (bool)checkUSBHIDConnection {
         // TODO: USBポートに接続されていない場合はfalse
