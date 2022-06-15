94e7de0cb3ecdd1e418a9a9a04c597b8096c7132
Makoto Morita <makmorit@outlook.jp>
2022-06-15 17:35:14 +0900

    永続化機能の実装
    ・プラットフォーム非依存コードの実装
    （データ更新関連のみ）

--- CCID/oath_lib/ccid_oath_object.h
+++ CCID/oath_lib/ccid_oath_object.h
@@ -7,10 +7,19 @@
 #ifndef CCID_OATH_OBJECT_H
 #define CCID_OATH_OBJECT_H
 
+#include <stdbool.h>
+#include <stdint.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+//
+// 関数群
+//
+uint16_t    ccid_oath_object_account_set(char *account_name, char *secret, uint8_t property, uint8_t *challange);
+void        ccid_oath_object_write_retry(void);
+void        ccid_oath_object_write_resume(bool success);
 
 #ifdef __cplusplus
 }
