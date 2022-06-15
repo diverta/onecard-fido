cb374f9efb950ef55d8f02824fe373be9c3fb94a
Makoto Morita <makmorit@outlook.jp>
2022-06-15 13:40:48 +0900

    永続化機能の実装
    ・永続化機能モジュールを新設

---
+++ CCID/oath_lib/ccid_oath_object.c
@@ -0,0 +1,6 @@
+/* 
+ * File:   ccid_oath_object.c
+ * Author: makmorit
+ *
+ * Created on 2022/06/15, 13:36
+ */
---
+++ CCID/oath_lib/ccid_oath_object.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   ccid_oath_object.h
+ * Author: makmorit
+ *
+ * Created on 2022/06/15, 13:36
+ */
+#ifndef CCID_OATH_OBJECT_H
+#define CCID_OATH_OBJECT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CCID_OATH_OBJECT_H */
