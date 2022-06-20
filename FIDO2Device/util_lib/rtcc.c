4e69194bcea6ca5b227b755aa7401139a4295623
Makoto Morita <makmorit@outlook.jp>
2022-06-20 10:45:59 +0900

    時刻同期機能の実装
    ・時刻同期用の共通関数を実装

--- FIDO2Device/util_lib/rtcc.c
+++ FIDO2Device/util_lib/rtcc.c
@@ -4,8 +4,6 @@
  *
  * Created on 2022/06/01, 12:06
  */
-#include "app_rtcc.h"
-
 // 業務処理／HW依存処理間のインターフェース
 #include "fido_platform.h"
 
@@ -41,4 +39,22 @@ void rtcc_init(void)
 
     // 現在時刻を表示
     fido_log_info("RTCC is available. Current timestamp: %s", log_strdup(work_buf));
+}
+
+bool rtcc_update_timestamp_by_unixtime(uint32_t unixtime)
+{
+    // カウンターをRTCCに設定
+    uint8_t timezone_diff_hours = 9;
+    if (app_rtcc_set_timestamp(unixtime, timezone_diff_hours) == false) {
+        return false;
+    }
+
+    // 設定された現在時刻を取得
+    if (app_rtcc_get_timestamp(work_buf, sizeof(work_buf)) == false) {
+        return false;
+    }
+
+    // 現在時刻を表示
+    fido_log_info("Current timestamp (updated by unixtime): %s", log_strdup(work_buf));
+    return true;
 }
