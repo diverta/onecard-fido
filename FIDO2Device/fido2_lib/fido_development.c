0b9c5cf2b924b7dc5a151529f9d19cae3aa4ad1d
Makoto Morita <makmorit@outlook.jp>
2022-06-27 12:22:20 +0900

    開発ツール用の鍵インストール処理を追加
    ・疎通確認用の仮実装

--- FIDO2Device/fido2_lib/fido_development.c
+++ FIDO2Device/fido2_lib/fido_development.c
@@ -18,6 +18,10 @@
 fido_log_module_register(fido_development);
 #endif
 
+// 関数群
+static void command_install_attestation(void);
+static void command_reset_attestation(void);
+
 // トランスポート種別を保持
 static TRANSPORT_TYPE m_transport_type;
 
@@ -43,6 +47,12 @@ void fido_development_command(TRANSPORT_TYPE transport_type)
     // リクエストデータ受信後に実行すべき処理を判定
     uint8_t cmd = get_command_byte();
     switch (cmd) {
+        case MNT_COMMAND_INSTALL_ATTESTATION:
+            command_install_attestation();
+            break;
+        case MNT_COMMAND_RESET_ATTESTATION:
+            command_reset_attestation();
+            break;
         default:
             break;
     }
@@ -56,10 +66,77 @@ void fido_development_command_report_sent(void)
     // 全フレーム送信後に行われる後続処理を実行
     uint8_t cmd = get_command_byte();
     switch (cmd) {
+        case MNT_COMMAND_INSTALL_ATTESTATION:
+            fido_log_info("Install FIDO attestation end");
+            break;
+        case MNT_COMMAND_RESET_ATTESTATION:
+            fido_log_info("Reset FIDO attestation end");
+            break;
         default:
             break;
     }
 
     // LEDをアイドル状態に遷移
     fido_status_indicator_idle();
+}
+
+//
+// レスポンスデータ格納領域
+//
+static uint8_t response_buffer[1024];
+
+static void send_command_response(uint8_t ctap2_status, size_t length)
+{
+    // １バイトめにステータスコードをセット
+    response_buffer[0] = ctap2_status;
+
+    // レスポンスデータを送信パケットに設定し送信
+    if (m_transport_type == TRANSPORT_HID) {
+        uint32_t cid = fido_hid_receive_header()->CID;
+        uint8_t cmd = fido_hid_receive_header()->CMD;
+        fido_hid_send_command_response(cid, cmd, response_buffer, length);
+    } 
+}
+
+static void send_command_error_response(uint8_t ctap2_status) 
+{
+    // レスポンスデータを送信パケットに設定し送信
+    //   エラーなので送信バイト数＝１
+    send_command_response(ctap2_status, 1);
+}
+
+//
+// Install FIDO attestation
+//
+static void command_install_attestation(void)
+{
+    uint8_t *data = fido_hid_receive_apdu()->data;
+    uint16_t length = fido_hid_receive_apdu()->Lc;
+
+    // 元データチェック
+    if (data == NULL || length == 0) {
+        send_command_error_response(CTAP2_ERR_VENDOR_FIRST);
+        return;
+    }
+    fido_log_info("Install FIDO attestation start");
+
+    // Flash ROMに登録する鍵・証明書データを準備
+    if (fido_flash_skey_cert_data_prepare(data, length) == false) {
+        send_command_error_response(CTAP2_ERR_VENDOR_FIRST);
+        return;
+    }
+
+    // TODO: 仮の実装です
+    send_command_error_response(CTAP1_ERR_SUCCESS);
+}
+
+//
+// Erase FIDO attestation
+//
+static void command_reset_attestation(void)
+{
+    fido_log_info("Reset FIDO attestation start");
+
+    // TODO: 仮の実装です
+    send_command_error_response(CTAP1_ERR_SUCCESS);
 }
