0da6165d9b38322c17c340c3a07922ff16c5108e
Makoto Morita <makmorit@outlook.jp>
2022-07-03 12:18:01 +0900

    BOS／OSディスクリプター要求対応
    ・OSディスクリプターをモジュール内に保持

--- nRF5340_app/secure_device_app/src/app_usb.c
+++ nRF5340_app/secure_device_app/src/app_usb.c
@@ -16,6 +16,54 @@
 LOG_MODULE_REGISTER(app_usb);
 
 //
+// OSディスクリプターを保持
+//
+static const uint8_t msos2_descriptor[] = {
+    // Microsoft OS 2.0 descriptor set header (table 10)
+    0x0A, 0x00,             // Descriptor size (10 bytes)
+    0x00, 0x00,             // MS OS 2.0 descriptor set header
+    0x00, 0x00, 0x03, 0x06, // Windows version (8.1) (0x06030000)
+    0xB2, 0x00,             // Size, MS OS 2.0 descriptor set
+
+    // Microsoft OS 2.0 configuration subset header
+    0x08, 0x00,             // Descriptor size (8 bytes)
+    0x01, 0x00,             // MS OS 2.0 configuration subset header
+    0x00,                   // bConfigurationValue
+    0x00,                   // Reserved
+    0xA8, 0x00,             // Size, MS OS 2.0 configuration subset
+
+    // Microsoft OS 2.0 function subset header
+    0x08, 0x00,             // Descriptor size (8 bytes)
+    0x02, 0x00,             // MS OS 2.0 function subset header
+    0x01,                   // First interface number
+    0x00,                   // Reserved
+    0xA0, 0x00,             // Size, MS OS 2.0 function subset
+
+    // Microsoft OS 2.0 compatible ID descriptor (table 13)
+    0x14, 0x00,             // wLength
+    0x03, 0x00,             // MS_OS_20_FEATURE_COMPATIBLE_ID
+    'W',  'I',  'N',  'U',  'S',  'B',  0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+
+    0x84, 0x00,             // wLength:
+    0x04, 0x00,             // wDescriptorType: MS_OS_20_FEATURE_REG_PROPERTY: 0x04 (Table 9)
+    0x07, 0x00,             // wPropertyDataType: REG_MULTI_SZ (Table 15)
+    0x2A, 0x00,             // wPropertyNameLength:
+    // bPropertyName: "DeviceInterfaceGUID"
+    'D', 0x00, 'e', 0x00, 'v', 0x00, 'i', 0x00, 'c', 0x00, 'e', 0x00, 'I', 0x00,
+    'n', 0x00, 't', 0x00, 'e', 0x00, 'r', 0x00, 'f', 0x00, 'a', 0x00, 'c', 0x00,
+    'e', 0x00, 'G', 0x00, 'U', 0x00, 'I', 0x00, 'D', 0x00, 's', 0x00, 0x00, 0x00,
+    0x50, 0x00,             // wPropertyDataLength
+    // bPropertyData: "{244eb29e-e090-4e49-81fe-1f20f8d3b8f4}"
+    '{', 0x00, '2', 0x00, '4', 0x00, '4', 0x00, 'E', 0x00, 'B', 0x00, '2', 0x00,
+    '9', 0x00, 'E', 0x00, '-', 0x00, 'E', 0x00, '0', 0x00, '9', 0x00, '0', 0x00,
+    '-', 0x00, '4', 0x00, 'E', 0x00, '4', 0x00, '9', 0x00, '-', 0x00, '8', 0x00,
+    '1', 0x00, 'F', 0x00, 'E', 0x00, '-', 0x00, '1', 0x00, 'F', 0x00, '2', 0x00,
+    '0', 0x00, 'F', 0x00, '8', 0x00, 'D', 0x00, '3', 0x00, 'B', 0x00, '8', 0x00,
+    'F', 0x00, '4', 0x00, '}', 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+//
 // USBデバイスのステータスを管理
 //
 static void status_cb(enum usb_dc_status_code status, const uint8_t *param)
