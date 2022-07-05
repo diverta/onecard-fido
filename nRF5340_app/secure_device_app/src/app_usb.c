c5403c3c76ae2ee8a6f6ba23736bd0ad04c1e16d
Makoto Morita <makmorit@outlook.jp>
2022-07-05 10:34:29 +0900

    BOS／OSディスクリプター要求対応
    ・BOSディスクリプターをモジュール内に保持

--- nRF5340_app/secure_device_app/src/app_usb.c
+++ nRF5340_app/secure_device_app/src/app_usb.c
@@ -6,7 +6,9 @@
  */
 #include <zephyr/types.h>
 #include <zephyr.h>
+#include <sys/byteorder.h>
 #include <usb/usb_device.h>
+#include <usb/bos.h>
 
 #include "app_event.h"
 #include "app_usb_hid.h"
@@ -61,6 +63,48 @@ static const uint8_t msos2_descriptor[] = {
     '1', 0x00, 'F', 0x00, 'E', 0x00, '-', 0x00, '1', 0x00, 'F', 0x00, '2', 0x00,
     '0', 0x00, 'F', 0x00, '8', 0x00, 'D', 0x00, '3', 0x00, 'B', 0x00, '8', 0x00,
     'F', 0x00, '4', 0x00, '}', 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+//
+// BOSディスクリプターを保持
+//
+USB_DEVICE_BOS_DESC_DEFINE_CAP struct usb_bos_msosv2_desc {
+    struct usb_bos_platform_descriptor platform;
+    struct usb_bos_capability_msos cap;
+} __packed bos_cap_msosv2 = {
+    /* Microsoft OS 2.0 Platform Capability Descriptor
+     * See https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/
+     * microsoft-defined-usb-descriptors
+     * Adapted from the source:
+     * https://github.com/sowbug/weblight/blob/master/firmware/webusb.c
+     * (BSD-2) Thanks http://janaxelson.com/files/ms_os_20_descriptors.c
+     */
+    .platform = {
+        .bLength = sizeof(struct usb_bos_platform_descriptor)
+                + sizeof(struct usb_bos_capability_msos),
+        .bDescriptorType = USB_DESC_DEVICE_CAPABILITY,
+        .bDevCapabilityType = USB_BOS_CAPABILITY_PLATFORM,
+        .bReserved = 0,
+        .PlatformCapabilityUUID = {
+            /**
+             * MS OS 2.0 Platform Capability ID
+             * D8DD60DF-4589-4CC7-9CD2-659D9E648A9F
+             */
+            0xDF, 0x60, 0xDD, 0xD8,
+            0x89, 0x45,
+            0xC7, 0x4C,
+            0x9C, 0xD2,
+            0x65, 0x9D, 0x9E, 0x64, 0x8A, 0x9F,
+        },
+    },
+    .cap = {
+        /* Windows version (8.1) (0x06030000) */
+        .dwWindowsVersion = sys_cpu_to_le32(0x06030000),
+        .wMSOSDescriptorSetTotalLength =
+                sys_cpu_to_le16(sizeof(msos2_descriptor)),
+        .bMS_VendorCode = 0x02,
+        .bAltEnumCode = 0x00
+    },
 };
 
 //
