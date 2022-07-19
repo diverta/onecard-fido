35ee505688ec084d79e7695bf37b96468e538bd1
Makoto Morita <makmorit@outlook.jp>
2022-07-19 10:17:31 +0900

    FIDO設定画面の新設
    ・PINコード設定画面をFIDO設定画面から開くことができるよう修正

--- MaintenanceTool/macOSApp/MaintenanceToolFIDO/FIDOSettingWindow.m
+++ MaintenanceTool/macOSApp/MaintenanceToolFIDO/FIDOSettingWindow.m
@@ -6,6 +6,7 @@
 //
 #import "AppCommonMessage.h"
 #import "AppDefine.h"
+#import "FIDOPinSettingWindow.h"
 #import "FIDOSettingCommand.h"
 #import "FIDOSettingWindow.h"
 #import "ToolCommonFunc.h"
@@ -15,6 +16,8 @@
 
     // 親画面の参照を保持
     @property (nonatomic) NSWindow                         *parentWindow;
+    // 画面の参照を保持
+    @property (nonatomic) FIDOPinSettingWindow             *pinSettingWindow;
     // コマンドクラスの参照を保持
     @property (nonatomic, weak) id                          fidoSettingCommandRef;
     // ヘルスチェック処理のパラメーターを保持
@@ -25,7 +28,11 @@
 @implementation FIDOSettingWindow
 
     - (void)windowDidLoad {
+        // 画面のインスタンスを生成
         [super windowDidLoad];
+        if ([self pinSettingWindow] == nil) {
+            [self setPinSettingWindow:[[FIDOPinSettingWindow alloc] initWithWindowNibName:@"FIDOPinSettingWindow"]];
+        }
     }
 
     - (void)setParentWindowRef:(id)ref withCommandRef:(id)commandRef withParameterRef:(id)parameterRef {
@@ -42,11 +49,12 @@
     }
 
     - (IBAction)buttonSetPinParamDidPress:(id)sender {
-        // TODO: PIN番号入力画面を開く
-        [[self commandParameterRef] setCommand:COMMAND_NONE];
-        [[self commandParameterRef] setPinNew:@""];
-        [[self commandParameterRef] setPinOld:@""];
-        [self terminateWindow:NSModalResponseOK];
+        // USBポートに接続されていない場合は処理中止
+        if ([self checkUSBHIDConnection] == false) {
+            return;
+        }
+        // PIN番号入力画面を開く
+        [self pinSettingWindowWillOpen];
     }
 
     - (IBAction)buttonResetDidPress:(id)sender {
@@ -85,6 +93,29 @@
         // USBポートに接続されていない場合は処理中止
         FIDOSettingCommand *command = (FIDOSettingCommand *)[self fidoSettingCommandRef];
         return [ToolCommonFunc checkUSBHIDConnectionOnWindow:[self window] connected:[command isUSBHIDConnected]];
+    }
+
+#pragma mark - Interface for PIN entry window
+
+    - (void)pinSettingWindowWillOpen {
+        // 画面に親画面参照をセット
+        [[self pinSettingWindow] setParentWindowRef:[self window] withCommandRef:[self fidoSettingCommandRef] withParameterRef:[self commandParameterRef]];
+        // ダイアログをモーダルで表示
+        NSWindow *dialog = [[self pinSettingWindow] window];
+        FIDOSettingWindow * __weak weakSelf = self;
+        [[self window] beginSheet:dialog completionHandler:^(NSModalResponse response){
+            // ダイアログが閉じられた時の処理
+            [weakSelf pinSettingWindowDidClose:self modalResponse:response];
+        }];
+    }
+
+    - (void)pinSettingWindowDidClose:(id)sender modalResponse:(NSInteger)modalResponse {
+        // PIN入力画面を閉じる
+        [[self pinSettingWindow] close];
+        if (modalResponse == NSModalResponseOK) {
+            // この画面も閉じる
+            [self terminateWindow:NSModalResponseOK];
+        }
     }
 
 @end
