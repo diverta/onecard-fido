b67b5047d11076f2efe91275c3df417794734e14
Makoto Morita <makmorit@outlook.jp>
2022-07-19 10:15:10 +0900

    FIDO設定画面の新設
    ・FIDO設定画面をメイン画面から開くことができるよう修正
    ・ボタン名を修正

--- MaintenanceTool/macOSApp/MaintenanceToolFIDO/FIDOSettingCommand.m
+++ MaintenanceTool/macOSApp/MaintenanceToolFIDO/FIDOSettingCommand.m
@@ -5,6 +5,7 @@
 //  Created by Makoto Morita on 2022/07/18.
 //
 #import "FIDOSettingCommand.h"
+#import "FIDOSettingWindow.h"
 
 @implementation FIDOSettingCommandParameter
 
@@ -14,6 +15,8 @@
 
     // 親画面の参照を保持
     @property (nonatomic) NSWindow                     *parentWindow;
+    // 画面の参照を保持
+    @property (nonatomic) FIDOSettingWindow            *fidoSettingWindow;
     // PIN番号管理処理のパラメーターを保持
     @property (nonatomic) FIDOSettingCommandParameter  *commandParameter;
 
@@ -24,6 +27,8 @@
     - (id)initWithDelegate:(id)delegate {
         self = [super initWithDelegate:delegate];
         if (self) {
+            // 画面のインスタンスを生成
+            [self setFidoSettingWindow:[[FIDOSettingWindow alloc] initWithWindowNibName:@"FIDOSettingWindow"]];
             // ヘルパークラスのインスタンスを生成
             [self setCommandParameter:[[FIDOSettingCommandParameter alloc] init]];
         }
@@ -33,11 +38,27 @@
     - (void)fidoSettingWindowWillOpen:(id)sender parentWindow:(NSWindow *)parentWindow {
         // 親画面の参照を保持
         [self setParentWindow:parentWindow];
+        // 画面に親画面参照をセット
+        [[self fidoSettingWindow] setParentWindowRef:parentWindow withCommandRef:self withParameterRef:[self commandParameter]];
+        // ダイアログをモーダルで表示
+        NSWindow *dialog = [[self fidoSettingWindow] window];
+        FIDOSettingCommand * __weak weakSelf = self;
+        [parentWindow beginSheet:dialog completionHandler:^(NSModalResponse response){
+            // ダイアログが閉じられた時の処理
+            [weakSelf fidoSettingWindowDidClose:self modalResponse:response];
+        }];
     }
 
     - (bool)isUSBHIDConnected {
         // TODO: USBポートに接続されていない場合はfalse
         return false;
+    }
+
+#pragma mark - Perform functions
+
+    - (void)fidoSettingWindowDidClose:(id)sender modalResponse:(NSInteger)modalResponse {
+        // 画面を閉じる
+        [[self fidoSettingWindow] close];
     }
 
 @end
