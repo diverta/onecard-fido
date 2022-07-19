695c1e9a637888ca13f8596917951ac686c1c9f6
Makoto Morita <makmorit@outlook.jp>
2022-07-19 15:14:01 +0900

    BLE設定画面の新設
    ・BLE設定画面／コマンドクラスを新設

---
+++ MaintenanceTool/macOSApp/MaintenanceToolBLE/BLESettingCommand.m
@@ -0,0 +1,87 @@
+//
+//  BLESettingCommand.m
+//  MaintenanceTool
+//
+//  Created by Makoto Morita on 2022/07/19.
+//
+#import "AppCommonMessage.h"
+#import "BLESettingCommand.h"
+#import "BLESettingWindow.h"
+
+@implementation BLESettingCommandParameter
+
+@end
+
+@interface BLESettingCommand ()
+
+    // 親画面の参照を保持
+    @property (nonatomic) NSWindow                     *parentWindow;
+    // 画面の参照を保持
+    @property (nonatomic) BLESettingWindow             *bleSettingWindow;
+    // 処理のパラメーターを保持
+    @property (nonatomic) BLESettingCommandParameter   *commandParameter;
+
+@end
+
+@implementation BLESettingCommand
+
+    - (id)initWithDelegate:(id)delegate {
+        self = [super initWithDelegate:delegate];
+        if (self) {
+            // 画面のインスタンスを生成
+            [self setBleSettingWindow:[[BLESettingWindow alloc] initWithWindowNibName:@"BLESettingWindow"]];
+            // ヘルパークラスのインスタンスを生成
+            [self setCommandParameter:[[BLESettingCommandParameter alloc] init]];
+        }
+        return self;
+    }
+
+    - (void)bleSettingWindowWillOpen:(id)sender parentWindow:(NSWindow *)parentWindow {
+        // 親画面の参照を保持
+        [self setParentWindow:parentWindow];
+        // 画面に親画面参照をセット
+        [[self bleSettingWindow] setParentWindowRef:parentWindow withCommandRef:self withParameterRef:[self commandParameter]];
+        // ダイアログをモーダルで表示
+        NSWindow *dialog = [[self bleSettingWindow] window];
+        BLESettingCommand * __weak weakSelf = self;
+        [parentWindow beginSheet:dialog completionHandler:^(NSModalResponse response){
+            // ダイアログが閉じられた時の処理
+            [weakSelf bleSettingWindowDidClose:self modalResponse:response];
+        }];
+    }
+
+    - (bool)isUSBHIDConnected {
+        // TODO: USBポートに接続されていない場合はfalse
+        return true;
+    }
+
+#pragma mark - Perform functions
+
+    - (void)bleSettingWindowDidClose:(id)sender modalResponse:(NSInteger)modalResponse {
+        // 画面を閉じる
+        [[self bleSettingWindow] close];
+        // 実行コマンドにより処理分岐
+        switch ([[self commandParameter] command]) {
+            case COMMAND_PAIRING:
+                [self notifyCommandStartedWithCommandName:PROCESS_NAME_PAIRING];
+                // TODO: 仮の実装です。
+                [self notifyCommandTerminated:[self commandName] message:nil success:false fromWindow:[self parentWindow]];
+                break;
+            case COMMAND_ERASE_BONDS:
+                [self notifyCommandStartedWithCommandName:PROCESS_NAME_ERASE_BONDS];
+                // TODO: 仮の実装です。
+                [self notifyCommandTerminated:[self commandName] message:nil success:false fromWindow:[self parentWindow]];
+                break;
+            default:
+                // メイン画面に制御を戻す
+                break;
+        }
+    }
+
+    - (void)notifyCommandStartedWithCommandName:(NSString *)commandName {
+        // コマンド開始メッセージを画面表示
+        [self setCommandName:commandName];
+        [self notifyCommandStarted:[self commandName]];
+    }
+
+@end
