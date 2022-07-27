e258cf58939869bdcc31acf327764bee396bb2fb
makmorit <makmorit@outlook.jp>
2022-07-27 13:21:55 +0900

    PIV設定機能の実装
    ・証明書データのTLV解析処理を追加実装

--- MaintenanceTool/WindowsExe/MaintenanceToolGUI/MaintenanceToolGUI/ToolPIVCertDesc.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolGUI/MaintenanceToolGUI/ToolPIVCertDesc.cs
@@ -16,6 +16,9 @@ namespace MaintenanceToolGUI
         private string Issuer;
         private string PrintableHash;
 
+        // エラーメッセージを保持
+        private string ErrorMessage = AppCommon.MSG_OCCUR_UNKNOWN_ERROR;
+
         public ToolPIVCertDesc(ToolPIVSettingItem item)
         {
             // PIV設定情報を保持
@@ -58,16 +61,62 @@ namespace MaintenanceToolGUI
 
             // 必要な属性をPIVデータオブジェクトから抽出
             if (ExtractCertDescriptions(data) == false) {
-                AppUtil.OutputLogError(string.Format(AppCommon.MSG_ERROR_PIV_CERT_INFO_GET_FAILED, ""));
+                AppUtil.OutputLogError(string.Format(AppCommon.MSG_ERROR_PIV_CERT_INFO_GET_FAILED, ErrorMessage));
                 return false;
             }
 
             return true;
         }
 
+        // 証明書データ格納領域
+        byte[] m_binary_data = new byte[3072];
+
         private bool ExtractCertFromTLV(byte[] certData)
         {
+            //
+            // 証明書データを格納しているTLVから、証明書データだけを抽出します。
+            //   TLV: 538203957082038cXXXX...XXXX710100fe00 (921 bytes)
+            //   --> TLV data: 7082038cXXXX...XXXX710100fe00
+            //       TLV size: 0x0395 (917 bytes)
+            //   --> cert data: XXXX...XXXX
+            //       cert size: 0x038c (908 bytes)
+            //
+            // 領域を初期化
+            for (int i = 0; i < m_binary_data.Length; i++) {
+                m_binary_data[i] = 0;
+            }
+
+            // 証明書データを格納しているTLVを抽出
+            int offset = 1;
+            int size = certData.Length;
+            int tlv_size = size - 1;
+            int offset_obj = tlv_get_length(certData, offset, tlv_size);
+            if (offset_obj == 0) {
+                // 不正なTLVの場合は終了
+                ErrorMessage = "ExtractCertFromTLV: Invalid TLV (object length)";
+                return false;
+            }
+
             return true;
+        }
+
+        int obj_len = 0;
+        private int tlv_get_length(byte[] buffer, int offset, int size)
+        {
+            // 不正なTLVの場合、戻り値を０に設定します
+            if (buffer[offset + 0] < 0x80 && 1 < size) {
+                obj_len = buffer[offset + 0];
+                return (1 + obj_len <= size) ? 1 : 0;
+            } else if (buffer[offset + 0] == 0x81 && 2 < size) {
+                obj_len = buffer[offset + 1];
+                return (2 + obj_len <= size) ? 2 : 0;
+            } else if (buffer[offset + 0] == 0x82 && 3 < size) {
+                obj_len = ((buffer[offset + 1] << 8) & 0xff00) + (buffer[offset + 2] & 0x00ff);
+                return (3 + obj_len <= size) ? 3 : 0;
+            } else {
+                obj_len = 0;
+                return 0;
+            }
         }
 
         private bool ExtractCertDescriptions(byte[] certData)
