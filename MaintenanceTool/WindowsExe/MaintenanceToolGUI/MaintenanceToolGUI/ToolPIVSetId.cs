a7782474da2c78f51caf7f825c7b0cd51e4ecf08
makmorit <makmorit@outlook.jp>
2022-08-01 10:46:56 +0900

    ID設定機能の実装
    ・CCC設定用APDUの生成処理を実装

--- MaintenanceTool/WindowsExe/MaintenanceToolGUI/MaintenanceToolGUI/ToolPIVSetId.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolGUI/MaintenanceToolGUI/ToolPIVSetId.cs
@@ -11,6 +11,8 @@ namespace MaintenanceToolGUI
         public const int PIV_CARDID_SIZE = 16;
         public const int PIV_CARDEXP_OFFSET = 47;
         public const int PIV_CARDEXP_SIZE = 8;
+        public const int PIV_CCCID_OFFSET = 9;
+        public const int PIV_CCCID_SIZE = 14;
     }
 
     class ToolPIVSetId
@@ -28,6 +30,33 @@ namespace MaintenanceToolGUI
             0x34, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
             0x35, 0x08, 0x32, 0x30, 0x33, 0x30, 0x30, 0x31, 0x30, 0x31,
             0x3e, 0x00,
+            0xfe, 0x00
+        };
+
+        //
+        // CCCテンプレート
+        //  f0: Card Identifier
+        //      0xa000000116 == GSC-IS RID
+        //      0xff == Manufacturer ID (dummy)
+        //      0x02 == Card type (javaCard)
+        //      next 14 bytes: card ID
+        private byte[] CccTemplate = {
+            0xf0, 0x15,
+            0xa0, 0x00, 0x00, 0x01, 0x16,
+            0xff,
+            0x02,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            0xf1, 0x01, 0x21,
+            0xf2, 0x01, 0x21,
+            0xf3, 0x00,
+            0xf4, 0x01, 0x00,
+            0xf5, 0x01, 0x10,
+            0xf6, 0x00,
+            0xf7, 0x00,
+            0xfa, 0x00,
+            0xfb, 0x00,
+            0xfc, 0x00,
+            0xfd, 0x00,
             0xfe, 0x00
         };
 
@@ -74,8 +103,30 @@ namespace MaintenanceToolGUI
 
         public byte[] GenerateCccAPDU()
         {
-            // TODO: 仮の実装です。
-            return new byte[0];
+            // テンプレートをコピー
+            //   先頭５バイトはオブジェクトID
+            //   先頭２バイトはTLVのタグ／データ長
+            int leadingOffset = 5 + 2;
+            byte[] template = new byte[leadingOffset + CccTemplate.Length];
+            Array.Copy(CccTemplate, 0, template, leadingOffset, CccTemplate.Length);
+
+            // ランダムなIDを設定
+            byte[] randomBytes = new byte[ToolPIVSetIdConst.PIV_CCCID_SIZE];
+            GenerateRandomBytes(randomBytes);
+            Array.Copy(randomBytes, 0, template, leadingOffset + ToolPIVSetIdConst.PIV_CCCID_OFFSET, ToolPIVSetIdConst.PIV_CCCID_SIZE);
+
+            // オブジェクトIDを設定
+            template[0] = ToolPIVConst.TAG_DATA_OBJECT;
+            template[1] = 3;
+            UInt32 objectId = ToolPIVConst.PIV_OBJ_CAPABILITY;
+            template[2] = (byte)((objectId >> 16) & 0xff);
+            template[3] = (byte)((objectId >> 8) & 0xff);
+            template[4] = (byte)(objectId & 0xff);
+
+            // タグとデータ長を設定
+            template[5] = ToolPIVConst.TAG_DATA_OBJECT_VALUE;
+            template[6] = (byte)CccTemplate.Length;
+            return template;
         }
 
         private void GenerateRandomBytes(byte[] nonce)
