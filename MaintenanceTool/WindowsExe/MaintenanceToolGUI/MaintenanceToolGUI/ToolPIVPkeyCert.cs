1299da2f6cb324563e876478be3ee4e3fec024f9
makmorit <makmorit@outlook.jp>
2022-08-02 14:54:35 +0900

    鍵・証明書インストール機能の実装
    ・EC鍵・証明書のバイナリーイメージ取得処理まで実装

--- MaintenanceTool/WindowsExe/MaintenanceToolGUI/MaintenanceToolGUI/ToolPIVPkeyCert.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolGUI/MaintenanceToolGUI/ToolPIVPkeyCert.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
+using ToolGUICommon;
 
 namespace MaintenanceToolGUI
 {
@@ -8,6 +11,12 @@ namespace MaintenanceToolGUI
         // 鍵・証明書のアルゴリズムを保持
         public string PkeyAlgName { get; set; }
         public string CertAlgName { get; set; }
+
+        // EC鍵のバイナリーイメージ
+        public byte[] ECPrivKeyBytes = null;
+
+        // 証明書のバイナリーイメージ
+        public byte[] CertBytes = null;
 
         public ToolPIVPkeyCert()
         {
@@ -23,12 +32,53 @@ namespace MaintenanceToolGUI
                 PkeyAlgName = "RSA2048";
             } else if (privateKeyPem.Contains("EC PRIVATE KEY")) {
                 PkeyAlgName = "ECCP256";
+                if (ParseECPrivateKey(privateKeyPem) == false) {
+                    return false;
+                }
             } else {
                 PkeyAlgName = "";
                 return false;
             }
 
             return true;
+        }
+
+        public bool ParseECPrivateKey(string privateKeyPem)
+        {
+            // 秘密鍵を抽出（Base64エンコード文字列）
+            string ecKeyPem = "";
+            bool found = false;
+            foreach (string line in privateKeyPem.Split('\n')) {
+                if (found) {
+                    if (line.Contains("-----END EC PRIVATE KEY-----")) {
+                        break;
+                    } else {
+                        ecKeyPem += line;
+                    }
+                } else if (line.Contains("-----BEGIN EC PRIVATE KEY-----")) {
+                    found = true;
+                }
+            }
+
+            // 秘密鍵が抽出できなかった場合はエラー
+            if (ecKeyPem.Length == 0) {
+                AppUtil.OutputLogError(string.Format(AppCommon.MSG_ERROR_PIV_PKEY_PEM_LOAD_FAILED, "EC private key not found in PEM file"));
+                return false;
+            }
+
+            try {
+                // 秘密鍵情報をデコード
+                byte[] decodedBytes = Convert.FromBase64String(ecKeyPem);
+
+                // デコードされた秘密鍵情報の８バイト目から32バイトを抽出
+                //   スキップする７バイト＝ 30 77 02 01 01 04 20
+                ECPrivKeyBytes = decodedBytes.Skip(7).Take(32).ToArray();
+                return true;
+
+            } catch (Exception e) {
+                AppUtil.OutputLogError(string.Format(AppCommon.MSG_ERROR_PIV_PKEY_PEM_LOAD_FAILED, e.Message));
+                return false;
+            }
         }
 
         public bool LoadCertificate(string certPath)
@@ -47,6 +97,8 @@ namespace MaintenanceToolGUI
                 return false;
             }
 
+            // 証明書のバイナリーイメージを抽出
+            CertBytes = x509.GetRawCertData();
             return true;
         }
     }
