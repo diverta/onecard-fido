0db2ab3df8f9657712dc932f462101b7438acc79
Makoto Morita <makmorit@outlook.jp>
2022-08-11 14:57:57 +0900

    .NET 6.0版メイン画面の制作
    ・アプリケーション開始／終了ログを出力させるようにする

--- MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/App.xaml.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Security.Principal;
 using System.Threading;
 using System.Windows;
+using ToolAppCommon;
 
 namespace MaintenanceToolApp
 {
@@ -42,6 +43,9 @@ namespace MaintenanceToolApp
 
         protected override void OnExit(ExitEventArgs e)
         {
+            // アプリケーション終了ログを出力
+            AppLogUtil.OutputLogInfo(string.Format("{0}を終了しました", AppCommon.MSG_TOOL_TITLE));
+
             // Mutexを解放
             if (MutexRef != null) {
                 MutexRef.ReleaseMutex();
--- MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/MainWindowViewModel.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/MainWindowViewModel.cs
@@ -1,7 +1,19 @@
-﻿namespace MaintenanceToolApp
+﻿using ToolAppCommon;
+
+namespace MaintenanceToolApp
 {
     public class MainWindowViewModel
     {
-        public string Title { get; set; } = AppCommon.MSG_TOOL_TITLE;
+        public string Title { get; set; }
+
+        public MainWindowViewModel()
+        {
+            // メイン画面のタイトル
+            Title = AppCommon.MSG_TOOL_TITLE;
+
+            // アプリケーション開始ログを出力
+            AppLogUtil.SetOutputLogApplName("MaintenanceToolApp");
+            AppLogUtil.OutputLogInfo(string.Format("{0}を起動しました: {1}", AppCommon.MSG_TOOL_TITLE, "0.0.0"));
+        }
     }
 }
