74da8579cf8395f0e0cfe16a61fb08e3bed5b42a
Makoto Morita <makmorit@outlook.jp>
2022-08-11 11:33:04 +0900

    .NET 6.0版メイン画面の制作
    ・２重起動の抑止処理を実装

--- MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/App.xaml.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Reflection;
+using System.Threading;
 using System.Windows;
 
 namespace MaintenanceToolApp
@@ -13,5 +9,27 @@ namespace MaintenanceToolApp
     /// </summary>
     public partial class App : Application
     {
+        // Mutex作成
+        private readonly Mutex MutexRef = new Mutex(false, Assembly.GetExecutingAssembly().GetName().Name);
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Mutexの所有権を要求
+            if (MutexRef.WaitOne(0, false) == false) {
+                MessageBox.Show(AppCommon.MSG_ERROR_DOUBLE_START, AppCommon.MSG_TOOL_TITLE);
+                MutexRef.Close();
+                Shutdown();
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // Mutexを解放
+            if (MutexRef != null) {
+                MutexRef.ReleaseMutex();
+                MutexRef.Close();
+            }
+            base.OnExit(e);
+        }
     }
 }
---
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/AppCommon.cs
@@ -0,0 +1,9 @@
+﻿namespace MaintenanceToolApp
+{
+    public static class AppCommon
+    {
+        // 起動時のメッセージ文言
+        public const string MSG_TOOL_TITLE = "FIDO認証器管理ツール";
+        public const string MSG_ERROR_DOUBLE_START = "既に起動されています。";
+    }
+}
