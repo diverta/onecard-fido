713c9fdb2d6a3622ab6498ffe6f2d4478cc49767
Makoto Morita <makmorit@outlook.jp>
2022-08-11 15:41:35 +0900

    .NET 6.0版メイン画面の制作
    ・開始ログ出力にあたり、ツールのバージョンを表示させるよう修正

--- MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/MainWindowViewModel.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/MainWindowViewModel.cs
@@ -1,19 +1,37 @@
-﻿using ToolAppCommon;
+﻿using System.Reflection;
+using ToolAppCommon;
 
 namespace MaintenanceToolApp
 {
     public class MainWindowViewModel
     {
         public string Title { get; set; }
+        public string MaintenanceToolVersion { get; set; }
 
         public MainWindowViewModel()
         {
             // メイン画面のタイトル
             Title = AppCommon.MSG_TOOL_TITLE;
 
+            // ツールのバージョンを取得
+            MaintenanceToolVersion = string.Format("Version {0}", GetMaintenanceToolVersion());
+
             // アプリケーション開始ログを出力
             AppLogUtil.SetOutputLogApplName("MaintenanceToolApp");
-            AppLogUtil.OutputLogInfo(string.Format("{0}を起動しました: {1}", AppCommon.MSG_TOOL_TITLE, "0.0.0"));
+            AppLogUtil.OutputLogInfo(string.Format("{0}を起動しました: {1}", AppCommon.MSG_TOOL_TITLE, MaintenanceToolVersion));
+        }
+
+        private static string GetMaintenanceToolVersion()
+        {
+            // 製品バージョン文字列を戻す
+            Assembly asm = Assembly.GetExecutingAssembly();
+            System.Diagnostics.FileVersionInfo ver = System.Diagnostics.FileVersionInfo.GetVersionInfo(asm.Location);
+            string? versionString = ver.ProductVersion;
+            if (versionString == null) {
+                return "";
+            } else {
+                return versionString;
+            }
         }
     }
 }
