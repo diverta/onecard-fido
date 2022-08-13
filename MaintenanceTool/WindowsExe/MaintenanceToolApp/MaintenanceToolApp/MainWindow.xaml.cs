0fc537166271a3eb9915523f30e3866c55f12b65
Makoto Morita <makmorit@outlook.jp>
2022-08-13 12:51:28 +0900

    .NET 6.0版メイン画面の制作
    ・USBの脱着検知ができるよう実装

--- MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/MainWindow.xaml.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/MainWindow.xaml.cs
@@ -21,6 +21,9 @@ namespace MaintenanceToolApp
             // アプリケーション開始ログを出力
             AppLogUtil.SetOutputLogApplName("MaintenanceToolApp");
             AppLogUtil.OutputLogInfo(string.Format("{0}を起動しました: {1}", AppCommon.MSG_TOOL_TITLE, AppUtil.GetAppVersionString()));
+
+            // USBデバイスの脱着検知を開始
+            USBDevice.StartUSBDeviceNotification(this);
         }
     }
 }
