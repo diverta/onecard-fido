a4edcfc77d7b7a71647c87ad2c4d03040d74ff56
Makoto Morita <makmorit@outlook.jp>
2022-08-15 10:20:21 +0900

    USB接続／切断処理の実装
    ・メッセージリソースを登録

--- MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/AppCommon.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/AppCommon.cs
@@ -6,5 +6,12 @@
         public const string MSG_TOOL_TITLE = "FIDO認証器管理ツール";
         public const string MSG_INVALID_USER_ROLL = "このツールは、管理者として実行してください。\n\nプログラムアイコンを右クリックして、\nメニューから「管理者として実行」を選択します。";
         public const string MSG_ERROR_DOUBLE_START = "既に起動されています。";
+
+        // メッセージリソース
+        public const string MSG_USB_DETECT_FAILED = "USBデバイス検知の開始に失敗しました。";
+        public const string MSG_USB_DETECT_STARTED = "USBデバイス検知を開始しました。";
+        public const string MSG_USB_DETECT_END = "USBデバイス検知を終了しました。";
+        public const string MSG_HID_REMOVED = "USB HIDデバイスが取り外されました。";
+        public const string MSG_HID_CONNECTED = "USB HIDデバイスに接続されました。";
     }
 }
