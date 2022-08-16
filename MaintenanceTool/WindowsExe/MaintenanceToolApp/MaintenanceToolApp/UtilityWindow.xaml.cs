11ef8b7044453b4ffe3dae6d73cd63c1700f405e
Makoto Morita <makmorit@outlook.jp>
2022-08-16 12:33:56 +0900

    ユーティリティー画面の実装
    ・画面デザインを作成

---
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/UtilityWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MaintenanceToolApp
+{
+    /// <summary>
+    /// UtilityWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class UtilityWindow : Window
+    {
+        public UtilityWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
