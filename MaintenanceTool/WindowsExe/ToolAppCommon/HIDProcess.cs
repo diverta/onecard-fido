682e95c98522a9968e97f97d9702f983b86ab72b
Makoto Morita <makmorit@outlook.jp>
2022-08-18 17:05:02 +0900

    Flash ROM情報参照機能の実装
    ・HIDインターフェースによる、データ送受信処理を実装

--- MaintenanceTool/WindowsExe/ToolAppCommon/HIDProcess.cs
+++ MaintenanceTool/WindowsExe/ToolAppCommon/HIDProcess.cs
@@ -1,4 +1,6 @@
 ﻿using MaintenanceToolApp;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ToolAppCommon
@@ -90,6 +92,7 @@ namespace ToolAppCommon
 
             // デバイスを初期化
             device = new HIDDevice(devicePath);
+            device.DataReceived += new HIDDevice.dataReceivedEvent(ReceiveHIDMessage);
             AppLogUtil.OutputLogInfo(string.Format(AppCommon.MSG_HID_CONNECTED + "{0}", devicePath));
 
             // ハンドラー経由で接続通知
@@ -124,6 +127,244 @@ namespace ToolAppCommon
 
             // ハンドラー経由で接続切断通知
             OnConnectHIDDevice(false);
+        }
+
+        //
+        // データ送信
+        //
+        // リクエスト送信時のCIDを保持
+        private readonly byte[] SendHIDMessageCID = new byte[4];
+
+        // フレームデータを保持
+        private readonly byte[] SendFrameData = new byte[HIDProcessConst.HID_FRAME_LEN];
+
+        private void SendHIDMessage(byte[] cid, byte CMD, byte[] message)
+        {
+            // メッセージがない場合は終了
+            if (message == null) {
+                AppLogUtil.OutputLogError("SendHIDMessage: invalid message buffer");
+                return;
+            }
+            // リクエスト送信時のCIDを保持
+            cid.CopyTo(SendHIDMessageCID, 0);
+            // 
+            // 送信データをフレーム分割
+            // 
+            //  INITフレーム
+            //  1 - 4 バイト目: CID
+            //  5     バイト目: コマンド
+            //  6 - 7 バイト目: データ長
+            //  残りのバイト  : データ部（64 - 7 = 57）
+            //
+            //  CONTフレーム
+            //  1 - 4 バイト目: CID
+            //  5     バイト目: シーケンス
+            //  残りのバイト  : データ部（64 - 5 = 59）
+            // 
+            int transferred = 0;
+            int seq = 0;
+            int messageSize = message.Length;
+            if (messageSize == 0) {
+                // 送信メッセージサイズが０の場合は
+                // CIDとCMDだけを送信する。
+                SendHIDHeaderMessage(cid, CMD, message);
+            }
+            while (transferred < messageSize) {
+                for (int j = 0; j < SendFrameData.Length; j++) {
+                    // フレームデータを初期化
+                    SendFrameData[j] = 0;
+                }
+                for (int c = 0; c < cid.Length; c++) {
+                    // CIDをコピー
+                    SendFrameData[c] = cid[c];
+                }
+
+                if (transferred == 0) {
+                    // INITフレーム
+                    // ヘッダーを設定
+                    SendFrameData[4] = CMD;
+                    SendFrameData[5] = (byte)(messageSize / 256);
+                    SendFrameData[6] = (byte)(messageSize % 256);
+
+                    // データをコピー
+                    int maxLen = HIDProcessConst.HID_FRAME_LEN - HIDProcessConst.HID_INIT_HEADER_LEN;
+                    int dataLenInFrame = (messageSize < maxLen) ? messageSize : maxLen;
+                    for (int i = 0; i < dataLenInFrame; i++) {
+                        SendFrameData[HIDProcessConst.HID_INIT_HEADER_LEN + i] = message[transferred++];
+                    }
+
+                    string dump = AppLogUtil.DumpMessage(SendFrameData, HIDProcessConst.HID_INIT_HEADER_LEN + dataLenInFrame);
+                    AppLogUtil.OutputLogDebug(string.Format(
+                        "HID Sent INIT frame: data size={0} length={1}\r\n{2}",
+                        messageSize, dataLenInFrame, dump));
+
+                } else {
+                    // CONTフレーム
+                    // ヘッダーをコピー
+                    SendFrameData[4] = (byte)seq;
+
+                    // データをコピー
+                    int remaining = messageSize - transferred;
+                    int maxLen = HIDProcessConst.HID_FRAME_LEN - HIDProcessConst.HID_CONT_HEADER_LEN;
+                    int dataLenInFrame = (remaining < maxLen) ? remaining : maxLen;
+                    for (int i = 0; i < dataLenInFrame; i++) {
+                        SendFrameData[HIDProcessConst.HID_CONT_HEADER_LEN + i] = message[transferred++];
+                    }
+
+                    string dump = AppLogUtil.DumpMessage(SendFrameData, HIDProcessConst.HID_CONT_HEADER_LEN + dataLenInFrame);
+                    AppLogUtil.OutputLogDebug(string.Format(
+                        "HID Sent CONT frame: data seq={0} length={1}\r\n{2}",
+                        seq++, dataLenInFrame, dump));
+                }
+
+                // フレームデータを転送
+                device.Write(SendFrameData);
+            }
+        }
+
+        private void SendHIDHeaderMessage(byte[] cid, byte CMD, byte[] message)
+        {
+            // 送信メッセージサイズが０の場合は
+            // CIDとCMDだけを送信する。
+            for (int j = 0; j < SendFrameData.Length; j++) {
+                // フレームデータを初期化
+                SendFrameData[j] = 0;
+            }
+
+            for (int c = 0; c < cid.Length; c++) {
+                // CIDをコピー
+                SendFrameData[c] = cid[c];
+            }
+
+            // CMDをコピー
+            SendFrameData[4] = CMD;
+
+            string dump = AppLogUtil.DumpMessage(SendFrameData, HIDProcessConst.HID_INIT_HEADER_LEN);
+            AppLogUtil.OutputLogDebug(string.Format(
+                "HID Sent INIT frame: data size={0} length={1}\r\n{2}",
+                message.Length, 0, dump));
+
+            // フレームデータを転送
+            device.Write(SendFrameData);
+        }
+
+        //
+        // データ受信
+        //
+        // 送信ログを保持
+        private string ReceivedLogBuffer = "";
+
+        private void ReceiveHIDMessage(byte[] message)
+        {
+            // 受信データを保持
+            byte[] receivedMessage = new byte[0];
+            int receivedMessageLen = 0;
+            int received = 0;
+
+            // フレームデータを保持
+            byte[] frameData = new byte[HIDProcessConst.HID_FRAME_LEN];
+
+            // 受信したCID、コマンドを保持
+            byte[] receivedCID = new byte[4];
+            byte receivedCMD = 0;
+
+            // メッセージは最低 8 バイト
+            if (message.Length < 8) {
+                AppLogUtil.OutputLogError(String.Format(
+                    "Device_dataReceived: invalid received message size({0})", message.Length));
+                return;
+            }
+            for (int f = 0; f < frameData.Length; f++) {
+                // フレームデータを退避
+                frameData[f] = message[f + 1];
+            }
+            // 
+            // 受信したデータをバッファにコピー
+            // 
+            //  INITフレーム
+            //  1     バイト目: レポートID
+            //  2 - 5 バイト目: CID
+            //  6     バイト目: コマンド
+            //  7 - 8 バイト目: データ長
+            //  残りのバイト  : データ部（65 - 8 = 57）
+            //
+            //  CONTフレーム
+            //  1     バイト目: レポートID
+            //  2 - 5 バイト目: CID
+            //  6     バイト目: シーケンス
+            //  残りのバイト  : データ部（65 - 6 = 59）
+            // 
+            for (int c = 0; c < receivedCID.Length; c++) {
+                // CIDをコピー
+                receivedCID[c] = frameData[c];
+            }
+            // レスポンス受信時のCIDが、リクエスト送信時のCIDと異なる場合は無視
+            if (Enumerable.SequenceEqual(receivedCID, SendHIDMessageCID) == false) {
+                return;
+            }
+            int hid_init_data_len = 57;
+            int hid_cont_data_len = 59;
+            if (frameData[4] > 127) {
+                // INITフレームであると判断
+                receivedCMD = frameData[4];
+                byte cnth = frameData[5];
+                byte cntl = frameData[6];
+                receivedMessageLen = cnth * 256 + cntl;
+                receivedMessage = new byte[receivedMessageLen];
+                received = 0;
+
+                // データをコピー
+                int dataLenInFrame = (receivedMessageLen < hid_init_data_len) ? receivedMessageLen : hid_init_data_len;
+                for (int i = 0; i < dataLenInFrame; i++) {
+                    receivedMessage[received++] = frameData[HIDProcessConst.HID_INIT_HEADER_LEN + i];
+                }
+
+                ReceivedLogBuffer = "";
+                string dump = AppLogUtil.DumpMessage(frameData, dataLenInFrame + HIDProcessConst.HID_INIT_HEADER_LEN);
+                AppendLogToBuffer(string.Format(
+                    "HID Recv INIT frame: data size={0} length={1}\r\n{2}",
+                    receivedMessageLen, dataLenInFrame, dump));
+
+            } else {
+                // CONTフレームであると判断
+                int seq = frameData[4];
+
+                // データをコピー
+                int remaining = receivedMessageLen - received;
+                int dataLenInFrame = (remaining < hid_cont_data_len) ? remaining : hid_cont_data_len;
+                for (int i = 0; i < dataLenInFrame; i++) {
+                    receivedMessage[received++] = frameData[HIDProcessConst.HID_CONT_HEADER_LEN + i];
+                }
+
+                string dump = AppLogUtil.DumpMessage(frameData, dataLenInFrame + HIDProcessConst.HID_CONT_HEADER_LEN);
+                AppendLogToBuffer(string.Format(
+                    "HID Recv CONT frame: seq={0} length={1}\r\n{2}",
+                    seq, dataLenInFrame, dump));
+            }
+
+            // キープアライブレスポンスの場合は無視
+            if (receivedCMD == 0xbb) {
+                return;
+            }
+
+            if (received == receivedMessageLen) {
+                // 全フレームを受信できたら、
+                // この時点で一括してログ出力を行い、その後
+                // HIDデバイスからのデータを転送
+                AppLogUtil.OutputLogText(ReceivedLogBuffer);
+            }
+        }
+
+        private void AppendLogToBuffer(string message)
+        {
+            // メッセージに現在時刻を付加する
+            string formatted = string.Format("{0} [debug] {1}", DateTime.Now.ToString(), message);
+
+            // メッセージを一時領域に連結
+            if (ReceivedLogBuffer.Length > 0) {
+                ReceivedLogBuffer += "\r\n";
+            }
+            ReceivedLogBuffer += formatted;
         }
     }
 }
