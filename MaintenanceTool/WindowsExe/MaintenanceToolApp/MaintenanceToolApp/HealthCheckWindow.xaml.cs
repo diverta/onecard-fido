9e3768194dc9505a2775aaf311ccd36573003b7f
Makoto Morita <makmorit@outlook.jp>
2022-08-29 17:37:45 +0900

    ヘルスチェック画面の実装
    ・各ボタンにイベントを実装
    （中身は仮の実装です。）

--- MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/HealthCheckWindow.xaml.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/HealthCheckWindow.xaml.cs
@@ -24,6 +24,42 @@ namespace MaintenanceToolApp
             }
         }
 
+        private void DoBLECtap2HealthCheck()
+        {
+            // TODO: 仮の実装です。
+            TerminateWindow(true);
+        }
+
+        private void DoBLEU2FHealthCheck()
+        {
+            // TODO: 仮の実装です。
+            TerminateWindow(true);
+        }
+
+        private void DoBLEPingTest()
+        {
+            // TODO: 仮の実装です。
+            TerminateWindow(true);
+        }
+
+        private void DoHIDCtap2HealthCheck()
+        {
+            // TODO: 仮の実装です。
+            TerminateWindow(true);
+        }
+
+        private void DoHIDU2FHealthCheck()
+        {
+            // TODO: 仮の実装です。
+            TerminateWindow(true);
+        }
+
+        private void DoHIDPingTest()
+        {
+            // TODO: 仮の実装です。
+            TerminateWindow(true);
+        }
+
         private void TerminateWindow(bool dialogResult)
         {
             // この画面を閉じる
@@ -37,6 +73,36 @@ namespace MaintenanceToolApp
         private void buttonCancel_Click(object sender, RoutedEventArgs e)
         {
             TerminateWindow(false);
+        }
+
+        private void buttonBLECtap2HealthCheck_Click(object sender, RoutedEventArgs e)
+        {
+            DoBLECtap2HealthCheck();
+        }
+
+        private void buttonBLEU2FHealthCheck_Click(object sender, RoutedEventArgs e)
+        {
+            DoBLEU2FHealthCheck();
+        }
+
+        private void buttonBLEPingTest_Click(object sender, RoutedEventArgs e)
+        {
+            DoBLEPingTest();
+        }
+
+        private void buttonHIDCtap2HealthCheck_Click(object sender, RoutedEventArgs e)
+        {
+            DoHIDCtap2HealthCheck();
+        }
+
+        private void buttonHIDU2FHealthCheck_Click(object sender, RoutedEventArgs e)
+        {
+            DoHIDU2FHealthCheck();
+        }
+
+        private void buttonHIDPingTest_Click(object sender, RoutedEventArgs e)
+        {
+            DoHIDPingTest();
         }
     }
 }
