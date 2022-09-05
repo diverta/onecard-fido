c15aebc137cb3409b972a265678b39bebb551cbd
Makoto Morita <makmorit@outlook.jp>
2022-09-05 13:30:47 +0900

    CTAP2ヘルスチェック処理の実装
    ・CBOR処理ライブラリーの取り込み
    ・GetAgreementKeyリクエストのCBORエンコード処理を実装

---
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/Common/CTAP2CborUtil.cs
@@ -0,0 +1,28 @@
+﻿using PeterO.Cbor;
+using System.Linq;
+
+namespace MaintenanceToolApp.Common
+{
+    internal class CBOREncoder
+    {
+        public CBOREncoder()
+        {
+        }
+
+        public byte[] GetKeyAgreement(byte cborCommand, byte subCommand)
+        {
+            // 送信データを生成
+            //   0x01: pinProtocol = 1
+            //   0x02: subCommand = 0x02(getKeyAgreement)
+            CBORObject cbor = CBORObject.NewMap();
+            cbor.Add(0x01, 1);
+            cbor.Add(0x02, subCommand);
+            byte[] payload = cbor.EncodeToBytes();
+            byte[] encoded = new byte[] { cborCommand }.Concat(payload).ToArray();
+
+            // エンコードされたCBORバイト配列を戻す
+            return encoded;
+        }
+
+    }
+}
