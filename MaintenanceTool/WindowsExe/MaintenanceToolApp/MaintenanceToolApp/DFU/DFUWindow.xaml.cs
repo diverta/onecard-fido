ac714e0e8ae2dbb776d1ce03a80b2ffb1bbf5554
Makoto Morita <makmorit@outlook.jp>
2022-09-07 15:48:24 +0900

    ファームウェア更新画面の新設
    ・ファームウェア更新画面を追加
    （キャンセルボタンのみ配置しました）

---
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/DFU/DFUWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+
+namespace MaintenanceToolApp.DFU
+{
+    /// <summary>
+    /// DFUWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class DFUWindow : Window
+    {
+        // 処理実行のためのプロパティー
+        private readonly DFUParameter Parameter;
+
+        public DFUWindow(DFUParameter param)
+        {
+            // パラメーターの参照を保持
+            Parameter = param;
+            InitializeComponent();
+        }
+
+        public bool ShowDialogWithOwner(Window ownerWindow)
+        {
+            // この画面を、オーナー画面の中央にモード付きで表示
+            Owner = ownerWindow;
+            bool? b = ShowDialog();
+            if (b == null) {
+                return false;
+            } else {
+                return (bool)b;
+            }
+        }
+
+        private void TerminateWindow(bool dialogResult)
+        {
+            // この画面を閉じる
+            DialogResult = dialogResult;
+            Close();
+        }
+
+        //
+        // イベント処理部
+        // 
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            TerminateWindow(false);
+        }
+    }
+}
