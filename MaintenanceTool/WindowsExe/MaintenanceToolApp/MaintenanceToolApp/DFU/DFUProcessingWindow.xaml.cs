17d8b1aaa17990cd796d6fa8e8d158b679ba3e37
Makoto Morita <makmorit@outlook.jp>
2022-09-09 13:43:27 +0900

    DFU処理進捗画面の新設
    ・DFU処理進捗画面を追加
    （キャンセルボタンのみ配置しました）

---
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/DFU/DFUProcessingWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace MaintenanceToolApp.DFU
+{
+    /// <summary>
+    /// DFUProcessingWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class DFUProcessingWindow : Window
+    {
+        public DFUProcessingWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
