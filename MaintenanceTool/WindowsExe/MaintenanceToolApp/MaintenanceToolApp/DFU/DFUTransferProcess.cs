d6cbac54ca432e7195bfaad363070f297b691ddf
Makoto Morita <makmorit@outlook.jp>
2022-09-14 12:43:39 +0900

    DFU転送関連処理の実装
    ・DFU転送関連処理クラスをシングルトンとして実装

--- MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/DFU/DFUTransferProcess.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/DFU/DFUTransferProcess.cs
@@ -2,5 +2,28 @@
 {
     internal class DFUTransferProcess
     {
+        // このクラスのインスタンス
+        private static readonly DFUTransferProcess Instance = new DFUTransferProcess();
+
+        // 上位クラス／パラメーターの参照を保持
+        private DFUProcess DFUProcess = null!;
+        private DFUParameter DFUParameter = null!;
+
+        public void InvokeTransferProcess(DFUProcess process, DFUParameter parameter)
+        {
+            // 上位クラス／パラメーターの参照を保持
+            Instance.DFUProcess = process;
+            Instance.DFUParameter = parameter;
+
+            // 転送処理を起動
+            Instance.DoTransferProcess();
+        }
+
+        //
+        // 内部処理
+        //
+        public void DoTransferProcess()
+        {
+        }
     }
 }
