20723d90d62be3ec256a1c151a3eb931ae3b4c2e
Makoto Morita <makmorit@outlook.jp>
2022-09-14 16:58:58 +0900

    DFU転送関連処理の実装
    ・スロット照会リクエストの処理を実装

--- MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/DFU/DFUTransferProcess.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/DFU/DFUTransferProcess.cs
@@ -68,7 +68,7 @@ namespace MaintenanceToolApp.DFU
             SMPService.OnTransactionFailed += OnTransactionFailed;
 
             // 転送処理を開始する
-            DoTransferProcess();
+            DoRequestGetSlotInfo();
         }
 
         private void OnTerminatedDFUTransferProcess(bool success, string message)
@@ -86,8 +86,80 @@ namespace MaintenanceToolApp.DFU
         }
 
         //
+        // スロット照会
+        //
+        private void DoRequestGetSlotInfo()
+        {
+            // DFU実行開始を通知
+            DFUProcess.NotifyDFUProgress(AppCommon.MSG_DFU_PROCESS_TRANSFER_IMAGE, 0);
+
+            // リクエストデータを生成
+            byte[] bodyBytes = new byte[] { 0xbf, 0xff };
+            ushort len = (ushort)bodyBytes.Length;
+            byte[] headerBytes = BuildSMPHeader(OP_READ_REQ, 0x00, len, GRP_IMG_MGMT, 0x00, CMD_IMG_MGMT_STATE);
+
+            // リクエストデータを送信
+            Parameter.Command = BLEDFUCommand.GetSlotInfo;
+            SendSMPRequestData(bodyBytes, headerBytes);
+        }
+
+        private void DoResponseGetSlotInfo(byte[] responseData)
+        {
+            // スロット照会情報を参照し、チェックでNGの場合は以降の処理を行わない
+            if (CheckSlotInfo(responseData) == false) {
+                OnTerminatedDFUTransferProcess(false, AppCommon.MSG_DFU_SLOT_INFO_GET_FAILED);
+                return;
+            }
+
+            // 転送済みバイト数を事前にクリア
+            Parameter.ImageBytesSent = 0;
+
+            // 転送処理に移行
+            DoTransferProcess();
+        }
+
+        private bool CheckSlotInfo(byte[] responseData)
+        {
+            // TODO: 仮の実装です。
+            return true;
+        }
+
+        private byte[] BuildSMPHeader(byte op, byte flags, ushort len, ushort group, byte seq, byte id_int)
+        {
+            byte[] header = {
+                op,
+                flags,
+                (byte)(len >> 8),   (byte)(len & 0xff),
+                (byte)(group >> 8), (byte)(group & 0xff),
+                seq,
+                id_int
+            };
+            return header;
+        }
+
+        //
         // データ送受信時の処理
         //
+        private void SendSMPRequestData(byte[] requestBody, byte[] requestHeader)
+        {
+            // ヘッダーと本体を連結
+            byte[] requestData = Enumerable.Concat(requestHeader, requestBody).ToArray();
+
+            // リクエストデータを送信
+            SMPService.Send(requestData);
+
+            // ログ出力
+            if (Parameter.Command != BLEDFUCommand.UploadImage) {
+                string dump = AppLogUtil.DumpMessage(requestData, requestData.Length);
+                AppLogUtil.OutputLogDebug(string.Format(
+                    "Transmit SMP request ({0} bytes)\r\n{1}",
+                    requestData.Length, dump));
+            }
+
+            // 応答タイムアウト監視開始
+            responseTimer.Start();
+        }
+
         // 受信済みデータ／バイト数を保持
         private byte[] ResponseData = new byte[0];
         private int received = 0;
@@ -126,6 +198,9 @@ namespace MaintenanceToolApp.DFU
             if (received == totalSize) {
                 // 処理区分に応じて分岐
                 switch (Parameter.Command) {
+                case BLEDFUCommand.GetSlotInfo:
+                    DoResponseGetSlotInfo(ResponseData);
+                    break;
                 default:
                     break;
                 }
@@ -152,6 +227,9 @@ namespace MaintenanceToolApp.DFU
             // 処理区分に応じて分岐
             string errorMessage = AppCommon.MSG_NONE;
             switch (Parameter.Command) {
+            case BLEDFUCommand.GetSlotInfo:
+                errorMessage = AppCommon.MSG_DFU_SLOT_INFO_GET_FAILED;
+                break;
             default:
                 break;
             }
@@ -172,16 +250,18 @@ namespace MaintenanceToolApp.DFU
         //
         // SMPトランザクションに関する定義
         //
+        private const int OP_READ_REQ = 0;
+
+        private const int GRP_IMG_MGMT = 1;
+        private const int CMD_IMG_MGMT_STATE = 0;
+
         private const int SMP_HEADER_SIZE = 8;
 
+        //
+        // TODO: 仮の実装です。
+        //
         private void DoTransferProcess()
         {
-            // DFU実行開始を通知
-            DFUProcess.NotifyDFUProgress(AppCommon.MSG_DFU_PROCESS_TRANSFER_IMAGE, 0);
-
-            //
-            // TODO: 仮の実装です。
-            //
             System.Threading.Thread.Sleep(2000);
 
             DFUProcess.NotifyDFUTransferring(true);
