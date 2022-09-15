b27f11e9a1076d6c4b5dd7e43e962204bd0a9f14
Makoto Morita <makmorit@outlook.jp>
2022-09-15 11:15:10 +0900

    DFU転送関連処理の実装
    ・ファームウェア更新イメージの転送処理を実装

--- MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/AppCommon.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/AppCommon.cs
@@ -43,6 +43,7 @@
         public const string MSG_DFU_SUB_PROCESS_FAILED = "ファームウェア更新機能の内部処理が失敗しました。";
         public const string MSG_DFU_VERSION_INFO_GET_FAILED = "FIDO認証器ファームウェアのバージョンが取得できませんでした。";
         public const string MSG_DFU_IMAGE_NOT_AVAILABLE = "ファームウェア更新機能が利用できません。";
+        public const string MSG_DFU_IMAGE_TRANSFER_FAILED = "更新ファームウェアの転送に失敗しました。";
         public const string MSG_DFU_IMAGE_TRANSFER_CANCELED = "更新ファームウェアの転送が中断されました。";
         public const string MSG_DFU_IMAGE_ALREADY_INSTALLED = "更新ファームウェアが既に導入済みなので、ファームウェア更新処理を続行できません。";
         public const string MSG_DFU_UPDATE_IMAGE_FILE_NOT_EXIST = "ファームウェア更新イメージファイルが存在しません。";
