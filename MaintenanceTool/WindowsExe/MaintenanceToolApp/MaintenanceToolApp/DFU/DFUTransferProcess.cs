b367cb17ed65020ff6484a2705b4be48caa562e9
Makoto Morita <makmorit@outlook.jp>
2022-09-19 11:32:17 +0900

    機能修正
    ・nRF5340側に対するリセット要求が完了したら、BLE接続を破棄させるよう修正

--- MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/DFU/DFUTransferProcess.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/DFU/DFUTransferProcess.cs
@@ -296,6 +296,9 @@ namespace MaintenanceToolApp.DFU
 
         private void DoResponseResetApplication(byte[] responseData)
         {
+            // BLE接続を破棄
+            BLEProcess.DisconnctBLE();
+
             // DFU主処理の正常終了を通知
             OnTerminatedDFUTransferProcess(true, AppCommon.MSG_NONE);
         }
