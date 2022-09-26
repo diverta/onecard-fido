3792515d282f82a281eca046f10434eab3c91251
Makoto Morita <makmorit@outlook.jp>
2022-09-26 12:44:41 +0900

    FIDO設定画面の実装
    ・PINコード設定処理クラスの疎通を`GetKeyAgreement`まで確認

--- MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/FIDOSettings/SetPinCodeProcess.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/FIDOSettings/SetPinCodeProcess.cs
@@ -1,4 +1,7 @@
-﻿using ToolAppCommon;
+﻿using MaintenanceToolApp.Common;
+using MaintenanceToolApp.HealthCheck;
+using ToolAppCommon;
+using static MaintenanceToolApp.AppDefine;
 
 namespace MaintenanceToolApp.FIDOSettings
 {
@@ -6,6 +9,7 @@ namespace MaintenanceToolApp.FIDOSettings
     {
         // 処理実行のためのプロパティー
         private readonly FIDOSettingsParameter Parameter;
+        private readonly CTAP2HealthCheckParameter CTAP2Parameter;
 
         // 上位クラスに対するイベント通知
         public delegate void HandlerOnNotifyCommandTerminated(string commandTitle, string errorMessage, bool success);
@@ -18,6 +22,9 @@ namespace MaintenanceToolApp.FIDOSettings
         {
             // パラメーターの参照を保持
             Parameter = param;
+
+            // CTAP2パラメーターを生成
+            CTAP2Parameter = new CTAP2HealthCheckParameter();
 
             // コールバック参照を初期化
             OnCommandResponseRef = new CommandProcess.HandlerOnCommandResponse(OnCommandResponse);
@@ -49,9 +56,77 @@ namespace MaintenanceToolApp.FIDOSettings
         {
             // CTAPHID_INIT応答後の処理を実行
             switch (Parameter.Command) {
+            case Command.COMMAND_CLIENT_PIN_SET:
+            case Command.COMMAND_CLIENT_PIN_CHANGE:
+                DoRequestCommandGetKeyAgreement();
+                break;
             default:
                 // メイン画面に制御を戻す
                 NotifyCommandTerminated(Parameter.CommandTitle, AppCommon.MSG_OCCUR_UNKNOWN_ERROR, false);
+                break;
+            }
+        }
+
+        //
+        // ClientPINコマンド関連処理
+        //
+        private void DoRequestCommandGetKeyAgreement()
+        {
+            // 実行するコマンドを保持
+            CTAP2Parameter.CborCommand = FIDODefine.CTAP2_CBORCMD_CLIENT_PIN;
+            CTAP2Parameter.CborSubCommand = FIDODefine.CTAP2_SUBCMD_CLIENT_PIN_GET_AGREEMENT;
+
+            // GetAgreementコマンドバイトを生成
+            CBOREncoder cborEncoder = new CBOREncoder();
+            byte[] getAgreementCbor = CBOREncoder.GenerateGetKeyAgreementCbor(CTAP2Parameter.CborCommand, CTAP2Parameter.CborSubCommand);
+
+            // GetAgreementコマンドを実行
+            CommandProcess.RegisterHandlerOnCommandResponse(OnCommandResponseRef);
+            CommandProcess.DoRequestCtapHidCommand(HIDProcessConst.HID_CMD_CTAPHID_CBOR, getAgreementCbor);
+        }
+
+        private void DoResponseCommandGetKeyAgreement(byte[] cborBytes)
+        {
+            switch (Parameter.Command) {
+            default:
+                // 正しくレスポンスされなかったと判断し、画面に制御を戻す
+                NotifyCommandTerminated(Parameter.CommandTitle, AppCommon.MSG_OCCUR_UNKNOWN_ERROR, false);
+                break;
+            }
+        }
+
+        //
+        // CBORコマンド関連処理
+        //
+        private void DoResponseCtapHidCbor(byte[] message)
+        {
+            // ステータスバイトをチェック
+            string errorMessage;
+            if (CTAP2Util.CheckStatusByte(message, out errorMessage) == false) {
+                // 処理結果が不正の場合は画面に制御を戻す
+                NotifyCommandTerminated(Parameter.CommandTitle, errorMessage, false);
+                return;
+            }
+
+            switch (CTAP2Parameter.CborCommand) {
+            case FIDODefine.CTAP2_CBORCMD_CLIENT_PIN:
+                DoResponseCommandClientPin(message);
+                break;
+            default:
+                break;
+            }
+        }
+
+        private void DoResponseCommandClientPin(byte[] message)
+        {
+            // レスポンスされたCBORを抽出
+            byte[] cborBytes = AppUtil.ExtractCBORBytesFromResponse(message, message.Length);
+
+            switch (CTAP2Parameter.CborSubCommand) {
+            case FIDODefine.CTAP2_SUBCMD_CLIENT_PIN_GET_AGREEMENT:
+                DoResponseCommandGetKeyAgreement(cborBytes);
+                break;
+            default:
                 break;
             }
         }
@@ -73,6 +148,12 @@ namespace MaintenanceToolApp.FIDOSettings
             // INITからの戻りの場合
             if (CMD == HIDProcessConst.HID_CMD_CTAPHID_INIT) {
                 DoResponseCtapHidInit();
+                return;
+            }
+
+            // CBORからの戻りの場合
+            if (CMD == HIDProcessConst.HID_CMD_CTAPHID_CBOR) {
+                DoResponseCtapHidCbor(responseData);
                 return;
             }
 
