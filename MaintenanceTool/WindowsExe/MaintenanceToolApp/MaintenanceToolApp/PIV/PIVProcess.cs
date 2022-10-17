e764439cd3b8fc6f1e9c2358083a8b42639ecece
Makoto Morita <makmorit@outlook.jp>
2022-10-17 13:35:46 +0900

    PIV設定画面の実装
    ・処理／パラメータークラスを新設

---
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/PIV/PIVProcess.cs
@@ -0,0 +1,28 @@
+﻿using static MaintenanceToolApp.AppDefine;
+
+namespace MaintenanceToolApp.PIV
+{
+    public class PIVParameter
+    {
+        public Command Command { get; set; }
+        public string CommandTitle { get; set; }
+        public string CommandDesc { get; set; }
+
+        public PIVParameter()
+        {
+            Command = Command.COMMAND_NONE;
+            CommandTitle = string.Empty;
+            CommandDesc = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            string command = string.Format("Command:{0} CommandTitle:{1}", Command, CommandTitle);
+            return string.Format("{0}", command);
+        }
+    }
+
+    public class PIVProcess
+    {
+    }
+}
