d9aec7cd55a054eaca06d2ca9924ade5855e63bc
Makoto Morita <makmorit@outlook.jp>
2022-10-21 12:49:37 +0900

    準備作業
    ・状況確認のためのログ出力処理を一時的に追加

--- MaintenanceTool/macOSApp/ToolCommon/ToolHIDHelper.m
+++ MaintenanceTool/macOSApp/ToolCommon/ToolHIDHelper.m
@@ -37,6 +37,7 @@
         self = [super init];
         if (self) {
             [self setDelegate:delegate];
+            NSLog(@"%@", delegate);
             [self initializeHIDManager];
         }
         return self;
