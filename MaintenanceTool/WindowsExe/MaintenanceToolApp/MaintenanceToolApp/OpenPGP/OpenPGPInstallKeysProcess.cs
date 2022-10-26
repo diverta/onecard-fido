cc877d31d001bb670b0a5a83152bdf76190a323f
Makoto Morita <makmorit@outlook.jp>
2022-10-26 12:40:59 +0900

    リファクタリング
    ・PGP秘密鍵インストール処理を、すべて別クラス`OpenPGPInstallKeysProcess`に移設
    （ソースコードが長大になるのを回避するための措置）

---
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/OpenPGP/OpenPGPInstallKeysProcess.cs
@@ -0,0 +1,77 @@
+﻿using ToolAppCommon;
+using static MaintenanceToolApp.OpenPGP.Gpg4winParameter;
+
+namespace MaintenanceToolApp.OpenPGP
+{
+    internal class OpenPGPInstallKeysProcess
+    {
+        // 処理実行のためのプロパティー
+        private OpenPGPParameter Parameter = null!;
+
+        // 上位クラスに対するコールバックを保持
+        public delegate void HandlerOnCommandResponse(bool success, string errorMessage);
+        private HandlerOnCommandResponse OnCommandResponse = null!;
+
+        public void DoProcess(OpenPGPParameter parameterRef, HandlerOnCommandResponse handlerRef)
+        {
+            // パラメーターを保持
+            Parameter = parameterRef;
+
+            // コールバックを保持
+            OnCommandResponse = handlerRef;
+
+            // 主鍵生成処理から実行
+            DoRequestGenerateMainKey();
+        }
+
+        private void DoRequestGenerateMainKey()
+        {
+            // スクリプトを作業用フォルダーに生成
+            string scriptName = "generate_main_key.bat";
+            if (Gpg4winProcess.WriteScriptToTempFolder(scriptName, Parameter) == false) {
+                // エラー発生時は、作業用フォルダー消去処理に移行
+                OnCommandResponse(false, AppCommon.MSG_ERROR_OPENPGP_GENERATE_MAINKEY_GEN_BAT);
+                return;
+            }
+
+            // パラメーターファイルを作業用フォルダーに生成
+            string paramName = "generate_main_key.param";
+            if (Gpg4winProcess.WriteParamForGenerateMainKeyToTempFolder(paramName, Parameter) == false) {
+                // エラー発生時は、作業用フォルダー消去処理に移行
+                OnCommandResponse(false, AppCommon.MSG_ERROR_OPENPGP_GENERATE_MAINKEY_GEN_PAR);
+                return;
+            }
+
+            // スクリプトを実行し、主鍵を生成
+            string exe = string.Format("{0}\\{1}", Parameter.TempFolderPath, scriptName);
+            string args = string.Format("{0} {1} --no-tty", Parameter.TempFolderPath, Parameter.Passphrase);
+            Gpg4winParameter parameter = new Gpg4winParameter(GPGCommand.COMMAND_GPG_VERSION, exe, args, null!);
+            new Gpg4winProcess().DoRequestCommandLine(parameter, DoResponseGenerateMainKey);
+        }
+
+        private void DoResponseGenerateMainKey(bool success, string response, string error)
+        {
+            // レスポンス内容をチェック
+            if (Gpg4winUtility.CheckResponseOfScript(response)) {
+                // 生成鍵がCertify機能を有しているかチェック
+                string keyid = Gpg4winUtility.ExtractMainKeyIdFromResponse(response);
+                if (keyid != string.Empty) {
+                    // チェックOKの場合は鍵IDを保持し、次の処理に移行
+                    Parameter.GeneratedMainKeyId = keyid;
+                    AppLogUtil.OutputLogDebug(string.Format(AppCommon.MSG_FORMAT_OPENPGP_GENERATED_MAIN_KEY, Parameter.GeneratedMainKeyId));
+                    DoRequestAddSubKey();
+                    return;
+                }
+            }
+
+            // エラーメッセージを設定し、作業用フォルダー消去処理に移行
+            OnCommandResponse(false, AppCommon.MSG_ERROR_OPENPGP_GENERATE_MAINKEY_FAIL);
+        }
+
+        private void DoRequestAddSubKey()
+        {
+            // TODO: 仮の実装です。
+            OnCommandResponse(true, AppCommon.MSG_NONE);
+        }
+    }
+}
--- MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/OpenPGP/OpenPGPProcess.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/OpenPGP/OpenPGPProcess.cs
@@ -155,7 +155,7 @@ namespace MaintenanceToolApp.OpenPGP
             // コマンドに応じ、以下の処理に分岐
             switch (Parameter.Command) {
             case Command.COMMAND_OPENPGP_INSTALL_KEYS:
-                DoRequestGenerateMainKey();
+                DoRequestInstallKeys();
                 break;
             default:
                 NotifyProcessTerminated(false, AppCommon.MSG_OCCUR_UNKNOWN_ERROR);
@@ -193,52 +193,19 @@ namespace MaintenanceToolApp.OpenPGP
         //
         // PGP秘密鍵インストール関連
         //
-        private void DoRequestGenerateMainKey()
+        private void DoRequestInstallKeys()
         {
-            // スクリプトを作業用フォルダーに生成
-            string scriptName = "generate_main_key.bat";
-            if (Gpg4winProcess.WriteScriptToTempFolder(scriptName, Parameter) == false) {
+            new OpenPGPInstallKeysProcess().DoProcess(Parameter, DoResponseInstallKeys);
+        }
+
+        private void DoResponseInstallKeys(bool success, string errorMessage)
+        {
+            if (success == false) {
                 // エラー発生時は、作業用フォルダー消去処理に移行
-                DoRequestRemoveTempFolderWithInformative(false, AppCommon.MSG_ERROR_OPENPGP_GENERATE_MAINKEY_GEN_BAT);
+                DoRequestRemoveTempFolderWithInformative(false, errorMessage);
                 return;
             }
 
-            // パラメーターファイルを作業用フォルダーに生成
-            string paramName = "generate_main_key.param";
-            if (Gpg4winProcess.WriteParamForGenerateMainKeyToTempFolder(paramName, Parameter) == false) {
-                // エラー発生時は、作業用フォルダー消去処理に移行
-                DoRequestRemoveTempFolderWithInformative(false, AppCommon.MSG_ERROR_OPENPGP_GENERATE_MAINKEY_GEN_PAR);
-                return;
-            }
-
-            // スクリプトを実行し、主鍵を生成
-            string exe = string.Format("{0}\\{1}", Parameter.TempFolderPath, scriptName);
-            string args = string.Format("{0} {1} --no-tty", Parameter.TempFolderPath, Parameter.Passphrase);
-            Gpg4winParameter parameter = new Gpg4winParameter(GPGCommand.COMMAND_GPG_VERSION, exe, args, null!);
-            new Gpg4winProcess().DoRequestCommandLine(parameter, DoResponseGenerateMainKey);
-        }
-
-        private void DoResponseGenerateMainKey(bool success, string response, string error)
-        {
-            // レスポンス内容をチェック
-            if (Gpg4winUtility.CheckResponseOfScript(response)) {
-                // 生成鍵がCertify機能を有しているかチェック
-                string keyid = Gpg4winUtility.ExtractMainKeyIdFromResponse(response);
-                if (keyid != string.Empty) {
-                    // チェックOKの場合は鍵IDを保持し、次の処理に移行
-                    Parameter.GeneratedMainKeyId = keyid;
-                    AppLogUtil.OutputLogDebug(string.Format(AppCommon.MSG_FORMAT_OPENPGP_GENERATED_MAIN_KEY, Parameter.GeneratedMainKeyId));
-                    DoRequestAddSubKey();
-                    return;
-                }
-            }
-
-            // エラーメッセージを設定し、作業用フォルダー消去処理に移行
-            DoRequestRemoveTempFolderWithInformative(false, AppCommon.MSG_ERROR_OPENPGP_GENERATE_MAINKEY_FAIL);
-        }
-
-        private void DoRequestAddSubKey()
-        {
             // TODO: 仮の実装です。
             Parameter.CommandSuccess = true;
             DoRequestRemoveTempFolder();
