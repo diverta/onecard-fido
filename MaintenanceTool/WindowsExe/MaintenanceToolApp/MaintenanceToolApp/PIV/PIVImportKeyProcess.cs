bc77749369f105e4c129cb58f1af2e4fa5484416
Makoto Morita <makmorit@outlook.jp>
2022-10-31 10:53:08 +0900

    PIV鍵・証明書インストール処理の実装
    ・鍵・証明書インポート処理用のパラメータークラスを新設

---
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/PIV/PIVImportKeyProcess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace MaintenanceToolApp.PIV
+{
+    public class PIVImportKeyParameter
+    {
+        // 鍵・証明書のアルゴリズムを保持
+        public string PkeyAlgName { get; set; }
+        public string CertAlgName { get; set; }
+
+        // 秘密鍵インポート用APDU
+        public byte[] PkeyAPDUBytes = Array.Empty<byte>();
+
+        // 証明書インポート用APDU
+        public byte[] CertAPDUBytes = Array.Empty<byte>();
+
+        // RSA鍵のバイナリーイメージ
+        public byte[] RsaEBytes = Array.Empty<byte>();
+        public byte[] RsaPBytes = Array.Empty<byte>();
+        public byte[] RsaQBytes = Array.Empty<byte>();
+        public byte[] RsaDpBytes = Array.Empty<byte>();
+        public byte[] RsaDqBytes = Array.Empty<byte>();
+        public byte[] RsaQinvBytes = Array.Empty<byte>();
+
+        // EC鍵のバイナリーイメージ
+        public byte[] ECPrivKeyBytes = Array.Empty<byte>();
+
+        // 証明書のバイナリーイメージ
+        public byte[] CertBytes = Array.Empty<byte>();
+
+
+        public PIVImportKeyParameter()
+        {
+            PkeyAlgName = string.Empty;
+            CertAlgName = string.Empty;
+        }
+    }
+
+    internal class PIVImportKeyProcess
+    {
+    }
+}
