6f5eb6e5dcd532669c36278826a0e15154e16c5a
Makoto Morita <makmorit@outlook.jp>
2022-11-07 14:54:17 +0900

    リファクタリング
    ・不要となった処理を削除

--- MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/OpenPGP/OpenPGPPinManagementProcess.cs
+++ MaintenanceTool/WindowsExe/MaintenanceToolApp/MaintenanceToolApp/OpenPGP/OpenPGPPinManagementProcess.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Text;
 using ToolAppCommon;
 using static MaintenanceToolApp.AppDefine;
-using static MaintenanceToolApp.OpenPGP.Gpg4winParameter;
 
 namespace MaintenanceToolApp.OpenPGP
 {
@@ -229,84 +228,6 @@ namespace MaintenanceToolApp.OpenPGP
 
             // 上位クラスに制御を戻す
             OnCommandResponse(true, AppCommon.MSG_NONE);
-        }
-
-        private void DoRequestCardEditPasswdCommand()
-        {
-            // スクリプトを作業用フォルダーに生成
-            string scriptName = "card_edit_passwd.bat";
-            if (Gpg4winScriptUtil.WriteScriptToTempFolder(scriptName, Parameter) == false) {
-                // エラー発生時は、作業用フォルダー消去処理に移行
-                OnCommandResponse(false, string.Format(AppCommon.MSG_FORMAT_OPENPGP_CARD_EDIT_PASSWD_GEN_BAT_ERR, Parameter.CommandTitle));
-                return;
-            }
-
-            // パラメーターファイル名を設定
-            string paramName;
-            GPGCommand command;
-            if (Parameter.Command == Command.COMMAND_OPENPGP_UNBLOCK) {
-                paramName = "card_edit_unblock.param";
-                command = GPGCommand.COMMAND_GPG_CARD_EDIT_UNBLOCK;
-            } else {
-                paramName = "card_edit_passwd.param";
-                command = GPGCommand.COMMAND_GPG_CARD_EDIT_PASSWD;
-            }
-
-            // パラメーターファイルを作業用フォルダーに生成
-            if (Gpg4winScriptUtil.WriteParamForCardEditUnblockToTempFolder(paramName, Parameter) == false) {
-                // エラー発生時は、作業用フォルダー消去処理に移行
-                OnCommandResponse(false, string.Format(AppCommon.MSG_FORMAT_OPENPGP_CARD_EDIT_PASSWD_GEN_PAR_ERR, Parameter.CommandTitle));
-                return;
-            }
-
-            // スクリプトを実行
-            string exe = string.Format("{0}\\{1}", Parameter.TempFolderPath, scriptName);
-            string args = string.Format("{0} {1} --no-tty", Parameter.TempFolderPath, paramName);
-            Gpg4winParameter param = new Gpg4winParameter(command, exe, args, Parameter.TempFolderPath);
-            new Gpg4winProcess().DoRequestCommandLine(param, DoResponseCardEditPasswdCommand);
-        }
-
-        private void DoResponseCardEditPasswdCommand(bool success, string response, string error)
-        {
-            // 変数を初期化
-            bool commandSuccess = false;
-            string errorMessage = AppCommon.MSG_NONE;
-
-            // レスポンスをチェック
-            if (success == false) {
-                // スクリプトエラーの場合はOpenPGP cardエラーをチェック
-                if (Gpg4winUtility.CheckIfCardErrorFromResponse(error)) {
-                    errorMessage = AppCommon.MSG_ERROR_OPENPGP_SELECTING_CARD_FAIL;
-                } else {
-                    errorMessage = string.Format(AppCommon.MSG_FORMAT_OPENPGP_CARD_EDIT_PASSWD_ERR, Parameter.CommandTitle);
-                }
-
-            } else {
-                // 成功 or 失敗メッセージが出力されているかどうかチェック
-                if (Gpg4winUtility.CheckIfOperationSuccess(response)) {
-                    commandSuccess = true;
-                } else {
-                    string itemName = ItemNameForCardEditPasswdCommand();
-                    errorMessage = string.Format(AppCommon.MSG_FORMAT_OPENPGP_CARD_EDIT_PASSWD_NG, itemName);
-                }
-            }
-
-            // 作業用フォルダー消去処理に移行
-            OnCommandResponse(commandSuccess, errorMessage);
-        }
-
-        private string ItemNameForCardEditPasswdCommand()
-        {
-            switch (Parameter.Command) {
-            case Command.COMMAND_OPENPGP_UNBLOCK_PIN:
-            case Command.COMMAND_OPENPGP_CHANGE_ADMIN_PIN:
-            case Command.COMMAND_OPENPGP_SET_RESET_CODE:
-                return AppCommon.MSG_LABEL_ITEM_PGP_ADMIN_PIN;
-            case Command.COMMAND_OPENPGP_UNBLOCK:
-                return AppCommon.MSG_LABEL_ITEM_PGP_RESET_CODE;
-            default:
-                return AppCommon.MSG_LABEL_ITEM_PGP_PIN;
-            }
         }
     }
 }
