38c459920009aa5ef00a427e3422ce1cd1884d8c
Makoto Morita <makmorit@outlook.jp>
2022-11-09 15:56:42 +0900

    RTCC関連処理の実装
    ・RTCC操作用モジュールを新設

---
+++ nRF52840_app/examples/diverta/plat_lib/rv3028c7_i2c.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   rv3028c7_i2c.c
+ * Author: makmorit
+ *
+ * Created on 2022/11/09, 15:43
+ */
+#include <stdbool.h>
+
+// for logging informations
+#define NRF_LOG_MODULE_NAME rv3028c7_i2c
+#include "nrf_log.h"
+NRF_LOG_MODULE_REGISTER();
+
+bool rv3028c7_initialize(void)
+{
+    // TODO: 仮の実装です。
+    return true;
+}
---
+++ nRF52840_app/examples/diverta/plat_lib/rv3028c7_i2c.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   rv3028c7_i2c.h
+ * Author: makmorit
+ *
+ * Created on 2022/11/09, 15:43
+ */
+#ifndef RV3028C7_I2C_H
+#define RV3028C7_I2C_H
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//
+// 関数群
+//
+bool rv3028c7_initialize(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RV3028C7_I2C_H */
