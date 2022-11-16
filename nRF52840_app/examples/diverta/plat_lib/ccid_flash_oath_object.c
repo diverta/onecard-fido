556c70445090122d656d6d336ef4577d937701d0
Makoto Morita <makmorit@outlook.jp>
2022-11-16 15:56:56 +0900

    OATH機能の搭載
    ・リセット機能の実装
    ・コールバック処理を本格実装化

--- nRF52840_app/examples/diverta/plat_lib/ccid_flash_oath_object.c
+++ nRF52840_app/examples/diverta/plat_lib/ccid_flash_oath_object.c
@@ -11,12 +11,19 @@
 
 #include "ccid_flash_object.h"
 #include "ccid_oath.h"
+#include "ccid_oath_define.h"
 #include "ccid_oath_object.h"
 
 // for logging informations
 #define NRF_LOG_MODULE_NAME ccid_flash_oath_object
 #include "nrf_log.h"
 NRF_LOG_MODULE_REGISTER();
+
+// バッファサイズの最大長
+#define MAX_BUF_SIZE     (OATH_DATA_OBJ_WORDS_MAX+1)
+
+// Flash ROM書込み時に実行した関数の参照を保持
+static void *m_flash_func = NULL;
 
 //
 // オブジェクトのRead／Write
@@ -41,8 +48,12 @@ bool ccid_flash_oath_object_delete(uint16_t obj_tag, uint8_t *p_unique_key, size
 
 bool ccid_flash_oath_object_delete_all(void)
 {
-    // TODO: 仮の実装です。
-    return false;
+    // ファイル名を取得
+    uint16_t file_id = OATH_DATA_OBJ_FILE_ID;
+
+    // データファイルをFlash ROMから削除
+    m_flash_func = (void *)ccid_flash_oath_object_delete_all;
+    return fido_flash_fds_file_delete(file_id);
 }
 
 bool ccid_flash_oath_object_fetch(uint16_t obj_tag, int (*_fetch_func)(const char *key, void *data, size_t size))
@@ -56,12 +67,36 @@ bool ccid_flash_oath_object_fetch(uint16_t obj_tag, int (*_fetch_func)(const cha
 //
 void ccid_flash_oath_object_failed(void)
 {
-    // TODO: 仮の実装です。
+    if (m_flash_func == NULL) {
+        return;
+    }
+
+    // 判定用の参照を初期化
+    void *flash_func = m_flash_func;
+    m_flash_func = NULL;
+
+    // Flash ROM処理でエラーが発生時はエラーレスポンス送信
+    if (flash_func == ccid_flash_oath_object_delete_all) {
+        ccid_oath_object_write_resume(false);
+    }
 }
 
 void ccid_flash_oath_object_gc_done(void)
 {
-    // TODO: 仮の実装です。
+    if (m_flash_func == NULL) {
+        return;
+    }
+
+    // 判定用の参照を初期化
+    void *flash_func = m_flash_func;
+    m_flash_func = NULL;
+
+    // for nRF52840:
+    // FDSリソース不足解消のためGCが実行された場合は、
+    // GC実行直前の処理を再実行
+    if (flash_func == ccid_flash_oath_object_delete_all) {
+        ccid_oath_object_write_retry();
+    }
 }
 
 void ccid_flash_oath_object_record_updated(void)
@@ -71,5 +106,16 @@ void ccid_flash_oath_object_record_updated(void)
 
 void ccid_flash_oath_object_record_deleted(void)
 {
-    // TODO: 仮の実装です。
+    if (m_flash_func == NULL) {
+        return;
+    }
+
+    // 判定用の参照を初期化
+    void *flash_func = m_flash_func;
+    m_flash_func = NULL;
+
+    // 正常系の後続処理を実行
+    if (flash_func == ccid_flash_oath_object_delete_all) {
+        ccid_oath_object_write_resume(true);
+    }
 }
