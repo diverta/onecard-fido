a01dfbbbb6cff107416c0117b779c5e52a786f0d
Makoto Morita <makmorit@outlook.jp>
2022-11-17 16:43:07 +0900

    リファクタリング
    ・レコード走査（fetch）ロジックを、他の処理（list、delete）でも使用できるよう構造化

--- nRF52840_app/examples/diverta/plat_lib/ccid_flash_oath_object.c
+++ nRF52840_app/examples/diverta/plat_lib/ccid_flash_oath_object.c
@@ -22,6 +22,11 @@ NRF_LOG_MODULE_REGISTER();
 // バッファサイズの最大長
 #define MAX_BUF_SIZE     (OATH_DATA_OBJ_WORDS_MAX+1)
 
+//
+// FDSによりアクセスした物理レコード情報を保持
+//
+static fds_record_desc_t m_fds_record_desc;
+
 // Flash ROM書込み時に実行した関数の参照を保持
 static void *m_flash_func = NULL;
 
@@ -67,29 +72,33 @@ bool ccid_flash_oath_object_write(uint16_t obj_tag, uint8_t *obj_buff, size_t ob
     return fido_flash_fds_record_write(file_id, record_key, record_words, read_buffer, write_buffer);
 }
 
-static bool record_find(uint16_t file_id, uint16_t record_key, uint8_t *p_unique_key, size_t unique_key_size, size_t unique_key_offset)
+static bool fetch_records(uint16_t file_id, uint16_t record_key, uint8_t *p_unique_key, size_t unique_key_size, size_t unique_key_offset, bool (*function)(fds_record_desc_t *record_desc), bool *exist)
 {
     ret_code_t ret;
     uint32_t *read_buffer = (uint32_t *)ccid_flash_object_read_buffer();
     
     // Flash ROMから既存データを走査
+    bool success = true;
     bool found = false;
-    fds_record_desc_t record_desc;
-    fds_find_token_t  ftok = {0};
+    fds_find_token_t ftok = {0};
     do {
-        ret = fds_record_find(file_id, record_key, &record_desc, &ftok);
+        ret = fds_record_find(file_id, record_key, &m_fds_record_desc, &ftok);
         if (ret == NRF_SUCCESS) {
             // 同じキーのレコードかどうか判定 (先頭バイトを比較)
-            fido_flash_fds_record_get(&record_desc, read_buffer);
+            fido_flash_fds_record_get(&m_fds_record_desc, read_buffer);
             uint8_t *p_read_buffer = (uint8_t *)read_buffer;
             if (memcmp(p_unique_key, p_read_buffer + unique_key_offset, unique_key_size) == 0) {
                 found = true;
+                if (function != NULL) {
+                    success = function(&m_fds_record_desc);
+                }
                 break;
             }
         }
     } while (ret == NRF_SUCCESS && found == false);
 
-    return found;
+    *exist = found;
+    return success;
 }
 
 bool ccid_flash_oath_object_find(uint16_t obj_tag, uint8_t *p_unique_key, size_t unique_key_size, uint8_t *p_record_buffer, bool *exist, uint16_t *serial)
@@ -106,7 +115,7 @@ bool ccid_flash_oath_object_find(uint16_t obj_tag, uint8_t *p_unique_key, size_t
     // Flash ROMから属性データ（オブジェクト長＝１ワード）を読込
     // 既存データがなければ、最初の連番をセットして終了
     size_t unique_key_offset = OATH_DATA_OBJ_ATTR_WORDS * 4;
-    *exist = record_find(file_id, record_key, p_unique_key, unique_key_size, unique_key_offset);
+    fetch_records(file_id, record_key, p_unique_key, unique_key_size, unique_key_offset, NULL, exist);
     if (*exist == false) {
         *serial = 0;
         return true;
