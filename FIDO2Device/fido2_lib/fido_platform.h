7ad3c29cc84f4069400ccc6b4aff88ea3c339b91
Makoto Morita <makmorit@outlook.jp>
2022-11-21 15:23:26 +0900

    リファクタリング
    ・ラッパーモジュールのヘッダーファイルと、定義が重複している分を削除

--- FIDO2Device/fido2_lib/fido_platform.h
+++ FIDO2Device/fido2_lib/fido_platform.h
@@ -41,34 +41,9 @@ extern "C" {
 #include "app_platform.h"
 
 #else
-// ハードウェアの差異に依存しない定義を集約
-#include "fido_platform_common.h"
+// nRF5 SDKに依存する処理
+#include "fido_log.h"
 
-// ハードウェアの差異に依存する定義を集約
-#include "ble_service_common.h"
-#include "ccid_crypto.h"
-#include "fido_crypto_plat.h"
-#include "fido_crypto_keypair.h"
-#include "fido_crypto_sskey.h"
-
-//
-// fido_flash_skey_cert.c
-//
-bool      fido_flash_skey_cert_delete(void);
-bool      fido_flash_skey_cert_write(void);
-bool      fido_flash_skey_cert_read(void);
-bool      fido_flash_skey_cert_available(void);
-bool      fido_flash_skey_cert_data_prepare(uint8_t *data, uint16_t length);
-uint32_t *fido_flash_skey_cert_data(void);
-uint8_t  *fido_flash_skey_data(void);
-uint8_t  *fido_flash_cert_data(void);
-uint32_t  fido_flash_cert_data_length(void);
-
-//
-// fido_flash_password.c
-//
-uint8_t *fido_flash_password_get(void);
-bool     fido_flash_password_set(uint8_t *random_vector);
 #endif
 
 #ifdef __cplusplus
