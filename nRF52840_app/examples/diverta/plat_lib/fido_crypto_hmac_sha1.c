d0a9cf643183fbc7b9000b11ded491e8286678a6
Makoto Morita <makmorit@outlook.jp>
2022-11-24 19:05:39 +0900

    HMAC-SHA1対応
    ・`fido_crypto_hmac_sha1`と`sha1`の両モジュールを結合

--- nRF52840_app/examples/diverta/plat_lib/fido_crypto_hmac_sha1.c
+++ nRF52840_app/examples/diverta/plat_lib/fido_crypto_hmac_sha1.c
@@ -11,11 +11,34 @@
 #include "nrf_log.h"
 NRF_LOG_MODULE_REGISTER();
 
+//
+// SHA1関連処理
+//
+#include "sha1.h"
+#include "sha1_define.h"
+
+//
+// SHA1ハッシュ計算の元になる任意データを配列で保持
+//
+static uint8_t *challenge_addrs[2];
+static size_t   challenge_sizes[2];
+
+//
+// HMAC-SHA1計算処理
+//
 void fido_crypto_hmac_sha1_calculate(
     uint8_t *key_data, size_t key_data_size, 
     uint8_t *src_data, size_t src_data_size, uint8_t *src_data_2, size_t src_data_2_size,
     uint8_t *dest_data)
 {
-    // TODO: 仮の実装です。
-    NRF_LOG_ERROR("fido_crypto_hmac_sha1_calculate not implemented");
+    challenge_addrs[0] = src_data;
+    challenge_sizes[0] = src_data_size;
+    if (src_data_2 != NULL && src_data_2_size > 0) {
+        challenge_addrs[1] = src_data_2;
+        challenge_sizes[1] = src_data_2_size;
+        hmac_sha1_vector(key_data, key_data_size, 2, challenge_addrs, challenge_sizes, dest_data);
+
+    } else {
+        hmac_sha1_vector(key_data, key_data_size, 1, challenge_addrs, challenge_sizes, dest_data);
+    }
 }
