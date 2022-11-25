9a694da1b91ff503940a254147f64603ee7537ca
Makoto Morita <makmorit@outlook.jp>
2022-11-25 10:07:22 +0900

    HMAC-SHA1対応
    ・SHA-1ハッシュ計算処理を実装
    （`sha1_init`）

--- nRF52840_app/examples/diverta/plat_lib/sha1.c
+++ nRF52840_app/examples/diverta/plat_lib/sha1.c
@@ -7,7 +7,34 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#include "sdk_common.h"
+
+// for logging informations
+#define NRF_LOG_MODULE_NAME sha1
+#include "nrf_log.h"
+NRF_LOG_MODULE_REGISTER();
+
+struct sha1_context {
+    uint32_t state[5];
+    uint32_t count[2];
+    unsigned char buffer[64];
+};
+typedef struct sha1_context SHA1_CTX;
+
+static void sha1_init(SHA1_CTX* context)
+{
+    // SHA1 initialization constants
+    context->state[0] = 0x67452301;
+    context->state[1] = 0xEFCDAB89;
+    context->state[2] = 0x98BADCFE;
+    context->state[3] = 0x10325476;
+    context->state[4] = 0xC3D2E1F0;
+    context->count[0] = context->count[1] = 0;
+}
+
 void sha1_hash_calculate(size_t num_elem, uint8_t *addr[], size_t *len, uint8_t *mac)
 {
     // TODO: 仮の実装です。
+    SHA1_CTX ctx;
+    sha1_init(&ctx);
 }
