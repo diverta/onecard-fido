9cb3f5103e6242a18b00512a67cb91db73825490
Makoto Morita <makmorit@outlook.jp>
2023-01-11 14:21:50 +0900

    ベンダー向け機能追加対応
    ・画面にボタンを配置
    ・コマンドIDを再編成
    ・不要な参照／処理等を削除

--- MaintenanceTool/macOSApp/VendorMaintenanceToolApp/VendorFunctionWindow.m
+++ MaintenanceTool/macOSApp/VendorMaintenanceToolApp/VendorFunctionWindow.m
@@ -6,20 +6,17 @@
 //
 #import "AppDefine.h"
 #import "UtilityCommand.h"
+#import "VendorFunctionCommand.h"
 #import "VendorFunctionWindow.h"
 
 @interface VendorFunctionWindow ()
 
-    // 画面項目
-    @property (assign) IBOutlet NSButton                   *buttonToolVersionInfo;
-    @property (assign) IBOutlet NSButton                   *buttonViewLogFile;
-    @property (assign) IBOutlet NSButton                   *buttonCancel;
     // 親画面の参照を保持
     @property (nonatomic) NSWindow                         *parentWindow;
     // コマンドクラスの参照を保持
-    @property (nonatomic, weak) UtilityCommand             *utilityCommand;
-    // 実行するコマンドを保持
-    @property (nonatomic) Command                           command;
+    @property (nonatomic, weak) id                          vendorFunctionCommandRef;
+    // 処理パラメーターを保持
+    @property (nonatomic) VendorFunctionCommandParameter   *commandParameterRef;
 
 @end
 
@@ -29,43 +26,49 @@
         [super windowDidLoad];
     }
 
-    - (void)setParentWindowRef:(id)ref {
+    - (void)setParentWindowRef:(id)ref withCommandRef:(id)commandRef withParameterRef:(id)parameterRef {
         // 親画面の参照を保持
         [self setParentWindow:(NSWindow *)ref];
-    }
-
-    - (void)setCommandRef:(id)ref {
         // コマンドクラスの参照を保持
-        [self setUtilityCommand:(UtilityCommand *)ref];
+        [self setVendorFunctionCommandRef:commandRef];
+        // ヘルスチェック処理のパラメーターを保持
+        [self setCommandParameterRef:parameterRef];
+        // パラメーターを初期化
+        [[self commandParameterRef] setCommand:COMMAND_NONE];
     }
 
-    - (IBAction)buttonToolVersionInfoDidPress:(id)sender {
-        // このウィンドウを終了
-        [self terminateWindow:NSModalResponseOK withCommand:COMMAND_VIEW_APP_VERSION];
+    - (IBAction)buttonInstallAttestationDidPress:(id)sender {
+        // 実行コマンドを設定して画面を閉じる
+        [[self commandParameterRef] setCommand:COMMAND_INSTALL_ATTESTATION];
+        [self terminateWindow:NSModalResponseOK];
     }
 
-    - (IBAction)buttonViewLogFileDidPress:(id)sender {
-        // このウィンドウを終了
-        [self terminateWindow:NSModalResponseOK withCommand:COMMAND_VIEW_LOG_FILE];
+    - (IBAction)buttonRemoveAttestationDidPress:(id)sender {
+        // 実行コマンドを設定して画面を閉じる
+        [[self commandParameterRef] setCommand:COMMAND_REMOVE_ATTESTATION];
+        [self terminateWindow:NSModalResponseOK];
+    }
+
+    - (IBAction)buttonBootloaderModeDidPress:(id)sender {
+        // 実行コマンドを設定して画面を閉じる
+        [[self commandParameterRef] setCommand:COMMAND_HID_BOOTLOADER_MODE];
+        [self terminateWindow:NSModalResponseOK];
+    }
+
+    - (IBAction)buttonFirmwareResetDidPress:(id)sender {
+        // 実行コマンドを設定して画面を閉じる
+        [[self commandParameterRef] setCommand:COMMAND_HID_FIRMWARE_RESET];
+        [self terminateWindow:NSModalResponseOK];
     }
 
     - (IBAction)buttonCancelDidPress:(id)sender {
         // このウィンドウを終了
-        [self terminateWindow:NSModalResponseCancel withCommand:COMMAND_NONE];
+        [self terminateWindow:NSModalResponseCancel];
     }
 
-    - (void)terminateWindow:(NSModalResponse)response withCommand:(Command)command {
-        // 実行コマンドを保持
-        [self setCommand:command];
+    - (void)terminateWindow:(NSModalResponse)response {
         // この画面を閉じる
         [[self parentWindow] endSheet:[self window] returnCode:response];
-    }
-
-#pragma mark - Interface for parameters
-
-    - (Command)commandToPerform {
-        // 実行コマンドを戻す
-        return [self command];
     }
 
 @end
