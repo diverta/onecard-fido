a6fe8da967942636a34a88840d034f67f4b0b7b2
Makoto Morita <makmorit@outlook.jp>
2023-01-13 09:29:44 +0900

    鍵・証明書関連コマンド修正対応
    ・鍵・証明書関連処理を集約したモジュールの新設

---
+++ FIDO2Device/fido2_lib/fido_maintenance_attestation.c
@@ -0,0 +1,6 @@
+/* 
+ * File:   fido_attestation.c
+ * Author: makmorit
+ *
+ * Created on 2023/01/13, 9:24
+ */
---
+++ FIDO2Device/fido2_lib/fido_maintenance_attestation.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   fido_maintenance_attestation.h
+ * Author: makmorit
+ *
+ * Created on 2023/01/13, 9:27
+ */
+#ifndef FIDO_MAINTENANCE_ATTESTATION_H
+#define FIDO_MAINTENANCE_ATTESTATION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* FIDO_MAINTENANCE_ATTESTATION_H */
