6a753c6c40d8fd0e15757cabb8cf1b58785d9783
Makoto Morita <makmorit@outlook.jp>
2023-01-23 11:31:00 +0900

    ペアリング解除要求機能の追加
    ・ペアリング解除要求画面の新設

---
+++ MaintenanceTool/dotNET/MaintenanceToolApp/MaintenanceToolApp/BLESettings/UnpairingRequestWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using MaintenanceToolApp.CommonWindow;
+using System;
+using System.Windows;
+
+namespace MaintenanceToolApp.BLESettings
+{
+    /// <summary>
+    /// UnpairingRequestWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class UnpairingRequestWindow : Window
+    {
+        public UnpairingRequestWindow()
+        {
+            InitializeComponent();
+        }
+
+        public bool ShowDialogWithOwner(Window ownerWindow)
+        {
+            // この画面を、オーナー画面の中央にモード付きで表示
+            Owner = ownerWindow;
+            bool? b = ShowDialog();
+            if (b == null) {
+                return false;
+            } else {
+                return (bool)b;
+            }
+        }
+
+        private void TerminateWindow(bool dialogResult)
+        {
+            // この画面を閉じる
+            DialogResult = dialogResult;
+            Close();
+        }
+
+        //
+        // イベント処理部
+        // 
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            TerminateWindow(false);
+        }
+
+        //
+        // 閉じるボタンの無効化
+        //
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            CommonWindowUtil.DisableCloseWindowButton(this);
+        }
+    }
+}
