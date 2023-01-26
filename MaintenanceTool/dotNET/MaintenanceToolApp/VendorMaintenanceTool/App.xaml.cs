1fa67c9134708c03ceeee53b84dbc1e9edca7a6b
Makoto Morita <makmorit@outlook.jp>
2023-01-26 12:12:57 +0900

    ベンダー向けバンドルの新設
    ・開始用モジュール（`App.xaml`）を新設

---
+++ MaintenanceTool/dotNET/MaintenanceToolApp/VendorMaintenanceTool/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using MaintenanceToolApp;
+using System.Reflection;
+using System.Threading;
+using System.Windows;
+
+namespace VendorMaintenanceTool
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        // Mutex作成
+        private readonly Mutex MutexRef = new Mutex(false, Assembly.GetExecutingAssembly().GetName().Name);
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Mutexの所有権を要求
+            if (MutexRef.WaitOne(0, false) == false) {
+                MessageBox.Show(AppCommon.MSG_ERROR_DOUBLE_START, AppCommon.MSG_TOOL_TITLE);
+                MutexRef.Close();
+                Shutdown();
+            }
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            // 業務終了時の処理
+            CommandProcess.OnMainWindowTerminated();
+
+            // Mutexを解放
+            if (MutexRef != null) {
+                MutexRef.ReleaseMutex();
+                MutexRef.Close();
+            }
+            base.OnExit(e);
+        }
+    }
+}
