f049382efdff99cc1703aaf1eaab8d25b292dc0f
Makoto Morita <makmorit@outlook.jp>
2023-02-02 14:09:37 +0900

    FIDO鍵・証明書管理機能の新設
    ・FIDO鍵・証明書削除コマンドの実行処理を実装

--- MaintenanceTool/dotNET/MaintenanceToolApp/VendorMaintenanceTool/FIDOSettings/FIDOAttestationProcess.cs
+++ MaintenanceTool/dotNET/MaintenanceToolApp/VendorMaintenanceTool/FIDOSettings/FIDOAttestationProcess.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using ToolAppCommon;
 using VendorMaintenanceTool.VendorFunction;
+using static MaintenanceToolApp.FIDODefine;
 
 namespace VendorMaintenanceTool.FIDOSettings
 {
@@ -10,6 +11,9 @@ namespace VendorMaintenanceTool.FIDOSettings
         // 上位クラスに対するコールバックを保持
         public delegate void HandlerOnNotifyCommandTerminated(bool success, string errorMessage);
         private HandlerOnNotifyCommandTerminated OnNotifyCommandTerminated = null!;
+
+        // HIDからデータ受信時のコールバック参照
+        private readonly CommandProcess.HandlerOnCommandResponse OnCommandResponseRef;
 
         // 処理実行のためのプロパティー
         private readonly VendorFunctionParameter Parameter = null!;
@@ -21,6 +25,9 @@ namespace VendorMaintenanceTool.FIDOSettings
         {
             // パラメーターの参照を保持
             Parameter = param;
+
+            // コールバック参照を初期化
+            OnCommandResponseRef = new CommandProcess.HandlerOnCommandResponse(OnCommandResponse);
         }
 
         public void DoProcess(HandlerOnNotifyCommandTerminated handlerRef)
@@ -28,11 +35,91 @@ namespace VendorMaintenanceTool.FIDOSettings
             // コールバックを保持
             OnNotifyCommandTerminated = handlerRef;
 
+            // CTAPHID_INITから実行
+            DoRequestCtapHidInit();
+        }
+
+        //
+        // 内部処理
+        //
+        private void DoRequestCtapHidInit()
+        {
+            // INITコマンドを実行し、nonce を送信する
+            CommandProcess.RegisterHandlerOnCommandResponse(OnCommandResponseRef);
+            CommandProcess.DoRequestCtapHidInit();
+        }
+
+        private void DoResponseCtapHidInit()
+        {
+            if (Parameter.Command == AppDefine.Command.COMMAND_INSTALL_SKEY_CERT) {
+                DoRequestHidInstallAttestation();
+            }
+            if (Parameter.Command == AppDefine.Command.COMMAND_ERASE_SKEY_CERT) {
+                DoRequestHidRemoveAttestation();
+            }
+        }
+
+        private void DoRequestHidInstallAttestation()
+        {
             // TODO: 仮の実装です。
-            AppLogUtil.OutputLogInfo(Parameter.KeyPath);
-            AppLogUtil.OutputLogInfo(Parameter.CertPath);
             Thread.Sleep(2000);
             OnNotifyCommandTerminated(true, AppCommon.MSG_NONE);
+        }
+
+        private void DoResponseHidInstallAttestation()
+        {
+        }
+
+        private void DoRequestHidRemoveAttestation()
+        {
+            // 鍵・証明書削除コマンドを実行する
+            CommandProcess.RegisterHandlerOnCommandResponse(OnCommandResponseRef);
+            CommandProcess.DoRequestCtapHidCommand(0x80 | MNT_COMMAND_BASE, new byte[] { MNT_COMMAND_RESET_ATTESTATION });
+        }
+
+        private void DoResponseHidRemoveAttestation()
+        {
+            OnNotifyCommandTerminated(true, AppCommon.MSG_NONE);
+        }
+
+        //
+        // HIDからのレスポンス振分け処理
+        //
+        private void OnCommandResponse(byte CMD, byte[] responseData, bool success, string errorMessage)
+        {
+            // イベントを解除
+            CommandProcess.UnregisterHandlerOnCommandResponse(OnCommandResponseRef);
+
+            if (Parameter.Command != AppDefine.Command.COMMAND_INSTALL_SKEY_CERT &&
+                Parameter.Command != AppDefine.Command.COMMAND_ERASE_SKEY_CERT) {
+                return;
+            }
+
+            // 即時でアプリケーションに制御を戻す
+            if (success == false) {
+                OnNotifyCommandTerminated(success, errorMessage);
+                return;
+            }
+
+            // INITからの戻りの場合
+            if (CMD == HIDProcessConst.HID_CMD_CTAPHID_INIT) {
+                DoResponseCtapHidInit();
+                return;
+            }
+
+            // レスポンスメッセージの１バイト目（ステータスコード）を確認
+            if (responseData[0] != 0x00) {
+                string msg = string.Format(AppCommon.MSG_OCCUR_UNKNOWN_ERROR_ST, responseData[0]);
+                OnNotifyCommandTerminated(false, msg);
+            }
+
+            // 後続の処理を実行
+            if (Parameter.Command == AppDefine.Command.COMMAND_INSTALL_SKEY_CERT) {
+                DoResponseHidInstallAttestation();
+            }
+            if (Parameter.Command == AppDefine.Command.COMMAND_ERASE_SKEY_CERT) {
+                DoResponseHidRemoveAttestation();
+            }
         }
     }
 }
