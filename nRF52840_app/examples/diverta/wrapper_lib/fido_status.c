08e8d14ed39bdca92ceaad761d058b0d33ba21b0
Makoto Morita <makmorit@outlook.jp>
2023-02-07 14:18:20 +0900

    スリープ状態遷移の抑止対応
    ・以下２点を実装
    (1) BLEペリフェラルをビジー状態に遷移し、基板上のボタン押下を無効化
    (2) BLEペリフェラルのビジー状態を解除し、基板上のボタン押下を有効化
    (3) ペアリング要求時は、自動的にビジー状態に遷移
    (4) ペアリングエラー時は、自動的にビジー状態を解除

--- nRF52840_app/examples/diverta/wrapper_lib/fido_status.c
+++ nRF52840_app/examples/diverta/wrapper_lib/fido_status.c
@@ -4,10 +4,16 @@
  *
  * Created on 2023/02/07, 12:34
  */
+#include "ble_service_common.h"
+
 void fido_status_set_to_idle(void)
 {
+    // BLEペリフェラルモード時の処理抑止を解除
+    ble_service_peripheral_set_busy(false);
 }
 
 void fido_status_set_to_busy(void)
 {
+    // BLEペリフェラルモード時の処理を抑止
+    ble_service_peripheral_set_busy(true);
 }
