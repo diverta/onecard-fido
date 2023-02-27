a36bf676f0e1429f71880121792f61687315c50f
Makoto Morita <makmorit@outlook.jp>
2023-02-27 16:13:51 +0900

    認証用QRコードスキャン画面を新設
    ・現状実行できない機能を閉塞
    ・デザイン用の仮データを削除

--- MaintenanceTool/dotNET/MaintenanceToolApp/MaintenanceToolApp/OATH/ScanQRCodeWindow.xaml.cs
+++ MaintenanceTool/dotNET/MaintenanceToolApp/MaintenanceToolApp/OATH/ScanQRCodeWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using MaintenanceToolApp;
+using System.ComponentModel;
 using System.Windows;
 
 namespace MaintenanceTool.OATH
@@ -10,7 +11,9 @@ namespace MaintenanceTool.OATH
     {
         public ScanQRCodeWindow()
         {
+            // 画面項目の初期化
             InitializeComponent();
+            InitFieldValue();
         }
 
         public void ShowDialogWithOwner(Window ownerWindow)
@@ -19,6 +22,26 @@ namespace MaintenanceTool.OATH
             Owner = ownerWindow;
             ownerWindow.Hide();
             ShowDialog();
+        }
+
+        public void DoScan()
+        {
+            // TODO: 仮の実装です。
+            DialogUtil.ShowWarningMessage(this, Title, AppCommon.MSG_CMDTST_MENU_NOT_SUPPORTED);
+        }
+
+        //
+        // 画面初期化処理
+        //
+        private void InitFieldValue()
+        {
+            // ワンタイムパスワードの更新ボタンを使用不可とする
+            buttonUpdate.IsEnabled = false;
+
+            // 画面表示項目を初期化
+            labelAccountVal.Content = string.Empty;
+            labelIssuerVal.Content = string.Empty;
+            labelPassword.Content = string.Empty;
         }
 
         //
@@ -34,6 +57,11 @@ namespace MaintenanceTool.OATH
         {
             // 親画面を表示
             Owner.Show();
+        }
+
+        private void buttonScan_Click(object sender, RoutedEventArgs e)
+        {
+            DoScan();
         }
     }
 }
