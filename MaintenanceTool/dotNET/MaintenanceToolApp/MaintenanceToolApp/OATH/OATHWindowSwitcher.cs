98f44376c4ef69a6eb7e67091e544ec842583d3a
Makoto Morita <makmorit@outlook.jp>
2023-03-06 15:12:46 +0900

    リファクタリング
    ・OATH機能の画面表示制御を、`MainWindow.xaml.cs`から専用クラス`OATHWindowSwitcher`に移行

---
+++ MaintenanceTool/dotNET/MaintenanceToolApp/MaintenanceToolApp/OATH/OATHWindowSwitcher.cs
@@ -0,0 +1,36 @@
+﻿using MaintenanceToolApp;
+using System.Windows;
+using static MaintenanceToolApp.AppDefine;
+
+namespace MaintenanceTool.OATH
+{
+    internal class OATHWindowSwitcher
+    {
+        // 処理パラメーターの参照を保持
+        private readonly OATHParameter Parameter = null!;
+
+        public OATHWindowSwitcher(OATHParameter parameter)
+        {
+            // 処理パラメーターの参照を保持
+            Parameter = parameter;
+        }
+
+        //
+        // 画面表示制御
+        //
+        public void Switch(Window parentWindow)
+        {
+            // 実行コマンドにより処理分岐
+            switch (Parameter.Command) {
+            case Command.COMMAND_OATH_SCAN_QRCODE:
+                // QRコードスキャン-->アカウント登録-->ワンタイムパスワード参照を一息で実行
+                new ScanQRCodeWindow(Parameter).ShowDialogWithOwner(parentWindow);
+                break;
+            default:
+                // エラーメッセージをポップアップ表示
+                DialogUtil.ShowWarningMessage(parentWindow, parentWindow.Title, AppCommon.MSG_CMDTST_MENU_NOT_SUPPORTED);
+                break;
+            }
+        }
+    }
+}
