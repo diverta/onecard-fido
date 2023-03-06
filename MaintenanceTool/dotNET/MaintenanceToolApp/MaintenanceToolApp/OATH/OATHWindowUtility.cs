68f81c2ab506a593c4dad18d478ed9419d194b57
Makoto Morita <makmorit@outlook.jp>
2023-03-06 16:18:48 +0900

    ワンタイムパスワード参照画面の新設
    ・ワンタイムパスワード参照画面を表示する前に、ワンタイムパスワードを事前生成させるよう実装

--- MaintenanceTool/dotNET/MaintenanceToolApp/MaintenanceToolApp/OATH/OATHWindowUtility.cs
+++ MaintenanceTool/dotNET/MaintenanceToolApp/MaintenanceToolApp/OATH/OATHWindowUtility.cs
@@ -1,4 +1,7 @@
 ﻿using MaintenanceToolApp;
+using MaintenanceToolApp.CommonWindow;
+using System;
+using System.Threading.Tasks;
 using System.Windows;
 using static MaintenanceToolApp.AppDefine;
 
@@ -26,11 +29,65 @@ namespace MaintenanceTool.OATH
                 // QRコードスキャン-->アカウント登録-->ワンタイムパスワード参照を一息で実行
                 new ScanQRCodeWindow(Parameter).ShowDialogWithOwner(parentWindow);
                 break;
+            case Command.COMMAND_OATH_SHOW_PASSWORD:
+                // ワンタイムパスワード参照画面を表示
+                ShowTOTPDisplayWindow(parentWindow);
+                break;
             default:
                 // エラーメッセージをポップアップ表示
                 DialogUtil.ShowWarningMessage(parentWindow, parentWindow.Title, AppCommon.MSG_CMDTST_MENU_NOT_SUPPORTED);
                 break;
             }
+        }
+
+        private void ShowTOTPDisplayWindow(Window parentWindow)
+        {
+            // 選択されたアカウントをチェック
+            string[] account = Parameter.SelectedAccount.Split(":");
+            if (account.Length != 2) {
+                return;
+            }
+
+            // ワンタイムパスワードを事前生成
+            Parameter.OATHAccountIssuer = account[0];
+            Parameter.OATHAccountName = account[1];
+            Parameter.CommandTitle = AppCommon.MSG_LABEL_COMMAND_OATH_UPDATE_TOTP;
+            if (DoOATHProcess(parentWindow, Parameter) == false) {
+                return;
+            }
+
+            // ワンタイムパスワード参照画面を表示
+            new TOTPDisplayWindow(Parameter).ShowDialogWithOwner(parentWindow);
+        }
+
+        //
+        // OATHコマンド実行ユーティリティー
+        //
+        private static bool DoOATHProcess(Window window, OATHParameter parameter)
+        {
+            // パラメーターを設定し、コマンドを実行
+            Task task = Task.Run(() => {
+                new OATHProcess(parameter).DoProcess(OnOATHProcessTerminated);
+            });
+
+            // 進捗画面を表示
+            CommonProcessingWindow.OpenForm(window);
+
+            if (parameter.CommandSuccess == false) {
+                // 処理失敗時は、エラーメッセージをポップアップ表示
+                DialogUtil.ShowWarningMessage(window, parameter.ResultMessage, parameter.ResultInformativeMessage);
+                return false;
+            }
+
+            return true;
+        }
+
+        private static void OnOATHProcessTerminated(OATHParameter parameter)
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() => {
+                // 進捗画面を閉じる
+                CommonProcessingWindow.NotifyTerminate();
+            }));
         }
     }
 }
