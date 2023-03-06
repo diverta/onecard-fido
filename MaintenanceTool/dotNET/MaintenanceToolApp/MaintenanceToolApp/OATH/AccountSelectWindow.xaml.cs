cb8c1478e793c024ac9d4fd2640e43b8c06c4bad
Makoto Morita <makmorit@outlook.jp>
2023-03-06 09:22:44 +0900

    アカウント選択画面の新設
    ・アカウント選択画面のテンプレートを作成

---
+++ MaintenanceTool/dotNET/MaintenanceToolApp/MaintenanceToolApp/OATH/AccountSelectWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace MaintenanceTool.OATH
+{
+    /// <summary>
+    /// AccountSelectWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class AccountSelectWindow : Window
+    {
+        public AccountSelectWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
