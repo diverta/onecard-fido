0b13a931c8b16c661a1c3b62a8292af16360e01a
Makoto Morita <makmorit@outlook.jp>
2023-03-28 18:03:53 +0900

    暗号化処理の移行（Triple DES）
    ・Triple DESの専用関数を新設

---
+++ MaintenanceTool/macOSApp/tool_crypto/triple_des.c
@@ -0,0 +1,19 @@
+//
+//  triple_des.c
+//  MaintenanceTool
+//
+//  Created by Makoto Morita on 2023/03/28.
+//
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+bool triple_des_import_key(const unsigned char *key_raw, const size_t key_raw_size)
+{
+    return false;
+}
+
+bool triple_des_decrypt(const unsigned char *input, const size_t input_size, unsigned char *output, size_t *output_size)
+{
+    return false;
+}
