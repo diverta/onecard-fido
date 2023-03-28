150b033af9fa2406527a3285f2dc111d5ebe2275
Makoto Morita <makmorit@outlook.jp>
2023-03-28 11:40:08 +0900

    暗号化処理の移行（AES-256-CBC）
    ・暗号化／復号化処理を`CommonCrypto`で実装

--- MaintenanceTool/macOSApp/tool_crypto/aes_256_cbc.c
+++ MaintenanceTool/macOSApp/tool_crypto/aes_256_cbc.c
@@ -4,16 +4,55 @@
 //
 //  Created by Makoto Morita on 2023/03/28.
 //
+#import <CommonCrypto/CommonCryptor.h>
+
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdlib.h>
 
-uint8_t aes_256_cbc_enc(uint8_t *key, uint8_t *in, uint8_t *out, size_t *size)
+bool aes_256_cbc_enc(uint8_t *key, uint8_t *plain, size_t plain_size, uint8_t *encoded, size_t *encoded_size)
 {
-    return 0;
+    bool   ret = false;
+    size_t key_size = kCCKeySizeAES256;
+
+    // Allocate buffer for encryption
+    size_t size = plain_size + kCCBlockSizeAES128;
+    void  *buff = malloc(size);
+
+    // Perform encryption
+    CCCryptorStatus result = CCCrypt(kCCEncrypt, kCCAlgorithmAES, kCCOptionPKCS7Padding,
+                                     key, key_size, NULL, plain, plain_size, buff, size, encoded_size);
+    // Copy encrypted bytes
+    if (result == kCCSuccess) {
+        memcpy(encoded, buff, *encoded_size);
+        ret = true;
+    }
+
+    // Deallocate buffer
+    free(buff);
+    return ret;
 }
 
-uint8_t aes_256_cbc_dec(uint8_t *key, uint8_t *in, uint8_t *out, size_t *size)
+bool aes_256_cbc_dec(uint8_t *key, uint8_t *encoded, size_t encoded_size, uint8_t *decoded, size_t *decoded_size)
 {
-    return 0;
+    bool   ret = false;
+    size_t key_size = kCCKeySizeAES256;
+
+    // Allocate buffer for decryption
+    size_t size = encoded_size + kCCBlockSizeAES128;
+    void  *buff = malloc(size);
+
+    // Perform decryption
+    CCCryptorStatus result = CCCrypt(kCCDecrypt, kCCAlgorithmAES, kCCOptionPKCS7Padding,
+                                     key, key_size, NULL, encoded, encoded_size, buff, size, decoded_size);
+    // Copy decrypted bytes
+    if (result == kCCSuccess) {
+        memcpy(decoded, buff, *decoded_size);
+        ret = true;
+    }
+
+    // Deallocate buffer
+    free(buff);
+    return ret;
 }
--- MaintenanceTool/macOSApp/tool_crypto/aes_256_cbc.h
+++ MaintenanceTool/macOSApp/tool_crypto/aes_256_cbc.h
@@ -11,7 +11,7 @@
 #include <stdint.h>
 
 // 関数群
-uint8_t aes_256_cbc_enc(uint8_t *key, uint8_t *decoded, uint8_t *encoded, size_t *size);
-uint8_t aes_256_cbc_dec(uint8_t *key, uint8_t *encoded, uint8_t *decoded, size_t *size);
+bool aes_256_cbc_enc(uint8_t *key, uint8_t *plain, size_t plain_size, uint8_t *encoded, size_t *encoded_size);
+bool aes_256_cbc_dec(uint8_t *key, uint8_t *encoded, size_t encoded_size, uint8_t *decoded, size_t *decoded_size);
 
 #endif /* aes_256_cbc_h */
