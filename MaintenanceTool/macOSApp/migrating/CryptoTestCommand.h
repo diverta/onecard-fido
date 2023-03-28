a9cfb2349cdcd46e6462a4a1890d96dd0c777041
Makoto Morita <makmorit@outlook.jp>
2023-03-28 16:36:51 +0900

    暗号化処理の移行（AES-256-CBC）
    ・移行完了までは、移行前テスト用のモジュールを為念で残します。

---
+++ MaintenanceTool/macOSApp/migrating/CryptoTestCommand.h
@@ -0,0 +1,18 @@
+//
+//  CryptoTestCommand.h
+//  MaintenanceTool
+//
+//  Created by Makoto Morita on 2023/03/28.
+//
+#ifndef CryptoTestCommand_h
+#define CryptoTestCommand_h
+
+#import <Foundation/Foundation.h>
+
+@interface CryptoTestCommand : NSObject
+
+    - (void)testAES256CBC;
+
+@end
+
+#endif /* CryptoTestCommand_h */
