d03cad25da65ca64d745135c2cc2e0d65781ccf2
Makoto Morita <makmorit@outlook.jp>
2023-04-12 11:15:36 +0900

    文字列データの妥当性チェック対応
    ・アカウント一覧のアカウント名が、表示可能文字列かどうかのチェックを行うよう修正

--- MaintenanceTool/macOSApp/MaintenanceToolApp/AppCommonMessage.h
+++ MaintenanceTool/macOSApp/MaintenanceToolApp/AppCommonMessage.h
@@ -375,6 +375,7 @@
 #define MSG_ERROR_OATH_CALCULATE_FAILED             @"OATHワンタイムパスワード生成時、エラーが発生しました（SW=0x%04x）"
 #define MSG_INFO_OATH_CALCULATE_SUCCESS             @"OATHワンタイムパスワードを生成しました。"
 #define MSG_ERROR_OATH_LIST_ACCOUNT_FAILED          @"OATHアカウント一覧の取得時、エラーが発生しました（SW=0x%04x）"
+#define MSG_ERROR_OATH_LIST_ACCOUNT_DATA_INVALID    @"認証器から取得したOATHアカウント一覧のデータが不正です。"
 #define MSG_ERROR_OATH_ACCOUNT_DELETE_APDU_FAILED   @"OATHアカウント削除処理用のリクエストデータ生成に失敗しました。"
 #define MSG_ERROR_OATH_ACCOUNT_DELETE_FAILED        @"OATHアカウント削除時、エラーが発生しました（SW=0x%04x）"
 #define MSG_LABEL_COMMAND_OATH_GENERATE_TOTP        @"ワンタイムパスワードの生成"
