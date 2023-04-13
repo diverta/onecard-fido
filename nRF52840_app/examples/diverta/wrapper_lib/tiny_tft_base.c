35d2cf7b0428c3a67676038d51b7d160deea12fd
Makoto Morita <makmorit@outlook.jp>
2023-04-13 16:53:24 +0900

    リファクタリング
    ・#773 の移行内容を横展開
    （ラッパーモジュール`tiny_tft_base.c`を新設）

---
+++ nRF52840_app/examples/diverta/wrapper_lib/tiny_tft_base.c
@@ -0,0 +1,104 @@
+/* 
+ * File:   tiny_tft_base.c
+ * Author: makmorit
+ *
+ * Created on 2023/04/13, 16:45
+ */
+#include <sdk_config.h>
+
+#include "sdk_common.h"
+
+// for logging informations
+#define NRF_LOG_MODULE_NAME tiny_tft_base
+#include "nrf_log.h"
+NRF_LOG_MODULE_REGISTER();
+
+// 業務処理／HW依存処理間のインターフェース
+#include "fido_platform.h"
+
+// プラットフォーム依存コード
+#include "app_tiny_tft.h"
+
+// プラットフォーム非依存コード
+#include "tiny_tft_define.h"
+
+// for GPIO use
+#include "fido_board_define.h"
+
+void tiny_tft_base_start_reset(void)
+{
+    app_tiny_tft_set_rst(LOW);
+}
+
+void tiny_tft_base_end_reset(void)
+{
+    app_tiny_tft_set_rst(HIGH);
+}
+
+void tiny_tft_base_start_write(void)
+{
+    app_tiny_tft_set_c_s(LOW);
+}
+
+void tiny_tft_base_end_write(void)
+{
+    app_tiny_tft_set_c_s(HIGH);
+}
+
+void tiny_tft_base_delay_ms(uint32_t ms)
+{
+    fido_board_delay_ms(ms);
+}
+
+void tiny_tft_base_init(void)
+{
+    // Initialize spi config (SPI data clock frequency)
+    app_tiny_tft_initialize(4000000);
+
+    // Init basic control pins common to all connection types
+    app_tiny_tft_set_c_s(HIGH);
+    app_tiny_tft_set_d_c(HIGH);
+}
+
+//
+// データ転送関連
+//
+static uint8_t work_buf[16];
+
+bool tiny_tft_base_write_byte(uint8_t b)
+{
+    // １バイトを転送
+    work_buf[0] = b;
+    return app_tiny_tft_write(work_buf, 1);
+}
+
+bool tiny_tft_base_write_dword(uint32_t l)
+{
+    // ４バイトを転送
+    work_buf[0] = l >> 24;
+    work_buf[1] = l >> 16;
+    work_buf[2] = l >> 8;
+    work_buf[3] = l;
+    return app_tiny_tft_write(work_buf, 4);
+}
+
+void tiny_tft_base_write_command(uint8_t command_byte) 
+{
+    // Send the command byte
+    app_tiny_tft_set_d_c(LOW);
+    tiny_tft_base_write_byte(command_byte);
+    app_tiny_tft_set_d_c(HIGH);
+}
+
+void tiny_tft_base_write_data(uint8_t command_byte, uint8_t *data_bytes, uint8_t data_size) 
+{
+    // Send the command byte
+    app_tiny_tft_set_d_c(LOW);
+    tiny_tft_base_write_byte(command_byte);
+
+    // Send the data bytes
+    app_tiny_tft_set_d_c(HIGH);
+    if (data_size > 0) {
+        app_tiny_tft_write(data_bytes, data_size);
+    }
+}
